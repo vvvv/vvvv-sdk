d6fc10ba9ffd2ffa523140c79bad43b4fe8abfd5
joreg <joreg@vvvv.org>
2005-12-20 17:53:57 +0000

    updated auther/licence comments
    svn path=/freeframe/DevC++5/Trautner/; revision=1058

--- vvvv45/src/nodes/freeframes/Trautner/trunk/Trautner.cpp
+++ vvvv45/src/nodes/freeframes/Trautner/trunk/Trautner.cpp
@@ -1,37 +1,28 @@
-///////////////////////////////////////////////////////////////////////////////////
-// FreeFrameSample.cpp
-//
-// FreeFrame Open Video Plugin 
-// C Version
-//
-// Implementation of the Free Frame sample plugin
-//
-// www.freeframe.org
-// marcus@freeframe.org
+//////project name
+//Trautner
 
-/*
+//////description
+//freeframe plugin.
+//simple movement detection in regions defined via a grayscale bitmap. 
+//based on an idea by mr. trautner -> http://www.brainsalt.net
 
-Copyright (c) 2002, Marcus Clements www.freeframe.org
-All rights reserved.
+//////licence
+//GNU Lesser General Public License (LGPL)
+//english: http://www.gnu.org/licenses/lgpl.html
+//german: http://www.gnu.de/lgpl-ger.html
 
-FreeFrame 1.0 upgrade by Russell Blakeborough
-email: boblists@brightonart.org
+//////language/ide
+//dev-c++ 5
 
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+//////dependencies
+//opencv beta5 libraries:
+//http://sourceforge.net/projects/opencvlibrary
 
-   * Redistributions of source code must retain the above copyright
-     notice, this list of conditions and the following disclaimer.
-   * Redistributions in binary form must reproduce the above copyright
-     notice, this list of conditions and the following disclaimer in
-     the documentation and/or other materials provided with the
-     distribution.
-   * Neither the name of FreeFrame nor the names of its
-     contributors may be used to endorse or promote products derived
-     from this software without specific prior written permission.
+//////initial author
+//joreg -> joreg@gmx.at
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-*/
+//////edited by
+//your name here
 
 //includes 
 #include "Trautner.h"
--- vvvv45/src/nodes/freeframes/Trautner/trunk/readme.txt
+++ vvvv45/src/nodes/freeframes/Trautner/trunk/readme.txt
@@ -1,6 +1,32 @@
-devcpp5 code for trautner freeframe plugin.
+//////project name
+//Trautner
 
-simple movement detection in regions defined via a grayscale bitmap.
+//////description
+//freeframe plugin.
+//simple movement detection in regions defined via a grayscale bitmap. 
+//based on an idea by mr. trautner -> http://www.brainsalt.net
 
-to compile you'll need opencv beta5 libraries:
-http://sourceforge.net/projects/opencvlibrary
+//////licence
+//GNU Lesser General Public License (LGPL)
+//english: http://www.gnu.org/licenses/lgpl.html
+//german: http://www.gnu.de/lgpl-ger.html
+
+//////language/ide
+//dev-c++ 5
+
+//////dependencies
+//opencv beta5 libraries:
+//http://sourceforge.net/projects/opencvlibrary
+
+//////initial author
+//joreg -> joreg@gmx.at
+
+//////edited by
+//your name here
+
+
+___________________________________
+people that have been interested in
+Trautner
+have also been interested in
+http://vvvv.meso.net
