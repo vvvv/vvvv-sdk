d6b634f09d62b6fa56e9b468626f6eba746cf5d6
Jens Röhner <xsnej@gmx.de>
2008-07-10 10:07:02 +0000

    commented the need for common-controls
    svn path=/directshowfilter/VS2005/VSTHost/trunk/; revision=2252

--- vvvv45/src/nodes/directshowfilter/VSTHost/stdafx.h
+++ vvvv45/src/nodes/directshowfilter/VSTHost/stdafx.h
@@ -58,7 +58,7 @@ extern CAppModule _Module;
 #include <atlwin.h>
 
 #if defined _M_IX86
-  #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
+  //#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #elif defined _M_IA64
   #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='ia64' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #elif defined _M_X64
