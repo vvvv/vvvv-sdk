934afc2606315b4fd5d4586434ab8ea9aa8164f7
joreg <joreg@vvvv.org>
2008-08-15 14:02:55 +0000

    reworked to have separate Designer file
    svn path=/plugins/c#/Animation/Timeliner/trunk/; revision=2376

--- vvvv45/addonpack/src/nodes/plugins/Animation/Timeliner/Pins/TLValuePin.cs
+++ vvvv45/addonpack/src/nodes/plugins/Animation/Timeliner/Pins/TLValuePin.cs
@@ -9,18 +9,16 @@ using VVVV.PluginInterfaces.V1;
 
 namespace VVVV.Nodes.Timeliner
 {
-	public class TLValuePin: TLPin
+	public partial class TLValuePin: TLPin
 	{
 		private IValueOut FValueOut;
-		private RadioButton StepRadio, LinearRadio, CubicRadio;
-		private TLIOBoxValue FMinIO;
-		private TLIOBoxValue FMaxIO;
-		private Label FMinLabel, FMaxLabel;
-		
+
 		private TLInterpolationType FInterpolationIN;
 		
 		public TLValuePin(IPluginHost Host, TLTransformer Transformer, int Order, XmlNode PinSettings):base(Host, Transformer, Order, PinSettings, true, true)
 		{
+			InitializeComponent();
+			
 			//only load slices after all super constructors have been executed:
 			XmlAttribute attr = PinSettings.Attributes.GetNamedItem("SliceCount") as XmlAttribute;
         	LoadSlices(int.Parse(attr.Value));
@@ -38,23 +36,26 @@ namespace VVVV.Nodes.Timeliner
         	try
         	{
         		attr = PinSettings.Attributes.GetNamedItem("Minimum") as XmlAttribute;
-				FMinIO.Value = double.Parse(attr.Value, TimelinerPlugin.GNumberFormat);
+				MinIO.Value = double.Parse(attr.Value, TimelinerPlugin.GNumberFormat);
         	}
         	catch
         	{
-        		FMinIO.Value = -1;
+        		MinIO.Value = -1;
         	}
         	
         	try
         	{
         		attr = PinSettings.Attributes.GetNamedItem("Maximum") as XmlAttribute;
-				FMaxIO.Value = double.Parse(attr.Value, TimelinerPlugin.GNumberFormat);
+				MaxIO.Value = double.Parse(attr.Value, TimelinerPlugin.GNumberFormat);
         	}
         	catch
         	{
-        		FMaxIO.Value = 1;
+        		MaxIO.Value = 1;
         	}
         	
+        	MinIO.Minimum = MaxIO.Minimum = double.MinValue;
+	    	MinIO.Maximum = MaxIO.Maximum = double.MaxValue;
+	    	
         	UpdateSliceSpecificSettings();
 		}
 
@@ -72,77 +73,6 @@ namespace VVVV.Nodes.Timeliner
 	    	FHost.CreateValueOutput(Name, 1, null, TSliceMode.Dynamic, TPinVisibility.True, out FValueOut);
         	FValueOut.Order=Order;
         	FValueOut.SetSubType(double.MinValue, double.MaxValue, 0.0001D, 0, false, false, false);
-	    	
-	    	StepRadio = new System.Windows.Forms.RadioButton();
-	    	LinearRadio = new System.Windows.Forms.RadioButton();
-	    	CubicRadio = new System.Windows.Forms.RadioButton();
-	    	
-	    	FMinLabel = new Label();
-	    	FMinLabel.ForeColor = Color.Black;
-	    	FMinLabel.Text = "Minimum";
-	    	FMinLabel.AutoSize = true;
-	    	FMinLabel.Location = new Point(70, 70);
-	    	
-	    	FMaxLabel = new Label();
-	    	FMaxLabel.ForeColor = Color.Black;
-	    	FMaxLabel.Text = "Maximum";
-	    	FMaxLabel.Location = new Point(70, 40);
-	    	
-	    	FMinIO = new TLIOBoxValue();
-	    	FMinIO.Location = new Point(70, 85);
-	    	FMinIO.Size = new Size(60, 15);	    	
-	    	FMaxIO = new TLIOBoxValue();
-	    	FMaxIO.Location = new Point(70, 55);
-	    	FMaxIO.Size = new Size(60, 15);	    	
-			
-	    	FMinIO.Minimum = FMaxIO.Minimum = double.MinValue;
-	    	FMinIO.Maximum = FMaxIO.Maximum = double.MaxValue;
-	    	
-	    	UpdateMinMax();
-	    	
-			base.Controls.Add(StepRadio);
-			base.Controls.Add(LinearRadio);
-			base.Controls.Add(CubicRadio);
-			base.Controls.Add(FMinIO);
-			base.Controls.Add(FMaxIO);
-			base.Controls.Add(FMinLabel);
-			base.Controls.Add(FMaxLabel);			
-			
-			FMinIO.OnValueChange += new ValueChangeHandler(MinMaxIOChangedCB);
-			FMaxIO.OnValueChange += new ValueChangeHandler(MinMaxIOChangedCB);
-			
-			// 
-        	// StepRadio
-        	// 
-        	this.StepRadio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-        	this.StepRadio.Location = new System.Drawing.Point(4, 40);
-        	this.StepRadio.Name = "StepRadio";
-        	this.StepRadio.Size = new System.Drawing.Size(46, 26);
-        	this.StepRadio.TabIndex = 9;
-        	this.StepRadio.Text = "Step";
-        	this.StepRadio.UseVisualStyleBackColor = true;
-        	this.StepRadio.MouseDown += new System.Windows.Forms.MouseEventHandler(RadioButtonClick);
-        	// 
-        	// LinearRadio
-        	// 
-        	this.LinearRadio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-        	this.LinearRadio.Location = new System.Drawing.Point(4, 60);
-        	this.LinearRadio.Name = "LinearRadio";
-        	this.LinearRadio.Size = new System.Drawing.Size(53, 26);
-        	this.LinearRadio.Text = "Linear";
-        	this.LinearRadio.UseVisualStyleBackColor = true;
-        	this.LinearRadio.MouseDown += new System.Windows.Forms.MouseEventHandler(RadioButtonClick);
-        	// 
-        	// CubicRadio
-        	// 
-        	this.CubicRadio.Checked = true;
-        	this.CubicRadio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-        	this.CubicRadio.Location = new System.Drawing.Point(4, 80);
-        	this.CubicRadio.Name = "CubicRadio";
-        	this.CubicRadio.Size = new System.Drawing.Size(51, 26);
-        	this.CubicRadio.Text = "Cubic";
-        	this.CubicRadio.UseVisualStyleBackColor = true;
-        	this.CubicRadio.MouseDown += new System.Windows.Forms.MouseEventHandler(RadioButtonClick);
 		}
 		
 		protected override void InitializeHeight()
@@ -164,12 +94,12 @@ namespace VVVV.Nodes.Timeliner
     		pin.Attributes.Append(attr);
     		
     		attr = FSettings.CreateAttribute("Minimum");
-    		string tmp = String.Format("{0:F4}", FMinIO.Value);
+    		string tmp = String.Format("{0:F4}", MinIO.Value);
 			attr.Value = tmp.Replace(',', '.');
     		pin.Attributes.Append(attr);
     		
     		attr = FSettings.CreateAttribute("Maximum");
-			tmp = String.Format("{0:F4}", FMaxIO.Value);
+			tmp = String.Format("{0:F4}", MaxIO.Value);
 			attr.Value = tmp.Replace(',', '.');
     		pin.Attributes.Append(attr);
     		
@@ -211,8 +141,8 @@ namespace VVVV.Nodes.Timeliner
 		public override void AddSlice(int At)
 		{
 			TLValueSlice ss = new TLValueSlice(FHost, this, FOutputSlices.Count, FOrder);
-			ss.MinValue = FMinIO.Value;
-			ss.MaxValue = FMaxIO.Value;
+			ss.MinValue = MinIO.Value;
+			ss.MaxValue = MaxIO.Value;
 			
 			AddSlice(At, ss);
 			
@@ -256,8 +186,8 @@ namespace VVVV.Nodes.Timeliner
 		{
 			foreach (TLValueSlice vs in FOutputSlices)
 			{
-				vs.MinValue = FMinIO.Value;
-				vs.MaxValue = FMaxIO.Value;
+				vs.MinValue = MinIO.Value;
+				vs.MaxValue = MaxIO.Value;
 			}			
 		}
 		
