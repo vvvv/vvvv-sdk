2905270c8706083f577e1e385e72c238c5f67341
woei <woei@refect.net>
2008-10-28 19:18:13 +0000

    cleaned and updated
    svn path=/plugins/c#/Windows/ScreenInfo/trunk/; revision=2599

--- vvvv45/addonpack/src/nodes/plugins/Windows/ScreenInfo/ScreenInfo.cs
+++ vvvv45/addonpack/src/nodes/plugins/Windows/ScreenInfo/ScreenInfo.cs
@@ -32,12 +32,14 @@ namespace VVVV.Nodes
 {
 	
 	//class definition
-	public class ScreenInfo: IPlugin
+	public class ScreenInfo: IPlugin, IDisposable
     {	          	
     	#region field declaration
     	
     	//the host (mandatory)
-    	private IPluginHost FHost; 
+    	private IPluginHost FHost;
+    	// Track whether Dispose has been called.
+   		private bool FDisposed = false;
     	
     	//input pin declaration
     	private IValueIn FRefresh;
@@ -60,11 +62,61 @@ namespace VVVV.Nodes
 			//nothing to declare for this node
 		}
         
+        // Implementing IDisposable's Dispose method.
+        // Do not make this method virtual.
+        // A derived class should not be able to override this method.
+        public void Dispose()
+        {
+        	Dispose(true);
+        	// Take yourself off the Finalization queue
+        	// to prevent finalization code for this object
+        	// from executing a second time.
+        	GC.SuppressFinalize(this);
+        }
+        
+        // Dispose(bool disposing) executes in two distinct scenarios.
+        // If disposing equals true, the method has been called directly
+        // or indirectly by a user's code. Managed and unmanaged resources
+        // can be disposed.
+        // If disposing equals false, the method has been called by the
+        // runtime from inside the finalizer and you should not reference
+        // other objects. Only unmanaged resources can be disposed.
+        protected virtual void Dispose(bool disposing)
+        {
+        	// Check to see if Dispose has already been called.
+        	if(!FDisposed)
+        	{
+        		if(disposing)
+        		{
+        			// Dispose managed resources.
+        		}
+        		// Release unmanaged resources. If disposing is false,
+        		// only the following code is executed.
+	        	
+        		FHost.Log(TLogType.Debug, "ScreenInfo is being deleted");
+        		
+        		// Note that this is not thread safe.
+        		// Another thread could start disposing the object
+        		// after the managed resources are disposed,
+        		// but before the disposed flag is set to true.
+        		// If thread safety is necessary, it must be
+        		// implemented by the client.
+        	}
+        	FDisposed = true;
+        }
+        
+        // Use C# destructor syntax for finalization code.
+        // This destructor will run only if the Dispose method
+        // does not get called.
+        // It gives your base class the opportunity to finalize.
+        // Do not provide destructors in types derived from this class.
         ~ScreenInfo()
-	    {
-	    	//the nodes destructor
-        	//nothing to destruct
-	    }
+        {
+        	// Do not re-create Dispose clean-up code here.
+        	// Calling Dispose(false) is optimal in terms of
+        	// readability and maintainability.
+        	Dispose(false);
+        }
 
         #endregion constructor/destructor
         
