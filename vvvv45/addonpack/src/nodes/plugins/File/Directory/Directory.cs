3d78fe350d416d390cdc12dd207f8306260de313
woei <woei@refect.net>
2008-10-28 19:12:14 +0000

    cleaned and updated
    svn path=/plugins/c#/File/Directory/trunk/; revision=2597

--- vvvv45/addonpack/src/nodes/plugins/File/Directory/Directory.cs
+++ vvvv45/addonpack/src/nodes/plugins/File/Directory/Directory.cs
@@ -34,12 +34,14 @@ namespace VVVV.Nodes
 {
 	
 	//class definition
-	public class Directory: IPlugin
+	public class Directory: IPlugin, IDisposable
     {	          	
     	#region field declaration
     	
     	//the host (mandatory)
     	private IPluginHost FHost; 
+    	// Track whether Dispose has been called.
+   		private bool FDisposed = false;
     	
     	//input pin declaration
     	private IStringIn FDir;
@@ -61,12 +63,61 @@ namespace VVVV.Nodes
 			//the nodes constructor
 			//nothing to declare for this node
 		}
+        // Implementing IDisposable's Dispose method.
+        // Do not make this method virtual.
+        // A derived class should not be able to override this method.
+        public void Dispose()
+        {
+        	Dispose(true);
+        	// Take yourself off the Finalization queue
+        	// to prevent finalization code for this object
+        	// from executing a second time.
+        	GC.SuppressFinalize(this);
+        }
         
+        // Dispose(bool disposing) executes in two distinct scenarios.
+        // If disposing equals true, the method has been called directly
+        // or indirectly by a user's code. Managed and unmanaged resources
+        // can be disposed.
+        // If disposing equals false, the method has been called by the
+        // runtime from inside the finalizer and you should not reference
+        // other objects. Only unmanaged resources can be disposed.
+        protected virtual void Dispose(bool disposing)
+        {
+        	// Check to see if Dispose has already been called.
+        	if(!FDisposed)
+        	{
+        		if(disposing)
+        		{
+        			// Dispose managed resources.
+        		}
+        		// Release unmanaged resources. If disposing is false,
+        		// only the following code is executed.
+	        	
+        		FHost.Log(TLogType.Debug, "Directory is being deleted");
+        		
+        		// Note that this is not thread safe.
+        		// Another thread could start disposing the object
+        		// after the managed resources are disposed,
+        		// but before the disposed flag is set to true.
+        		// If thread safety is necessary, it must be
+        		// implemented by the client.
+        	}
+        	FDisposed = true;
+        }
+
+        // Use C# destructor syntax for finalization code.
+        // This destructor will run only if the Dispose method
+        // does not get called.
+        // It gives your base class the opportunity to finalize.
+        // Do not provide destructors in types derived from this class.
         ~Directory()
-	    {
-	    	//the nodes destructor
-        	//nothing to destruct
-	    }
+        {
+        	// Do not re-create Dispose clean-up code here.
+        	// Calling Dispose(false) is optimal in terms of
+        	// readability and maintainability.
+        	Dispose(false);
+        }
 
         #endregion constructor/destructor
         
@@ -179,11 +230,12 @@ namespace VVVV.Nodes
         			FNewDir.GetString(i, out curNewDir);
         			FRename.GetValue(i, out currentRename);
         			
+        			string hostPath;
+        			FHost.GetHostPath(out hostPath);
         			
-        			Assembly self = Assembly.GetExecutingAssembly();
         			if (curCustomRoot == "")
         			{
-        				curCustomRoot=self.Location.ToString();
+        				curCustomRoot = hostPath;
         			}
         			else
         			{
@@ -193,7 +245,7 @@ namespace VVVV.Nodes
         				    	message+=curCustomRoot;
         				    	message+= "\' is not a valid root-path";
         				    	FHost.Log(TLogType.Warning, message);
-        				    	curCustomRoot=self.Location.ToString();
+        				    	curCustomRoot=hostPath;
         				    }
         			}
         			
