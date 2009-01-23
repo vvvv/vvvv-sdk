6821a489312f389b498eac38e989b0be2e0fe524
joreg <joreg@vvvv.org>
2009-01-23 16:28:17 +0000

    svn path=/plugins/c#/_Samples/_PluginMeshTemplate/trunk/; revision=2939

--- vvvv45/addonpack/src/nodes/plugins/_Samples/_PluginMeshTemplate/PluginMeshTemplate.cs
+++ vvvv45/addonpack/src/nodes/plugins/_Samples/_PluginMeshTemplate/PluginMeshTemplate.cs
@@ -13,7 +13,7 @@
 //german: http://www.gnu.de/lgpl-ger.html
 
 //////language/ide
-//C# sharpdevelop 
+//C# sharpdevelop
 
 //////dependencies
 //VVVV.PluginInterfaces.V1;
@@ -43,112 +43,106 @@ namespace VVVV.Nodes
 {
 	//class definition
 	public class PluginMeshTemplate: IPlugin, IDisposable, IPluginDXMesh
-    {	          	
-    	#region field declaration
-    	
-    	//the host (mandatory)
-    	private IPluginHost FHost; 
-    	//Track whether Dispose has been called.
-   		private bool FDisposed = false;
-   		//Direct3D9 device
-   		private Device FD3D9Device;
-   		//Direct3D9 mesh
-   		private Mesh FD3D9Mesh;
-   		
-   		private IDXMeshIO FMyMeshOutput;
-    	
-    	#endregion field declaration
-       
-    	#region constructor/destructor
-    	
-        public PluginMeshTemplate()
-        {
+	{
+		#region field declaration
+		
+		//the host (mandatory)
+		private IPluginHost FHost;
+		//Track whether Dispose has been called.
+		private bool FDisposed = false;
+		
+		private IDXMeshIO FMyMeshOutput;
+		
+		private List<Mesh> FDeviceMeshes = new List<Mesh>();
+		
+		#endregion field declaration
+		
+		#region constructor/destructor
+		
+		public PluginMeshTemplate()
+		{
 			//the nodes constructor
 			//nothing to declare for this node
 		}
-        
-        // Implementing IDisposable's Dispose method.
-        // Do not make this method virtual.
-        // A derived class should not be able to override this method.
-        public void Dispose()
-        {
-        	Dispose(true);
-        	// Take yourself off the Finalization queue
-        	// to prevent finalization code for this object
-        	// from executing a second time.
-        	GC.SuppressFinalize(this);
-        }
-        
-        // Dispose(bool disposing) executes in two distinct scenarios.
-        // If disposing equals true, the method has been called directly
-        // or indirectly by a user's code. Managed and unmanaged resources
-        // can be disposed.
-        // If disposing equals false, the method has been called by the
-        // runtime from inside the finalizer and you should not reference
-        // other objects. Only unmanaged resources can be disposed.
-        protected virtual void Dispose(bool disposing)
-        {
-        	// Check to see if Dispose has already been called.
-        	if(!FDisposed)
-        	{
-        		if(disposing)
-        		{
-        			// Dispose managed resources.
-        			if (FD3D9Mesh != null)
-						FD3D9Mesh.Dispose();
-					if (FD3D9Device != null)
-						FD3D9Device.Dispose();
-        		}
-        		// Release unmanaged resources. If disposing is false,
-        		// only the following code is executed.
-	        	
-        		FHost.Log(TLogType.Debug, "PluginMeshTemplate is being deleted");
-        		
-        		// Note that this is not thread safe.
-        		// Another thread could start disposing the object
-        		// after the managed resources are disposed,
-        		// but before the disposed flag is set to true.
-        		// If thread safety is necessary, it must be
-        		// implemented by the client.
-        	}
-        	FDisposed = true;
-        }
+		
+		// Implementing IDisposable's Dispose method.
+		// Do not make this method virtual.
+		// A derived class should not be able to override this method.
+		public void Dispose()
+		{
+			Dispose(true);
+			// Take yourself off the Finalization queue
+			// to prevent finalization code for this object
+			// from executing a second time.
+			GC.SuppressFinalize(this);
+		}
+		
+		// Dispose(bool disposing) executes in two distinct scenarios.
+		// If disposing equals true, the method has been called directly
+		// or indirectly by a user's code. Managed and unmanaged resources
+		// can be disposed.
+		// If disposing equals false, the method has been called by the
+		// runtime from inside the finalizer and you should not reference
+		// other objects. Only unmanaged resources can be disposed.
+		protected virtual void Dispose(bool disposing)
+		{
+			// Check to see if Dispose has already been called.
+			if(!FDisposed)
+			{
+				if(disposing)
+				{
+					// Dispose managed resources.
+				}
+				// Release unmanaged resources. If disposing is false,
+				// only the following code is executed.
+				
+				FHost.Log(TLogType.Debug, "PluginMeshTemplate is being deleted");
+				
+				// Note that this is not thread safe.
+				// Another thread could start disposing the object
+				// after the managed resources are disposed,
+				// but before the disposed flag is set to true.
+				// If thread safety is necessary, it must be
+				// implemented by the client.
+			}
+			FDisposed = true;
+		}
 
-        // Use C# destructor syntax for finalization code.
-        // This destructor will run only if the Dispose method
-        // does not get called.
-        // It gives your base class the opportunity to finalize.
-        // Do not provide destructors in types derived from this class.
-        ~PluginMeshTemplate()
-        {
-        	// Do not re-create Dispose clean-up code here.
-        	// Calling Dispose(false) is optimal in terms of
-        	// readability and maintainability.
-        	Dispose(false);
-        }
-        #endregion constructor/destructor
-        
-        #region node name and infos
-       
-        //provide node infos 
-        private static IPluginInfo FPluginInfo;
-        public static IPluginInfo PluginInfo
-	    {
-	        get 
-	        {
-	        	if (FPluginInfo == null)
+		// Use C# destructor syntax for finalization code.
+		// This destructor will run only if the Dispose method
+		// does not get called.
+		// It gives your base class the opportunity to finalize.
+		// Do not provide destructors in types derived from this class.
+		~PluginMeshTemplate()
+		{
+			// Do not re-create Dispose clean-up code here.
+			// Calling Dispose(false) is optimal in terms of
+			// readability and maintainability.
+			Dispose(false);
+		}
+		#endregion constructor/destructor
+		
+		#region node name and infos
+		
+		//provide node infos
+		private static IPluginInfo FPluginInfo;
+		public static IPluginInfo PluginInfo
+		{
+			get
+			{
+				if (FPluginInfo == null)
 				{
 					//fill out nodes info
 					//see: http://www.vvvv.org/tiki-index.php?page=Conventions.NodeAndPinNaming
 					FPluginInfo = new PluginInfo();
 					
 					//the nodes main name: use CamelCaps and no spaces
-					FPluginInfo.Name = "TemplateMeshNode";
+					FPluginInfo.Name = "Template";
 					//the nodes category: try to use an existing one
 					FPluginInfo.Category = "Template";
 					//the nodes version: optional. leave blank if not
 					//needed to distinguish two nodes of the same name and category
-					FPluginInfo.Version = "";
+					FPluginInfo.Version = "Mesh";
 					
 					//the nodes author: your sign
 					FPluginInfo.Author = "vvvv group";
@@ -173,90 +167,87 @@ namespace VVVV.Nodes
 					//leave above as is
 				}
 				return FPluginInfo;
-	        }
+			}
 		}
 
-        public bool AutoEvaluate
-        {
-        	//return true if this node needs to calculate every frame even if nobody asks for its output
-        	get {return false;}
-        }
-        
-        #endregion node name and infos
-        
-      	#region pin creation
-        
-        //this method is called by vvvv when the node is created
-        public void SetPluginHost(IPluginHost Host)
-	    {
-        	//assign host
-	    	FHost = Host;
-	    	
-	    	//create outputs	    	
-	    	FHost.CreateMeshOutput("Mesh", TSliceMode.Dynamic, TPinVisibility.True, out FMyMeshOutput);
-        } 
+		public bool AutoEvaluate
+		{
+			//return true if this node needs to calculate every frame even if nobody asks for its output
+			get {return false;}
+		}
+		
+		#endregion node name and infos
+		
+		#region pin creation
+		
+		//this method is called by vvvv when the node is created
+		public void SetPluginHost(IPluginHost Host)
+		{
+			//assign host
+			FHost = Host;
+			
+			//create outputs
+			FHost.CreateMeshOutput("Mesh", TSliceMode.Dynamic, TPinVisibility.True, out FMyMeshOutput);
+		}
 
-        #endregion pin creation
-        
-        #region mainloop
-        
-        public void Configurate(IPluginConfig Input)
-        {
-        	//nothing to configure in this plugin
-        	//only used in conjunction with inputs of type cmpdConfigurate
-        }
-        
-        //here we go, thats the method called by vvvv each frame
-        //all data handling should be in here
-        public void Evaluate(int SpreadMax)
-        {
-
-        }
-             
-        #endregion mainloop  
-        
-        #region DXMesh
-        public void CreateResource(IPluginOut ForPin, int OnDevice)
-        {
-        	//if resource is not yet created for given OnDevice, create it now
-        	if (FD3D9Mesh == null)
-        	{
-        		FHost.Log(TLogType.Debug, "Creating Resource...");
-        		FD3D9Device = Device.FromPointer(new IntPtr(OnDevice));
-        		//FD3D9Mesh = Mesh.CreateTeapot(FD3D9Device);
-        		
-        		Mesh[] ms = new Mesh[2];
-        		ms[0] = Mesh.CreateTeapot(FD3D9Device);
-        		ms[1] = Mesh.CreateBox(FD3D9Device, 1, 2, 2);
-        		FD3D9Mesh = Mesh.Concatenate(FD3D9Device, ms, 0);
-        		
-        		ms[0].Dispose();
-        		ms[0] = null;
-        		ms[1].Dispose();
-        		ms[1] = null;
-        		ms = null;
-
-        		FMyMeshOutput.SliceCount = 2;
-        	}       	
-        }
-        
+		#endregion pin creation
+		
+		#region mainloop
+		
+		public void Configurate(IPluginConfig Input)
+		{
+			//nothing to configure in this plugin
+			//only used in conjunction with inputs of type cmpdConfigurate
+		}
+		
+		//here we go, thats the method called by vvvv each frame
+		//all data handling should be in here
+		public void Evaluate(int SpreadMax)
+		{
+			FMyMeshOutput.SliceCount = SpreadMax;
+		}
+		
+		#endregion mainloop
+		
+		#region DXMesh
+		public void UpdateResource(IPluginOut ForPin, int OnDevice)
+		{
+			Mesh m = FDeviceMeshes.Find(delegate(Mesh ms) {return ms.Device.ComPointer == (IntPtr)OnDevice;});
+			
+			//if resource is not yet created on given Device, create it now
+			if (m == null)
+			{
+				FHost.Log(TLogType.Debug, "Creating Resource...");
+				Device dev = Device.FromPointer(new IntPtr(OnDevice));
+				
+				FDeviceMeshes.Add(Mesh.CreateTeapot(dev));
+			}
+		}
+		
 		public void DestroyResource(IPluginOut ForPin, int OnDevice, bool OnlyUnManaged)
 		{
-			//dispose resources that were created for given OnDevice
-			FHost.Log(TLogType.Debug, "Destroying Resource...");
-			FD3D9Mesh.Dispose();
-			FD3D9Mesh = null;
+			//dispose resources that were created on given Device
+			Mesh m = FDeviceMeshes.Find(delegate(Mesh ms) {return ms.Device.ComPointer == (IntPtr)OnDevice;});
+			
+			if (m != null)
+			{
+				FHost.Log(TLogType.Debug, "Destroying Resource...");
+				FDeviceMeshes.Remove(m);
+				m.Dispose();
+				m = null;
+			}
 		}
 		
 		public void GetMesh(IDXMeshIO ForPin, int OnDevice, out int Mesh)
 		{
-        	Mesh = 0;
-        	if (ForPin == FMyMeshOutput)
-				if (FD3D9Mesh != null)
-					Mesh = FD3D9Mesh.ComPointer.ToInt32();
-        }
+			Mesh = 0;
+			if (ForPin == FMyMeshOutput)
+			{
+				Mesh m = FDeviceMeshes.Find(delegate(Mesh ms) {return ms.Device.ComPointer == (IntPtr)OnDevice;});
+				if (m != null)
+					Mesh = m.ComPointer.ToInt32();
+			}
+		}
 		#endregion
-		
-	
 	}
 }
