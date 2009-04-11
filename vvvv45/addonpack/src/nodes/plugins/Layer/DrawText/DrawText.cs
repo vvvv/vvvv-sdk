4d0bae5d0fe79225ea4fbc4faf30b9db6fcce349
joreg <joreg@vvvv.org>
2009-04-11 16:30:06 +0000

    svn path=/plugins/c#/Layer/DrawText/trunk/; revision=3184

--- vvvv45/addonpack/src/nodes/plugins/Layer/DrawText/DrawText.cs
+++ vvvv45/addonpack/src/nodes/plugins/Layer/DrawText/DrawText.cs
@@ -36,7 +36,7 @@ using VVVV.PluginInterfaces.V1;
 using VVVV.Utils;
 using VVVV.Utils.VColor;
 using VVVV.Utils.VMath;
-using VVVV.Utils.DX;
+using VVVV.Shared.VSlimDX;
 
 using SlimDX;
 using SlimDX.Direct3D9;
