b8da46b557c4cde0ca3874853733522064604c99
Nils Buhlert <nils.buhlert@gmx.de>
2009-09-29 08:16:40 +0000

    svn path=/plugins/c#/Http/Webinterface/trunk/; revision=3488

--- vvvv45/addonpack/src/nodes/plugins/Http/Webinterface/HttpServer/LoadSelectContent.cs
+++ vvvv45/addonpack/src/nodes/plugins/Http/Webinterface/HttpServer/LoadSelectContent.cs
@@ -12,30 +12,9 @@ namespace VVVV.Webinterface.HttpServer
     class LoadSelectContent
     {
 
-        private string mContent;
         private byte[] mContentAsByte;
-        private string mFileExtension;
-        private string mFileLocation;
         private WebinterfaceSingelton mWebinterfaceSingelton = WebinterfaceSingelton.getInstance();
         
-
-        public string Content
-        {
-            get
-            {
-                return mContent;
-            }
-        }
-
-
-        public string FileExtension
-        {
-            get
-            {
-                return mFileExtension;
-            }
-
-        }
 
         public byte[] ContentAsBytes
         {
@@ -45,76 +24,79 @@ namespace VVVV.Webinterface.HttpServer
             }
         }
 
+        /// <summary>
+        /// Load and Build the Requested File from VVVV or from Disc
+        /// </summary>
+        /// <param name="pFilename">Filename which is requested by the client</param>
+        /// <param name="pFileLocation">whole file URI which is reqquested by the client</param>
+        /// <param name="pPaths">Root Pathes set by VVVV Renderer (HTTP)</param>
+        /// <param name="pHtmlPages">Pages to Build by an GET Request</param>
         public LoadSelectContent( string pFilename,string pFileLocation, List<string> pPaths, SortedList<string,byte[]> pHtmlPages)
         {
 
-
             if (pFilename == "dummy.html")
             {
+                //Build an special HTML dummyFile for the comet communication
                 string tPageToSend = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">";
                 mContentAsByte = Encoding.UTF8.GetBytes(tPageToSend);
-            }else if(mWebinterfaceSingelton.ServerFilesUrl.Contains(pFilename))
+            }
+            else if(mWebinterfaceSingelton.ServerFilesUrl.Contains(pFilename))
             {
-
+                // if the file name is set by VVVV Build all Files from an Requested Page and Encode it as Bytes
                 SortedList<string, string> tServerFiles;
                 if (mWebinterfaceSingelton.GuiLists.ContainsKey(pFilename))
                 {
-                    mWebinterfaceSingelton.BuildPages(pFilename);
-                    tServerFiles = mWebinterfaceSingelton.ServerFiles;
+
+                    tServerFiles = mWebinterfaceSingelton.BuildPages(pFilename);
                 }
                 else
                 {
-                    tServerFiles = mWebinterfaceSingelton.ServerFiles;
+                    tServerFiles = mWebinterfaceSingelton.BuildPages(pFilename);
                 }
-
 
                 string RequestedFile;
                 tServerFiles.TryGetValue(pFilename, out RequestedFile);
                 mContentAsByte = Encoding.UTF8.GetBytes(RequestedFile);
             }else
             {
+                //loads file from disc
                 LoadFromDisc(pFilename,pFileLocation,pPaths);
             }
         }
 
-
+        /// <summary>
+        /// Checks is vvvv defines the file or if it should be searched in the defined root directories oon the disc
+        /// </summary>
+        /// <param name="pFilename">requested file name</param>
+        /// <param name="pFileLocation">request file URI</param>
+        /// <param name="pPaths">defined root directories</param>
         private void LoadFromDisc(string pFilename,string pFileLocation, List<string> pPaths)
         {
+
             bool FoundFileFlag = false;
 
+            //checks for evers path defined by vvvv if there is an file on disc
             foreach (string pPath in pPaths)
             {
+                //Changes the HTML References to windows file references
+                pFileLocation = Regex.Replace(pFileLocation, @"/", @"\");
 
-                
+                //delets the first backslashes because it throws an wrong path in Path.Combine
+                pFileLocation = Regex.Replace(pFileLocation, @"^\\", ""); 
+                string newPath = Path.Combine(pPath,pFileLocation);
 
-                try
+                //check the combined absolute path
+                if (File.Exists(newPath))
                 {
-                    pFileLocation = Regex.Replace(pFileLocation, @"/", @"\");
-                    pFileLocation = Regex.Replace(pFileLocation, @"^\\", ""); 
-                    string newPath = Path.Combine(pPath,pFileLocation);
-                    
-                    if (File.Exists(newPath))
-                    {
-                        Debug.WriteLine("Filepath exist");
-                        Debug.WriteLine("pPath: " + pPath);
-                        Debug.WriteLine("pFileLocation: " + pFileLocation);
-                        Debug.WriteLine("newFilePath:  " + newPath);    
-
-                        FoundFileFlag = true;
-                        LoadFile(newPath);
-                    }
+                    FoundFileFlag = true;
+                    LoadFile(newPath);
                 }
-                catch (Exception ex)
+                else
                 {
-                    Debug.WriteLine(ex);
-                }
+                    //searches in every root folder if there is the requested file 
+                    DirectoryInfo tDInfo = new DirectoryInfo(pPath);
+                    FileInfo[] tFileInfoList = tDInfo.GetFiles();
 
-
-                DirectoryInfo tDInfo = new DirectoryInfo(pPath);
-                FileInfo[] tFileInfoList = tDInfo.GetFiles();
-
-                if (FoundFileFlag == false)
-                {
                     foreach (FileInfo pFileInfo in tFileInfoList)
                     {
                         if (pFileInfo.Name == pFilename)
@@ -125,51 +107,37 @@ namespace VVVV.Webinterface.HttpServer
                     }
                 }
             }
-
             if (FoundFileFlag == false)
             {
-                mContent = "File " + "'" + pFilename + "'" + " Not Found";
-                mContentAsByte = Encoding.UTF8.GetBytes(mContent);
-                if (pFilename.Contains("."))
-                {
-                    mFileExtension = (pFilename.Split('.'))[1];
-                }
+                mContentAsByte = Encoding.UTF8.GetBytes("File " + "'" + pFilename + "'" + " Not Found");
             }
         }
 
-
+        /// <summary>
+        /// Loads the Requested File from disc and encodes it as bytes
+        /// </summary>
+        /// <param name="pFilePath">Requested File Path</param>
         private void LoadFile(string pFilePath)
         {
             try
             {
                 FileStream tFs = new FileStream(pFilePath, FileMode.Open, FileAccess.Read, FileShare.Read);
                 BinaryReader reader = new BinaryReader(tFs);
-                mFileExtension = pFilePath;
 
                 byte[] bytes = new byte[tFs.Length];
-                int read;
-
-                while ((read = reader.Read(bytes, 0, bytes.Length)) != 0)
-                {
-                    // Read from the file and write the data to the network
-                    mContent = mContent + Encoding.UTF8.GetString(bytes, 0, read);
-                }
 
                 mContentAsByte = bytes;
 
                 reader.Close();
                 tFs.Close();
             }
-
             catch (FileLoadException ex)
             {
-                //////Debug.WriteLine("LoadSelectContent: " + ex.Message.ToString());
-                mContent = ex.Message.ToString();
+                mContentAsByte = Encoding.UTF8.GetBytes(ex.Message);
             }
             catch (Exception ex)
             {
-                //////Debug.WriteLine("LoadSelectContent: " + ex.Message.ToString());
-                mContent = ex.Message.ToString();
+                mContentAsByte = Encoding.UTF8.GetBytes(ex.Message);
             }
         }
     }
