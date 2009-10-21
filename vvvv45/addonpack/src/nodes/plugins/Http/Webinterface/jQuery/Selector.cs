ca103b869f8a3afab25b1f2147ce3f138827e853
Joshua Samberg <samberg@meso.net>
2009-10-21 16:27:05 +0000

    Cleaning up unneeded classes
    svn path=/plugins/c#/Http/Webinterface/trunk/; revision=3551

--- vvvv45/addonpack/src/nodes/plugins/Http/Webinterface/jQuery/Selector.cs
+++ vvvv45/addonpack/src/nodes/plugins/Http/Webinterface/jQuery/Selector.cs
@@ -4,17 +4,10 @@ using System.Text;
 
 namespace VVVV.Nodes.jQuery
 {
-	public abstract class Selector : IScriptGenerator
+	public class Selector
 	{
 		public static Selector AllSelector = new RawStringSelector("*");
 		public static Selector DocumentSelector = new NameObjectSelector("document");
 		public static Selector ThisSelector = new NameObjectSelector("this");
-
-
-		#region IScriptGenerator Members
-
-		public abstract string PScript(int indentSteps, bool breakInternalLines);
-
-		#endregion
 	}
 }
