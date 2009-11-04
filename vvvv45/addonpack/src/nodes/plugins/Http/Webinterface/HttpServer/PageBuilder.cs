3d764d09bb8ee81e01e5f4bf361e2f0e367cd8a4
Nils Buhlert <nils.buhlert@gmx.de>
2009-11-04 10:04:40 +0000

    - added CssBuilder.cs and PageBuilder.cs
    svn path=/plugins/c#/Http/Webinterface/trunk/; revision=3664

---
+++ vvvv45/addonpack/src/nodes/plugins/Http/Webinterface/HttpServer/PageBuilder.cs
@@ -0,0 +1,233 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.Threading;
+using System.Diagnostics;
+using VVVV.Webinterface.Utilities;
+using VVVV.Nodes.Http.BaseNodes;
+
+namespace VVVV.Nodes.Http
+{
+    class PageBuilder
+    {
+
+        #region field declaration
+
+
+        private Page mPage;
+        List<string> mJsFileList = new List<string>();
+        private CssBuilder mCssBuilder = new CssBuilder();
+        private Body mBody = new Body();
+        private SortedList<string, Tag> mTags = new SortedList<string, Tag>();
+        List<GuiDataObject> mGuiElemente  =new List<GuiDataObject>();
+        bool mBuildFlag = false;
+        private Object _updatelock = new Object();
+
+        public Page Page
+        {
+            get
+            {
+                Debug.WriteLine("get Page");
+                return mPage;
+            }
+        }
+
+        public Body Body
+        {
+            get
+            {
+                return mPage.Body;
+            }
+        }
+
+        public Head Head
+        {
+            get
+            {
+                return mPage.Head;
+            }
+        }
+
+        public string HtmlFile
+        {
+            get
+            {
+                Debug.WriteLine("Get HtmlFile");
+                return mPage.Text;
+            }
+        }
+
+        public StringBuilder CssFile
+        {
+            get
+            {
+                return mCssBuilder.CssFile;
+            }
+        }
+
+        public StringBuilder JsFile
+        {
+            get
+            {
+                StringBuilder tJsFile = new StringBuilder();
+                foreach (string pScript in mJsFileList)
+                {
+                    tJsFile.Append(pScript + Environment.NewLine);
+                }
+                return tJsFile;
+            }
+        }
+
+ 
+        #endregion field declaration
+
+
+        public PageBuilder()
+        {
+            
+        }
+
+
+        public void UpdateGuiList(List<GuiDataObject> pGuiElemente, Page pPage)
+        {
+            //Debug.WriteLine("Update Start");
+            if (Monitor.TryEnter(_updatelock, 1000))
+            {
+
+                try
+                {
+                    if (mBuildFlag == false)
+                    {
+                        mPage = pPage;
+                        mJsFileList.Clear();
+                        mCssBuilder.Reset();
+                        mTags.Clear();
+
+                        //Build
+                        mGuiElemente = new List<GuiDataObject>(pGuiElemente);
+                    }
+                }
+                finally
+                {
+                    Monitor.Exit(_updatelock);
+                }
+            }
+            //Debug.WriteLine("Update Ende");
+        }
+
+        public void Build()
+        {
+            Monitor.Enter(_updatelock);
+            mBuildFlag = true;
+            //Debug.WriteLine("Start Build Vorgang");
+            if (mGuiElemente.Count == 0)
+            {
+                Debug.WriteLine("count 0");
+            }
+
+            mPage.Body.ClearTagsInside();
+            mPage.Body = (Body)BuildHtmlFrame(new List<GuiDataObject>(mGuiElemente), mPage.Body);
+            //Debug.WriteLine("Ende Build Vorgang");
+            mCssBuilder.Build();
+            mBuildFlag = false;
+            Monitor.Exit(_updatelock);
+        }
+
+
+        private Tag BuildHtmlFrame(List<GuiDataObject> pGuiObjectIn, Tag tTag)
+        {
+
+            ////Debug.WriteLine("------------ Enter BuildHtmlFrame -------------");
+            ////Debug.WriteLine("pGuiObjectIn.Count: " + pGuiObjectIn.Count.ToString());
+            
+
+            foreach (GuiDataObject pElement in pGuiObjectIn)
+            {
+                //Debug.WriteLine("---------------- Enter foreachSchleife-----------------");
+                //Debug.WriteLine("SliceId: " + pElement.SliceId);
+                //Debug.WriteLine("tTag: " + tTag.Text + Environment.NewLine);
+                ////Debug.WriteLine("tTag.Level: " + tTag.Level);
+                ////Debug.WriteLine("tTag.Name: " + tTag.Name);
+
+                string javaScript = pElement.JavaScript;
+                
+                if (javaScript != null && javaScript.ToString() != "")
+                {
+                    AddJavaScript(javaScript);
+                }
+
+                Tag mtTag = pElement.Tag;
+
+                if (mTags.ContainsKey(pElement.SliceId) == false)
+                {
+                    mTags.Add(pElement.SliceId, pElement.Tag);
+                    AddCssProperties(pElement);
+
+                    if (pElement.GuiUpstreamList != null)
+                    {
+                        AddCssProperties(pElement);
+                       
+                        tTag.Insert(BuildHtmlFrame(pElement.GuiUpstreamList, pElement.Tag));
+                    }
+                    else
+                    {
+                            tTag.Insert(pElement.Tag);
+                    }
+                }
+                else
+                {
+                    Tag tDummyTag;
+                    mTags.TryGetValue(pElement.SliceId, out tDummyTag);
+
+                        tTag.Insert(tDummyTag);
+                    
+                }
+            }
+
+            //Debug.WriteLine("---------------- Exit foreachSchleife-----------------");
+            //Debug.WriteLine("tTag: " + tTag.Text + Environment.NewLine);
+            return tTag;
+        }
+
+        public void AddJavaScript(string pScript)
+        {
+            if(mJsFileList.Contains(pScript) == false)
+            {
+                mJsFileList.Add(pScript);
+            }
+        }
+
+
+
+        private void AddCssProperties(GuiDataObject pObject)
+        {
+            SortedList<string, string> tTransform = new SortedList<string, string>();
+
+            if (pObject.Transform != null)
+            {
+                tTransform = new SortedList<string, string>(pObject.Transform);
+            }
+
+            SortedList<string, string> tCssProperties = new SortedList<string, string>();
+            if (pObject.CssProperties != null)
+            {
+                tCssProperties = new SortedList<string, string>(pObject.CssProperties);
+            }
+
+            foreach (KeyValuePair<string, string> KeyPair in tTransform)
+            {
+                tCssProperties.Add(KeyPair.Key, KeyPair.Value);
+            }
+
+            mCssBuilder.AddCssSliceList(pObject.SliceId, tCssProperties);
+            mCssBuilder.AddNodeId(pObject.NodeId);
+        }
+
+
+
+
+  
+
+    }
+}
