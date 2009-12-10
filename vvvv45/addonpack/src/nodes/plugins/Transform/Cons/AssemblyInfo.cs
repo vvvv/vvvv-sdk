7bf245fd7d55e9d40da58000ba2afbc316d37f50
Joshua Samberg <samberg@meso.net>
2009-12-10 13:25:52 +0000

    svn path=/plugins/c#/Transform/Cons/trunk/; revision=3755

---
+++ vvvv45/addonpack/src/nodes/plugins/Transform/Cons/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Information about this assembly is defined by the following
+// attributes.
+//
+// change them to the information which is associated with the assembly
+// you compile.
+
+[assembly: AssemblyTitle("ConsTransform")]
+[assembly: AssemblyDescription("A node to concatenate spreads of Transforms")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Iceberg")]
+[assembly: AssemblyProduct("ConsTransform")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// This sets the default COM visibility of types in the assembly to invisible.
+// If you need to expose a type to COM, use [ComVisible(true)] on that type.
+[assembly: ComVisible(false)]
+
+// The assembly version has following format :
+//
+// Major.Minor.Build.Revision
+//
+// You can specify all values by your own or you can build default build and revision
+// numbers with the '*' character (the default):
+
+[assembly: AssemblyVersion("1.0.*")]
