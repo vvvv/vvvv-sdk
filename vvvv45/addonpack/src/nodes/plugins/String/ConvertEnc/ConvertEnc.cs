880085a3a4c2076cefc509f36834176c99e81c50
woei <woei@refect.net>
2009-12-22 00:51:18 +0000

    svn path=/plugins/c#/String/ConvertEnc/trunk/; revision=3802

---
+++ vvvv45/addonpack/src/nodes/plugins/String/ConvertEnc/ConvertEnc.cs
@@ -0,0 +1,260 @@
+#region licence/info
+
+//////project name
+//ConvertEnc
+
+//////description
+//converts strings between ANSI and UTF8
+
+//////licence
+//GNU Lesser General Public License (LGPL)
+//english: http://www.gnu.org/licenses/lgpl.html
+//german: http://www.gnu.de/lgpl-ger.html
+
+//////language/ide
+//C# sharpdevelop 
+
+//////dependencies
+//VVVV.PluginInterfaces.V1;
+
+//////initial author
+//woei
+
+#endregion licence/info
+
+//use what you need
+using System;
+using System.Text;
+using VVVV.PluginInterfaces.V1;
+
+//the vvvv node namespace
+namespace VVVV.Nodes
+{
+	
+	//class definition
+	public class ConvertEnc: IPlugin, IDisposable
+    {	          	
+    	#region field declaration
+    	
+    	//the host (mandatory)
+    	private IPluginHost FHost; 
+    	// Track whether Dispose has been called.
+   		private bool FDisposed = false;
+
+    	//input pin declaration
+    	private IStringIn FInput;
+    	private IEnumIn FSrc;
+    	private IEnumIn FDst;
+    	
+    	//output pin declaration
+    	private IStringOut FOutput;
+
+    	
+    	#endregion field declaration
+       
+    	#region constructor/destructor
+    	
+        public ConvertEnc()
+        {
+			//the nodes constructor
+			//nothing to declare for this node
+		}
+        
+        // Implementing IDisposable's Dispose method.
+        // Do not make this method virtual.
+        // A derived class should not be able to override this method.
+        public void Dispose()
+        {
+        	Dispose(true);
+        	// Take yourself off the Finalization queue
+        	// to prevent finalization code for this object
+        	// from executing a second time.
+        	GC.SuppressFinalize(this);
+        }
+        
+        // Dispose(bool disposing) executes in two distinct scenarios.
+        // If disposing equals true, the method has been called directly
+        // or indirectly by a user's code. Managed and unmanaged resources
+        // can be disposed.
+        // If disposing equals false, the method has been called by the
+        // runtime from inside the finalizer and you should not reference
+        // other objects. Only unmanaged resources can be disposed.
+        protected virtual void Dispose(bool disposing)
+        {
+        	// Check to see if Dispose has already been called.
+        	if(!FDisposed)
+        	{
+        		if(disposing)
+        		{
+        			// Dispose managed resources.
+        		}
+        		// Release unmanaged resources. If disposing is false,
+        		// only the following code is executed.
+	        	
+        		FHost.Log(TLogType.Debug, "Convert (String) is being deleted");
+        		
+        		// Note that this is not thread safe.
+        		// Another thread could start disposing the object
+        		// after the managed resources are disposed,
+        		// but before the disposed flag is set to true.
+        		// If thread safety is necessary, it must be
+        		// implemented by the client.
+        	}
+        	FDisposed = true;
+        }
+
+        // Use C# destructor syntax for finalization code.
+        // This destructor will run only if the Dispose method
+        // does not get called.
+        // It gives your base class the opportunity to finalize.
+        // Do not provide destructors in types derived from this class.
+        ~ConvertEnc()
+        {
+        	// Do not re-create Dispose clean-up code here.
+        	// Calling Dispose(false) is optimal in terms of
+        	// readability and maintainability.
+        	Dispose(false);
+        }
+        #endregion constructor/destructor
+        
+        #region node name and infos
+       
+        //provide node infos 
+        private static IPluginInfo FPluginInfo;
+        public static IPluginInfo PluginInfo
+	    {
+	        get 
+	        {
+	        	if (FPluginInfo == null)
+				{
+					//fill out nodes info
+					//see: http://www.vvvv.org/tiki-index.php?page=Conventions.NodeAndPinNaming
+					FPluginInfo = new PluginInfo();
+					
+					//the nodes main name: use CamelCaps and no spaces
+					FPluginInfo.Name = "Convert";
+					//the nodes category: try to use an existing one
+					FPluginInfo.Category = "String";
+					//the nodes version: optional. leave blank if not
+					//needed to distinguish two nodes of the same name and category
+					FPluginInfo.Version = "";
+					
+					//the nodes author: your sign
+					FPluginInfo.Author = "woei";
+					//describe the nodes function
+					FPluginInfo.Help = "converts strings between ANSI and UTF8";
+					//specify a comma separated list of tags that describe the node
+					FPluginInfo.Tags = "";
+					
+					//give credits to thirdparty code used
+					FPluginInfo.Credits = "";
+					//any known problems?
+					FPluginInfo.Bugs = "";
+					//any known usage of the node that may cause troubles?
+					FPluginInfo.Warnings = "";
+					
+					//leave below as is
+					System.Diagnostics.StackTrace st = new System.Diagnostics.StackTrace(true);
+					System.Diagnostics.StackFrame sf = st.GetFrame(0);
+					System.Reflection.MethodBase method = sf.GetMethod();
+					FPluginInfo.Namespace = method.DeclaringType.Namespace;
+					FPluginInfo.Class = method.DeclaringType.Name;
+					//leave above as is
+				}
+				return FPluginInfo;
+	        }
+		}
+
+        public bool AutoEvaluate
+        {
+        	//return true if this node needs to calculate every frame even if nobody asks for its output
+        	get {return false;}
+        }
+        
+        #endregion node name and infos
+        
+      	#region pin creation
+        
+        //this method is called by vvvv when the node is created
+        public void SetPluginHost(IPluginHost Host)
+	    {
+        	//assign host
+	    	FHost = Host;
+
+	    	//create inputs
+	    	FHost.CreateStringInput("Input", TSliceMode.Dynamic, TPinVisibility.True, out FInput);
+	    	FInput.SetSubType("vvvv", false);	
+
+	    	FHost.CreateEnumInput("Source Encoding", TSliceMode.Dynamic, TPinVisibility.True, out FSrc);
+	    	FSrc.SetSubType("CharEncoding");
+	    	
+	    	FHost.CreateEnumInput("Destination Encoding", TSliceMode.Dynamic, TPinVisibility.True, out FDst);
+	    	FDst.SetSubType("CharEncoding");
+	    	
+	    	//create outputs	    	
+	    	FHost.CreateStringOutput("Output", TSliceMode.Dynamic, TPinVisibility.True, out FOutput);
+	    	FOutput.SetSubType("", false);
+        }
+
+        #endregion pin creation
+        
+        #region mainloop
+        
+        public void Configurate(IPluginConfig Input)
+        {
+        	//nothing to configure in this plugin
+        	//only used in conjunction with inputs of type cmpdConfigurate
+        }
+        
+        //here we go, thats the method called by vvvv each frame
+        //all data handling should be in here
+        public void Evaluate(int SpreadMax)
+        {     	
+        	//if any of the inputs has changed
+        	//recompute the outputs
+        	if (FInput.PinIsChanged || FSrc.PinIsChanged || FDst.PinIsChanged)
+        	{	
+	        	FOutput.SliceCount = SpreadMax;	
+	        	
+	        	string s;
+	        	
+	        	int src, dst;
+	        	Encoding srcEnc = Encoding.Default;
+	        	Encoding dstEnc = Encoding.Default;
+        		//loop for all slices
+        		for (int i=0; i<SpreadMax; i++)
+        		{		
+        			//read data from inputs
+        			FInput.GetString(i, out s);
+					FSrc.GetOrd(i, out src);
+					
+					switch (src)
+					{
+						case (1):
+							srcEnc = Encoding.UTF8;
+							break;
+//						case (2):
+//							srcEnc = Encoding.Unicode;
+//							break;
+					}
+					
+					FDst.GetOrd(i, out dst);
+					switch (dst)
+					{
+						case (1):
+							dstEnc = Encoding.UTF8;
+							break;
+//						case (2):
+//							dstEnc = Encoding.Unicode;
+//							break;
+					}
+					
+					FOutput.SetString(i, srcEnc.GetString(dstEnc.GetBytes(s)));
+
+        		}
+        	}      	
+        }
+             
+        #endregion mainloop  
+	}
+}
