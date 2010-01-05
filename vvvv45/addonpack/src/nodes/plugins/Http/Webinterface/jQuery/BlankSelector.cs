676e25ad7a1bc0ce6251f4beaa9666ea783f5a24
Nils Buhlert <nils.buhlert@gmx.de>
2010-01-05 13:42:01 +0000

    svn path=/plugins/c#/Http/Webinterface/trunk/; revision=3860

---
+++ vvvv45/addonpack/src/nodes/plugins/Http/Webinterface/jQuery/BlankSelector.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VVVV.Webinterface.jQuery
+{
+    class BlankSelector:JavaScriptObject
+    {
+
+        private string FName;
+
+        public BlankSelector(string Name)
+        {
+            FName = Name;
+        }
+
+        protected override string GenerateObjectScript(int indentSteps, bool breakInternalLines, bool breakAfter)
+        {
+            return FName;
+        }
+    }
+}
