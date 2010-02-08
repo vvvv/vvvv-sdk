c686564b760505fdf19ce830ff2dc9d525105ef9
Elias Holzer <elias@vvvv.org>
2010-02-08 21:45:17 +0000

    Made sure dispose method stops throwing NullPointerException if FHost not set.
    svn path=/plugins/c#/_Samples/_PluginThreadedTemplate/trunk/; revision=4035

--- vvvv45/addonpack/src/nodes/plugins/_Samples/_PluginThreadedTemplate/PluginThreadedTemplate.cs
+++ vvvv45/addonpack/src/nodes/plugins/_Samples/_PluginThreadedTemplate/PluginThreadedTemplate.cs
@@ -97,7 +97,8 @@ namespace VVVV.Nodes
         		// Release unmanaged resources. If disposing is false,
         		// only the following code is executed.
 	        	
-        		FHost.Log(TLogType.Debug, "PluginTemplate is being deleted");
+        		if (FHost != null)
+        			FHost.Log(TLogType.Debug, "PluginTemplate is being deleted");
         		
         		// Note that this is not thread safe.
         		// Another thread could start disposing the object
