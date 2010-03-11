6fded74caa7c252032e193efc39a3d3c22a511ff
Matthias Zauner <sagishi@zive.at>
2010-03-11 10:40:36 +0000

    * fixed syntax errors * changed decomposition method (needs cleanup)
    svn path=/plugins/c#/_SharedSources/trunk/; revision=4196

--- vvvv45/src/core/SharedSources/SlimDX/SlimDXUtils.cs
+++ vvvv45/src/core/SharedSources/SlimDX/SlimDXUtils.cs
@@ -83,9 +83,9 @@ namespace VVVV.Shared.VSlimDX
 			}
 			else
 			{
-				scale = new Vector3D();
-				rotationQuaternion = new Vector4D();
-				translation = new Vector3D();
+				scale = new Vector3D(0);
+				rotationQuaternion = new Vector4D(0);
+				translation = new Vector3D(0);
 				return false;
 			}
 		}
@@ -101,9 +101,12 @@ namespace VVVV.Shared.VSlimDX
 				scale.x = s.X;
 				scale.y = s.Y;
 				scale.z = s.Z;
-				rotation.x = Math.Atan2(2*(r.X * r.Y + r.Z * r.W), 1 - 2 * (r.Y * r.Y + r.Z * r.Z));
-				rotation.y = Math.Asin(2 * (r.X * r.Z - r.W * r.Y));
-				rotation.z = Math.Atan2(2 * (r.X * r.W + r.Y * r.Z), 1 - 2 * (r.Z * r.Z + r.W * r.W));
+				
+				SlimDX.Vector3 euler = QuaternionToEulerAngleVector3(r);
+				rotation.x = euler.X;
+				rotation.y = euler.Y;
+				rotation.z = euler.Z;
+				
 				translation.x = t.X;
 				translation.y = t.Y;
 				translation.z = t.Z;
@@ -112,11 +115,57 @@ namespace VVVV.Shared.VSlimDX
 			}
 			else
 			{
-				scale = new Vector3D();
-				rotation = new Vector3D();
-				translation = new Vector3D();
+				scale = new Vector3D(0);
+				rotation = new Vector3D(0);
+				translation = new Vector3D(0);
 				return false;
 			}
 		}
+		
+		public static Vector3 AngleTo(Vector3 from, Vector3 location)
+		{
+		    Vector3 angle = new Vector3(); 
+		    Vector3 v3 = Vector3.Normalize(location - from); 
+		 
+		    angle.X = (float)Math.Asin(v3.Y);
+		    angle.Y = (float)Math.Atan2((double)-v3.X, (double)-v3.Z);
+		 
+		    return angle;
+		}
+
+		
+		public static Vector3 QuaternionToEulerAngleVector3(Quaternion rotation)
+		{
+		    Vector3 rotationaxes = new Vector3();
+		    Vector4 forward4 = Vector3.Transform(new Vector3(0, 0, -1), rotation);
+		    Vector4 up4 = Vector3.Transform(new Vector3(0, 1, 0), rotation);
+		    Vector3 forward = new Vector3(forward4.X, forward4.Y, forward4.Z);
+		    Vector3 up = new Vector3(up4.X, up4.Y, up4.Z);
+		 
+		    rotationaxes = AngleTo(new Vector3(), forward);
+		 
+		    if (rotationaxes.X == Math.PI/2)
+		    {
+		        rotationaxes.Y = (float)Math.Atan2((double)up.X, (double)up.Z);
+		        rotationaxes.Z = 0;
+		    }
+		    else if (rotationaxes.X == -Math.PI/2)
+		    {
+		        rotationaxes.Y = (float)Math.Atan2((double)-up.X, (double)-up.Z);
+		        rotationaxes.Z = 0;
+		    }
+		    else
+		    {
+		        up4 = Vector3.Transform(up, Matrix.RotationY(-rotationaxes.Y));
+		        up = new Vector3(up4.X, up4.Y, up4.Z);
+		        up4 = Vector3.Transform(up, Matrix.RotationX(-rotationaxes.X));
+		        up = new Vector3(up4.X, up4.Y, up4.Z);
+		 
+		        rotationaxes.Z = (float)Math.Atan2((double)-up.X, (double)up.Y);
+		    } 
+		 
+		    return rotationaxes;
+		}
+
 	}
 }
