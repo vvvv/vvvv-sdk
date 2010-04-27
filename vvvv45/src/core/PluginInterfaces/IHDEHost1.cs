5c5d3e6f76eba3074613c3a5f5abec9f1d986a44
joreg <joreg@vvvv.org>
2010-04-27 15:54:30 +0000

    removed Log from IHDEHost
    svn path=/plugins/c#/_PluginInterfaces/branches/DynamicNodes/; revision=4671

--- vvvv45/src/core/PluginInterfaces/IHDEHost1.cs
+++ vvvv45/src/core/PluginInterfaces/IHDEHost1.cs
@@ -13,12 +13,5 @@ namespace VVVV.PluginInterfaces.V1
 		/// Provides access to the Solution hosted by this IHDEHost.
 		/// </summary>
 		ISolution Solution { get; }
-		
-		/// <summary>
-		/// Allows a HDE plugin to write messages to a console on the host (ie. Renderer (TTY) in vvvv).
-		/// </summary>
-		/// <param name="Type">The type of message. Depending on the setting of this parameter the HDEHost can handle messages differently.</param>
-		/// <param name="Message">The message to be logged.</param>
-		void Log(TLogType Type, string Message);
 	}
 }
