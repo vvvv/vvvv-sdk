b2b8f05c3fa46b405f8844a98328c12cbb00e2a3
Elias Holzer <elias@vvvv.org>
2010-06-29 20:07:33 +0000

    Added new static class PathUtils. Provides method to get relative path between two absolute paths.
    svn path=/plugins/c#/_Utils/trunk/; revision=4799

---
+++ common/src/core/Utils/PathUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VVVV.Utils
+{
+	/// <summary>
+	/// Utility methods missing in the System.IO.Path class.
+	/// </summary>
+	public static class PathUtils
+	{
+		/// <summary>
+		/// Returns the relative path to reach toPath from fromPath.
+		/// </summary>
+		public static string MakeRelativePath(string fromPath, string toPath)
+		{
+			var fromUri = new Uri(fromPath);
+			var toUri = new Uri(toPath);
+			
+			var relativeUri = fromUri.MakeRelativeUri(toUri);
+			return relativeUri.ToString().Replace('/', '\\');
+		}
+	}
+}
