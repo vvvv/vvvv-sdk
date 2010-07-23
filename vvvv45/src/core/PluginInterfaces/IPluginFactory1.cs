25bac2d2ddeac72e6e4eecb55716ea6b887f19ca
Elias Holzer <elias@vvvv.org>
2010-07-23 23:05:10 +0000

    cleanup
    svn path=/plugins/c#/_PluginInterfaces/branches/DynamicNodes/; revision=4867

--- vvvv45/src/core/PluginInterfaces/IPluginFactory1.cs
+++ vvvv45/src/core/PluginInterfaces/IPluginFactory1.cs
@@ -12,21 +12,17 @@ namespace VVVV.PluginInterfaces.V2
 	/// <summary>
 	/// General addon factory
 	/// </summary>
-	[Guid("36D732BD-7F2C-4ECF-91E1-5961B213BBF3"),
-	 InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
 	public interface IAddonFactory
 	{
 	    event NodeInfoEventHandler NodeInfoAdded;
 	    event NodeInfoEventHandler NodeInfoRemoved;
-	    bool ExtractNodeInfos(string filename, out INodeInfo[] nodeInfos);
+	    IEnumerable<INodeInfo> ExtractNodeInfos(string filename);
 	    void StartWatching();
 	}
 	
 	/// <summary>
 	/// A factory which handles plugin nodes
 	/// </summary>
-	[Guid("445266A3-02A6-41F3-9055-50083FCE1CBC"),
-	 InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
 	public interface IPluginFactory : IAddonFactory
 	{		
 	    bool Create(INodeInfo nodeInfo, IPluginHost host, out IPluginBase plugin);
