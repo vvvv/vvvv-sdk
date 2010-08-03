d2a6de8aab4989715ac98a6c3435b679c95244b8
Tebjan Halm <tebjan@vvvv.org>
2010-08-03 19:38:52 +0000

    fixed set host bug in EnumManager.cs
    svn path=/plugins/c#/_PluginInterfaces/branches/DynamicNodes/; revision=4902

--- vvvv45/src/core/PluginInterfaces/V2/EnumManager.cs
+++ vvvv45/src/core/PluginInterfaces/V2/EnumManager.cs
@@ -1,30 +1,53 @@
 ﻿using System;
 using System.ComponentModel.Composition;
 using VVVV.PluginInterfaces.V1;
+using VVVV.Utils.VMath;
 
 namespace VVVV.PluginInterfaces.V2
 {
 	/// <summary>
-	/// Represents a Key/Value Pair of an Enum
+	/// Represents a Key/Value Pair of an enum.
 	/// </summary>
 	public class EnumEntry
 	{
+		/// <summary>
+		/// The string representation of this entry.
+		/// </summary>
 		public string Name { get; private set; }
+		
+		/// <summary>
+		/// The index of this entry in the enum.
+		/// </summary>
 		public int Index { get; private set; }
-
+		
+		/// <summary>
+		/// The name of the enum to which this entry belongs.
+		/// </summary>
+		public string EnumName { get; private set; }
+		
+		/// <summary>
+		/// Creates an EnumEntry from a specific enum.
+		/// </summary>
+		/// <param name="enumName">The enum name</param>
+		/// <param name="index">Position in enum</param>
 		public EnumEntry(string enumName, int index)
 		{
 			Index = index;
 			Name = EnumManager.GetEnumEntryString(enumName, index);
+			EnumName = enumName;
 		}
 
-		//EnumEntry can be used like a string
+		/// <summary>
+		/// EnumEntry can be used like a string
+		/// </summary>
 		public static implicit operator string(EnumEntry e)
 		{
 			return e.Name;
 		}
 
-		//EnumEntry can be used like an int
+		/// <summary>
+		/// EnumEntry can be used like an int
+		/// </summary>
 		public static implicit operator int(EnumEntry e)
 		{
 			return e.Index;
@@ -32,22 +55,71 @@ namespace VVVV.PluginInterfaces.V2
 	}
 
 	/// <summary>
-	/// Manages the global Enums of the Host.
+	/// Manages the global Enums of the HDEHost.
 	/// </summary>
 	public static class EnumManager
 	{
-		[Import]
+		//the host
 		private static IHDEHost FHost;
-
+		public static void SetHDEHost(IHDEHost host)
+		{
+			FHost = host;
+		}
+		
 		/// <summary>
 		/// Allows a plugin to create/update an Enum with vvvv
 		/// </summary>
-		/// <param name="enumName">The Enums name.</param>
-		/// <param name="defaultEntry">The Enums default value.</param>
+		/// <param name="enumName">The enums name.</param>
+		/// <param name="defaultEntry">The enums default value.</param>
 		/// <param name="enumEntries">An array of strings that specify the enums entries.</param>
 		public static void UpdateEnum(string enumName, string defaultEntry, string[] enumEntries)
 		{
 			FHost.UpdateEnum(enumName, defaultEntry, enumEntries);
+		}
+		
+		/// <summary>
+		/// Adds an enum entry to the end of an enum.
+		/// </summary>
+		/// <param name="enumName">The enums name.</param>
+		/// <param name="entryName">The new enum entry.</param>
+		public static void AddEntry(string enumName, string entryName)
+		{
+			var count = GetEnumEntryCount(enumName);
+			var entries = new string[count + 1];
+			
+			for (int i = 0; i<count; i++) 
+			{
+				entries[i] = GetEnumEntryString(enumName, i);
+			}
+			
+			entries[count] = entryName;
+			
+			UpdateEnum(enumName, entries[0], entries);
+		}
+		
+		/// <summary>
+		/// Adds an enum entry at a specific position of an enum.
+		/// </summary>
+		/// <param name="enumName">The enums name.</param>
+		/// <param name="entryName">The new enum entry.</param>
+		/// <param name="index">Position of the new entry.</param>
+		public static void AddEntry(string enumName, string entryName, int index)
+		{
+			var count = GetEnumEntryCount(enumName);
+			
+			index = VMath.Clamp(index, 0, count);
+			
+			var entries = new string[count + 1];
+			
+			for (int i = 0; i < count; i++) 
+			{
+				int offset = (i < index) ? 0 : 1;
+				entries[i + offset] = GetEnumEntryString(enumName, i);
+			}
+			
+			entries[index] = entryName;
+			
+			UpdateEnum(enumName, entries[0], entries);
 		}
 
 		/// <summary>
