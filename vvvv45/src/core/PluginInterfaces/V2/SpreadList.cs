4a5f377f7a3ad384d4c1f1699299418f35cc1505
Tebjan Halm <tebjan@vvvv.org>
2010-08-04 23:31:43 +0000

    added spread list framework, implemented InputSpreadList
    svn path=/plugins/c#/_PluginInterfaces/branches/DynamicNodes/; revision=4912

---
+++ vvvv45/src/core/PluginInterfaces/V2/SpreadList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using VVVV.PluginInterfaces.V1;
+
+namespace VVVV.PluginInterfaces.V2
+{
+	/// <summary>
+	/// base class for spread lists
+	/// </summary>
+	public class SpreadList<T> : ISpreadList<T>
+	{
+		protected Pin<T>[] FPins;
+		protected IPluginHost FHost;
+		protected PinAttribute FAttribute;
+		
+		public SpreadList(IPluginHost host, PinAttribute attribute)
+		{
+			FHost = host;
+			FAttribute = attribute;
+			FPins = new Pin<T>[0];
+		}
+			
+		public ISpread<T>[] Spreads 
+		{ 
+			get
+			{
+				return FPins as ISpread<T>[];
+			}
+		}
+	}
+}
