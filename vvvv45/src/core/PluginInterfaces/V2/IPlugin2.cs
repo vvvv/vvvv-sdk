64adcc29a011f9e2ac84277bcced77e52be60753
joreg <joreg@vvvv.org>
2010-08-17 15:12:45 +0000

    introduced IQueryDelete, modified IVVVVInternalHost.Clone()
    svn path=/plugins/c#/_PluginInterfaces/branches/DynamicNodes/; revision=4957

--- vvvv45/src/core/PluginInterfaces/V2/IPlugin2.cs
+++ vvvv45/src/core/PluginInterfaces/V2/IPlugin2.cs
@@ -13,7 +13,7 @@ namespace VVVV.PluginInterfaces.V2
 {
 	#region basic interfaces
 	/// <summary>
-	/// Interface that identifies HDE plugins 
+	/// Interface that identifies HDE plugins. 
 	/// </summary>
 	[Guid("69BD2770-3E93-4088-8622-0D39DE2DB013"),
 	 InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
@@ -21,7 +21,7 @@ namespace VVVV.PluginInterfaces.V2
 	{}
 	
 	/// <summary>
-	/// Basic interface to provide a plugin with an Evaluate function
+	/// Basic interface to provide a plugin with an Evaluate function.
 	/// </summary>
 	[Guid("5BDEF445-5734-427C-BD9C-A69809277799"),
 	 InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
@@ -34,6 +34,19 @@ namespace VVVV.PluginInterfaces.V2
 		/// <param name="SpreadMax">The maximum SliceCount of all of the plugins inputs, which would typically be used
 		/// to adjust the SliceCounts of all outputs accordingly.</param>
 		void Evaluate(int SpreadMax);
+	}
+	
+	/// <summary>
+	/// Optional interface to provide a plugin with the possibility to prohibit its own deletion.
+	/// </summary>
+	[Guid("E66B6358-4850-41D3-B4F1-1C09F0557DD0"),
+	 InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+	public interface IQueryDelete
+	{
+		/// <summary>
+		/// Called by the PluginHost when the plugin is about to be deleted. The plugin can return FALSE to prohibit the deletion. 
+		/// </summary>
+		bool DeleteMe();
 	}
 	#endregion basic interfaces
 }
