d7269e123e48d774024235c4ee9de60cae96aada
Tebjan Halm <tebjan@vvvv.org>
2010-08-20 13:34:24 +0000

    consistent naming, renamed Observable to Diff
    svn path=/plugins/c#/_PluginInterfaces/branches/DynamicNodes/; revision=5042

--- vvvv45/src/core/PluginInterfaces/V2/DiffInputWrapperPin.cs
+++ vvvv45/src/core/PluginInterfaces/V2/DiffInputWrapperPin.cs
@@ -20,39 +20,39 @@ namespace VVVV.PluginInterfaces.V2
 			var type = typeof(T);
 			
 			if (type == typeof(double))
-				FPin = new DiffDoubleInputPin(host, attribute) as DiffPin<T>;
+				DiffPin = new DiffDoubleInputPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(float))
-				FPin = new DiffFloatInputPin(host, attribute) as DiffPin<T>;
+				DiffPin = new DiffFloatInputPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(int))
-				FPin = new DiffIntInputPin(host, attribute) as DiffPin<T>;
+				DiffPin = new DiffIntInputPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(bool))
-				FPin = new DiffBoolInputPin(host, attribute) as DiffPin<T>;
+				DiffPin = new DiffBoolInputPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(string))
-				FPin = new StringInputPin(host, attribute) as DiffPin<T>;
+				DiffPin = new StringInputPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(RGBAColor))
-				FPin = new ColorInputPin(host, attribute) as DiffPin<T>;
+				DiffPin = new ColorInputPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(Matrix4x4))
-				FPin = new Matrix4x4InputPin(host, attribute) as DiffPin<T>;
+				DiffPin = new Matrix4x4InputPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(Matrix))
-				FPin = new SlimDXMatrixInputPin(host, attribute) as DiffPin<T>;
+				DiffPin = new SlimDXMatrixInputPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(Vector2D))
-				FPin = new Vector2DInputPin(host, attribute) as DiffPin<T>;
+				DiffPin = new Vector2DInputPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(Vector3D))
-				FPin = new Vector3DInputPin(host, attribute) as DiffPin<T>;
+				DiffPin = new Vector3DInputPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(Vector4D))
-				FPin = new Vector4DInputPin(host, attribute) as DiffPin<T>;
+				DiffPin = new Vector4DInputPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(Vector2))
-				FPin = new Vector2InputPin(host, attribute) as DiffPin<T>;
+				DiffPin = new Vector2InputPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(Vector3))
-				FPin = new Vector3InputPin(host, attribute) as DiffPin<T>;
+				DiffPin = new Vector3InputPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(Vector4))
-				FPin = new Vector4InputPin(host, attribute) as DiffPin<T>;
+				DiffPin = new Vector4InputPin(host, attribute) as DiffPin<T>;
 			else if (type.BaseType == typeof(Enum))
-				FPin = new EnumInputPin<T>(host, attribute) as DiffPin<T>;
+				DiffPin = new EnumInputPin<T>(host, attribute) as DiffPin<T>;
 			else if (type == typeof(EnumEntry))
-				FPin = new DynamicEnumInputPin(host, attribute) as DiffPin<T>;
+				DiffPin = new DynamicEnumInputPin(host, attribute) as DiffPin<T>;
 			else
-				FPin = new GenericInputPin<T>(host, attribute) as DiffPin<T>;
+				DiffPin = new GenericInputPin<T>(host, attribute) as DiffPin<T>;
 		}
 	}
 }
