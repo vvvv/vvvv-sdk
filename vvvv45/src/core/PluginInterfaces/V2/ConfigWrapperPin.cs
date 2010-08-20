27d9a70251536f3c9f701b4a51b7f85e21eed3df
Tebjan Halm <tebjan@vvvv.org>
2010-08-20 13:20:45 +0000

    consistent naming, renamed Observable to Diff
    svn path=/plugins/c#/_PluginInterfaces/branches/DynamicNodes/; revision=5037

--- vvvv45/src/core/PluginInterfaces/V2/ConfigWrapperPin.cs
+++ vvvv45/src/core/PluginInterfaces/V2/ConfigWrapperPin.cs
@@ -11,7 +11,7 @@ using VVVV.Utils.VMath;
 
 namespace VVVV.PluginInterfaces.V2
 {
-	public class ConfigWrapperPin<T> : ObservableWrapperPin<T>
+	public class ConfigWrapperPin<T> : DiffWrapperPin<T>
 	{
 		public ConfigWrapperPin(IPluginHost host, ConfigAttribute attribute)
 		{
@@ -20,33 +20,33 @@ namespace VVVV.PluginInterfaces.V2
 			var type = typeof(T);
 			
 			if (type == typeof(double))
-				ObservablePin = new DoubleConfigPin(host, attribute) as ObservablePin<T>;
+				FPin = new DoubleConfigPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(float))
-				ObservablePin = new FloatConfigPin(host, attribute) as ObservablePin<T>;
+				FPin = new FloatConfigPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(int))
-				ObservablePin = new IntConfigPin(host, attribute) as ObservablePin<T>;
+				FPin = new IntConfigPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(bool))
-				ObservablePin = new BoolConfigPin(host, attribute) as ObservablePin<T>;
+				FPin = new BoolConfigPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(string))
-				ObservablePin = new StringConfigPin(host, attribute) as ObservablePin<T>;
+				FPin = new StringConfigPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(RGBAColor))
-				ObservablePin = new ColorConfigPin(host, attribute) as ObservablePin<T>;
+				FPin = new ColorConfigPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(Vector2D))
-				ObservablePin = new Vector2DConfigPin(host, attribute) as ObservablePin<T>;
+				FPin = new Vector2DConfigPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(Vector3D))
-				ObservablePin = new Vector3DConfigPin(host, attribute) as ObservablePin<T>;
+				FPin = new Vector3DConfigPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(Vector4D))
-				ObservablePin = new Vector4DConfigPin(host, attribute) as ObservablePin<T>;
+				FPin = new Vector4DConfigPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(Vector2))
-				ObservablePin = new Vector2ConfigPin(host, attribute) as ObservablePin<T>;
+				FPin = new Vector2ConfigPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(Vector3))
-				ObservablePin = new Vector3ConfigPin(host, attribute) as ObservablePin<T>;
+				FPin = new Vector3ConfigPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(Vector4))
-				ObservablePin = new Vector4ConfigPin(host, attribute) as ObservablePin<T>;
+				FPin = new Vector4ConfigPin(host, attribute) as DiffPin<T>;
 			else if (type.BaseType == typeof(Enum))
-				ObservablePin = new EnumConfigPin<T>(host, attribute) as ObservablePin<T>;
+				FPin = new EnumConfigPin<T>(host, attribute) as DiffPin<T>;
 			else if (type == typeof(EnumEntry))
-				ObservablePin = new DynamicEnumConfigPin(host, attribute) as ObservablePin<T>;
+				FPin = new DynamicEnumConfigPin(host, attribute) as DiffPin<T>;
 			else
 				throw new NotImplementedException(string.Format("ConfigPin of type '{0}' not supported.", type));
 		}
