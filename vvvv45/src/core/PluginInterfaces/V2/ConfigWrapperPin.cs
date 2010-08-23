382b7939f4f75881e7238f8cfa3c1dc88f2688ac
Elias Holzer <elias@vvvv.org>
2010-08-23 14:58:07 +0000

    Fixed null pointer exception in ConfigWrapperPin.
    svn path=/plugins/c#/_PluginInterfaces/branches/DynamicNodes/; revision=5051

--- vvvv45/src/core/PluginInterfaces/V2/ConfigWrapperPin.cs
+++ vvvv45/src/core/PluginInterfaces/V2/ConfigWrapperPin.cs
@@ -20,33 +20,33 @@ namespace VVVV.PluginInterfaces.V2
 			var type = typeof(T);
 			
 			if (type == typeof(double))
-				FPin = new DoubleConfigPin(host, attribute) as DiffPin<T>;
+				DiffPin = new DoubleConfigPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(float))
-				FPin = new FloatConfigPin(host, attribute) as DiffPin<T>;
+				DiffPin = new FloatConfigPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(int))
-				FPin = new IntConfigPin(host, attribute) as DiffPin<T>;
+				DiffPin = new IntConfigPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(bool))
-				FPin = new BoolConfigPin(host, attribute) as DiffPin<T>;
+				DiffPin = new BoolConfigPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(string))
-				FPin = new StringConfigPin(host, attribute) as DiffPin<T>;
+				DiffPin = new StringConfigPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(RGBAColor))
-				FPin = new ColorConfigPin(host, attribute) as DiffPin<T>;
+				DiffPin = new ColorConfigPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(Vector2D))
-				FPin = new Vector2DConfigPin(host, attribute) as DiffPin<T>;
+				DiffPin = new Vector2DConfigPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(Vector3D))
-				FPin = new Vector3DConfigPin(host, attribute) as DiffPin<T>;
+				DiffPin = new Vector3DConfigPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(Vector4D))
-				FPin = new Vector4DConfigPin(host, attribute) as DiffPin<T>;
+				DiffPin = new Vector4DConfigPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(Vector2))
-				FPin = new Vector2ConfigPin(host, attribute) as DiffPin<T>;
+				DiffPin = new Vector2ConfigPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(Vector3))
-				FPin = new Vector3ConfigPin(host, attribute) as DiffPin<T>;
+				DiffPin = new Vector3ConfigPin(host, attribute) as DiffPin<T>;
 			else if (type == typeof(Vector4))
-				FPin = new Vector4ConfigPin(host, attribute) as DiffPin<T>;
+				DiffPin = new Vector4ConfigPin(host, attribute) as DiffPin<T>;
 			else if (type.BaseType == typeof(Enum))
-				FPin = new EnumConfigPin<T>(host, attribute) as DiffPin<T>;
+				DiffPin = new EnumConfigPin<T>(host, attribute) as DiffPin<T>;
 			else if (type == typeof(EnumEntry))
-				FPin = new DynamicEnumConfigPin(host, attribute) as DiffPin<T>;
+				DiffPin = new DynamicEnumConfigPin(host, attribute) as DiffPin<T>;
 			else
 				throw new NotImplementedException(string.Format("ConfigPin of type '{0}' not supported.", type));
 		}
