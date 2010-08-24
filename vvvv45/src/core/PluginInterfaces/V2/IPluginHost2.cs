4b5a44d76cd42333ccadaf7110dd264285ec602e
joreg <joreg@vvvv.org>
2010-08-24 22:24:03 +0000

    svn path=/AddonsCore/trunk/PluginInterfaces/; revision=5088

--- vvvv45/src/core/PluginInterfaces/V2/IPluginHost2.cs
+++ vvvv45/src/core/PluginInterfaces/V2/IPluginHost2.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 using VVVV.PluginInterfaces.V1;
 
-namespace VVVV.Interfaces
+namespace VVVV.PluginInterfaces.V2
 {
     [Guid("21230B31-1929-44F8-B8C0-03E5C2AA42EF"),
 	 InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
