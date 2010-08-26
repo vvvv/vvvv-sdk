20828076f88e9efc04d21b63e13e87bfa81ce919
Tebjan Halm <tebjan@vvvv.org>
2010-08-26 17:12:52 +0000

    copied GetCurrentTime, GetHostPath und GetNodePath to HDE host
    svn path=/AddonsCore/trunk/PluginInterfaces/; revision=5127

--- vvvv45/src/core/PluginInterfaces/V2/IHDE2.cs
+++ vvvv45/src/core/PluginInterfaces/V2/IHDE2.cs
@@ -51,6 +51,25 @@ namespace VVVV.PluginInterfaces.V2
 		/// <param name="Index">Index of the EnumEntry.</param>
 		/// <param name="EntryName">String representation of the EnumEntry.</param>
 		void GetEnumEntry(string EnumName, int Index, out string EntryName);
+		
+		/// <summary>
+		/// Returns the current time which the plugin should use if it does timebased calculations.
+		/// </summary>
+		/// <param name="CurrentTime">The hosts current time.</param>
+		void GetCurrentTime(out double CurrentTime);
+		
+		/// <summary>
+		/// Returns the absolut file path to the plugins host.
+		/// </summary>
+		/// <param name="Path">Absolut file path to the plugins host (i.e path to the patch the plugin is placed in, in vvvv).</param>
+		void GetHostPath(out string Path);
+		
+		/// <summary>
+		/// Returns a slash-separated path of node IDs that uniquely identifies this node in the vvvv graph.
+		/// </summary>
+		/// <param name="UseDescriptiveNames">If TRUE descriptive node names are used where available instead of the node ID.</param>
+		/// <param name="Path">Slash-separated path of node IDs that uniquely identifies this node in the vvvv graph.</param>
+		void GetNodePath(bool UseDescriptiveNames, out string Path);
 	    
 	}
 	#endregion IHDEHost
