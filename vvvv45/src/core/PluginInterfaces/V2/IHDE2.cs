e2d7096e0a30d65a12ea95de082d8738708dc261
joreg <joreg@vvvv.org>
2010-11-06 19:22:48 +0000

    IHDEHost ShowPatchOfNode is now ShowPatch
    svn path=/AddonsCore/trunk/PluginInterfaces/; revision=5390

--- vvvv45/src/core/PluginInterfaces/V2/IHDE2.cs
+++ vvvv45/src/core/PluginInterfaces/V2/IHDE2.cs
@@ -84,10 +84,10 @@ namespace VVVV.PluginInterfaces.V2
 		void SelectNodes(INode[] nodes);
 		
 		/// <summary>
-		/// Opens the patch of the given node.
+		/// Opens the patch of the given node if this node is a patch.
 		/// </summary>
 		/// <param name="node">The node whose patch to open.</param>
-		void ShowPatchOfNode(INode node);
+		void ShowPatch(INode node);
 		
 		/// <summary>
 		/// Opens the help-patch of the given nodeinfo.
