fe12164d1fd8e73c8d7b5462020f8674262497f6
Elias Holzer <elias@vvvv.org>
2010-12-03 18:01:17 +0000

    missed to commit IPinFactory
    svn path=/AddonsCore/trunk/PluginInterfaces/; revision=5487

---
+++ vvvv45/src/core/PluginInterfaces/V2/IPinFactory.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+namespace VVVV.PluginInterfaces.V2
+{
+	public delegate ISpread<T> PinCreator<T>(IPluginHost2 host, PinAttribute attribute);
+	
+	public interface IPinFactory
+	{
+		ISpread<T> CreateSpread<T>(PinAttribute attribute);
+		IDiffSpread<T> CreateDiffSpread<T>(PinAttribute attribute);
+		Pin<T> CreatePin<T>(PinAttribute attribute);
+		DiffPin<T> CreateDiffPin<T>(PinAttribute attribute);
+		
+		void RegisterPin<T>(PinCreator<T> creator);
+	}
+}
