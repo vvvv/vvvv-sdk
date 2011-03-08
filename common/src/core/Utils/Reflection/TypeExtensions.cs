599da6b5236761d3f2950a667c551b0372aa8a90
Tebjan Halm <tebjan@vvvv.org>
2011-03-08 18:48:12 +0000

    added TypeExtensions.cs
    svn path=/AddonsCore/trunk/Utils/; revision=5822

---
+++ common/src/core/Utils/Reflection/TypeExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VVVV.Utils.Reflection
+{
+    public static class TypeExtensions
+    {
+        private static string GetName(Type t)
+        {
+            var ret = t.Name;
+
+            if (t.IsArray)
+            {
+                ret = GetName(t.GetElementType()) + "[]";
+            }
+            else if(t.IsGenericType)
+            {
+                var elems = t.GetGenericArguments();
+                var elemNames = new List<string>();
+
+                foreach (var gt in elems)
+                {
+                    elemNames.Add(GetName(gt));
+                }
+
+                ret += "<" + String.Join(", ", elemNames.ToArray()) + ">";
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Creates the full name of a type, including generics 
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public static string GetExpandedName(this Type t)
+        {
+            return GetName(t);
+        }
+    }
+}
