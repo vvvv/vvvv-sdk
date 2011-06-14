9cbc287444fafe0c6d93590658946e16f1259636
Julien Vulliet <julien@mrvux.com>
2011-06-14 18:05:08 +0000

    import core engine.
    svn path=/plugins/c#/Devices/TodoMap/trunk/; revision=5917

---
+++ vvvv45/addonpack/src/nodes/plugins/Devices/TodoMap/Lib/Modules/Midi/TodoMidiDeviceManager.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sanford.Multimedia.Midi;
+
+namespace VVVV.TodoMap.Lib.Modules.Midi
+{
+    //Abstraction for a midi device, some can have same name so also provide an index
+    public class MidiDevice
+    {
+        /// <summary>
+        /// Device Name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Device Index (if more than two with the same name)
+        /// </summary>
+        public int Index { get; set; }
+
+        /// <summary>
+        /// Device Index (In midi driver device list), -1 if doesnt not exist
+        /// </summary>
+        public int DeviceIndex { get; set; }
+
+        /// <summary>
+        /// Device Status
+        /// </summary>
+        public eTodoMidiStatus Status { get; set; }
+
+        /// <summary>
+        /// Warm starting for device
+        /// </summary>
+        public bool AutoStart { get; set; }
+    }
+
+    public class MidiDeviceList : List<MidiDevice>
+    {
+        
+    }
+
+    public class TodoMidiDeviceManager
+    {
+        private MidiDeviceList inputs = new MidiDeviceList();
+
+        private Dictionary<int, InputDevice> inputdevs = new Dictionary<int, InputDevice>();
+
+
+        public void AddDevice(string name, int index,bool autostart)
+        {
+            
+        }
+
+        public void ReScan()
+        {
+
+        }
+
+        private int CountInputByName(string name)
+        {
+            int result = 0;
+            for (int i = 0; i < InputDevice.DeviceCount; i++)
+            {
+                if (InputDevice.GetDeviceCapabilities(i).name == name) { result++; }
+            }
+
+            return result;
+        }
+    }
+}
