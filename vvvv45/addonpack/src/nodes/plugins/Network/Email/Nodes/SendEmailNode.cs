1b9c092f4862daa9e48f6779327114974470e19d
Nils Buhlert <nils.buhlert@gmx.de>
2011-07-21 12:56:35 +0000

    - made send async
    svn path=/plugins/c#/Network/Email/trunk/; revision=6060

--- vvvv45/addonpack/src/nodes/plugins/Network/Email/Nodes/SendEmailNode.cs
+++ vvvv45/addonpack/src/nodes/plugins/Network/Email/Nodes/SendEmailNode.cs
@@ -3,6 +3,9 @@ using System.ComponentModel.Composition;
 using System.Net;
 using System.Net.Mail;
 using System.Net.Mime;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
 
 using VVVV.Core.Logging;
 using VVVV.PluginInterfaces.V1;
@@ -39,25 +42,28 @@ namespace VVVV.Nodes
         [Input("Use SSL", IsSingle= true, DefaultValue=1)]
         IDiffSpread<bool> FPinInSSL;
 
-        [Input("From", DefaultString="User@gmail.com")]
+        [Input("From", IsSingle = true, DefaultString = "User@gmail.com")]
         IDiffSpread<string> FPinInFrom;
 
-        [Input("To", DefaultString = "User@gmail.com")]
+        [Input("To", IsSingle = true, DefaultString = "User@gmail.com")]
         IDiffSpread<string> FPinInTo;
 
-        [Input("Subject")]
+        [Input("Subject", IsSingle = true)]
         IDiffSpread<string> FPinInSubject;
 
-        [Input("Message")]
+        [Input("Message", IsSingle = true)]
         IDiffSpread<string> FPinInMessage;
 
-        [Input("EmailEncoding", EnumName = "EmailEncoding")]
+        [Input("Message Path", IsSingle = true, StringType = StringType.Filename)]
+        IDiffSpread<string> FPinMassagePath;
+
+        [Input("EmailEncoding", IsSingle = true, EnumName = "EmailEncoding")]
         IDiffSpread<EnumEntry> FEmailEncoding;
 
-        [Input("AsHtml")]
+        [Input("AsHtml", IsSingle = true)]
         IDiffSpread<bool> FPinInIsHtml;
 
-        [Input("Attachment", StringType = StringType.Filename)]
+        [Input("Attachment", IsSingle = true, StringType = StringType.Filename)]
         IDiffSpread<string> FPinInAttachment;
 
         [Output("Success")]
@@ -68,6 +74,8 @@ namespace VVVV.Nodes
 
         [Import()]
         ILogger FLogger;
+
+        string FError = "";
 
         [ImportingConstructor]
         public SendEmailNode()
@@ -90,6 +98,7 @@ namespace VVVV.Nodes
 
                 SmtpClient EmailClient = new SmtpClient(FPinInHost[0], FPinInPort[0]);
                 EmailClient.EnableSsl = FPinInSSL[0];
+                EmailClient.SendCompleted += new SendCompletedEventHandler(EmailClient_SendCompleted);
 
                 if (Username.Length > 0 && Pwd.Length > 0)
                 {
@@ -99,65 +108,135 @@ namespace VVVV.Nodes
 
                 FPinOutSuccess.SliceCount = SpreadMax;
 
-                for (int i = 0; i < SpreadMax; i++)
+                //for (int i = 0; i < SpreadMax; i++)
+                //{
+                try
                 {
-                    try
+                    //string message = FPinInMessage[0];
+
+                    //Debug.WriteLine(message);
+
+                    //Encoding VVVV = Encoding.UTF8;
+                    //byte[] byteArray = VVVV.GetBytes(message);
+                    //byte[] ConvertedArray = Encoding.Convert(VVVV, Encoding.Unicode, byteArray);
+                    //string finalString = Encoding.Unicode.GetString(ConvertedArray);
+                    //Debug.WriteLine(finalString);
+
+
+                    //// Convert the UTF-16 encoded source string to UTF-8 and ASCII.
+                    //byte[] utf8String = Encoding.UTF8.GetBytes(message);
+                    //byte[] asciiString = Encoding.ASCII.GetBytes(message);
+
+                    //// Write the UTF-8 and ASCII encoded byte arrays. 
+                    //Debug.WriteLine("UTF-8  Bytes: {0}", BitConverter.ToString(utf8String));
+                    //Debug.WriteLine("ASCII  Bytes: {0}", BitConverter.ToString(asciiString));
+
+
+                    //// Convert UTF-8 and ASCII encoded bytes back to UTF-16 encoded  
+                    //// string and write.
+                    //Debug.WriteLine("UTF-8  Text : {0}", Encoding.UTF8.GetString(utf8String));
+                    //Debug.WriteLine("ASCII  Text : {0}", Encoding.ASCII.GetString(asciiString));
+
+                    //Console.WriteLine(Encoding.UTF8.GetString(asciiString));
+                    //Console.WriteLine(Encoding.ASCII.GetString(asciiString));
+
+                    string Message = "";
+
+                    if (File.Exists(FPinMassagePath[0]))
                     {
-                        string message = FPinInMessage[i];
-
-
-                        UTF8Encoding utf8 = new UTF8Encoding();
-                        byte[] byteArray = Encoding.Unicode.GetBytes(FPinInMessage[i]);
-                        byte[] utf8Array = Encoding.Convert(Encoding.Unicode, Encoding.UTF8, byteArray);
-                        string finalString = utf8.GetString(utf8Array);
-
-
-                        MailMessage mail = new MailMessage(FPinInFrom[i], FPinInTo[i], FPinInSubject[i], finalString);
-                        mail.IsBodyHtml = FPinInIsHtml[i];
-
-                        switch (FEmailEncoding[i].Index)
-                        {
-                            case (0):
-                                mail.BodyEncoding = Encoding.ASCII;
-                                break;
-                            case (1):
-                                mail.BodyEncoding = Encoding.UTF8;
-                                break;
-                            case (2):
-                                mail.BodyEncoding = Encoding.UTF32;
-                                break;
-                            case (3):
-                                mail.BodyEncoding = Encoding.Unicode;
-                                break;
-                            default:
-                                mail.BodyEncoding = Encoding.ASCII;
-                                break;
-                        }
-
-                        if (!String.IsNullOrEmpty(FPinInAttachment[i]))
-                        {
-                            Attachment Attachment = new Attachment(FPinInAttachment[i]);
-                            ContentDisposition Disposition = Attachment.ContentDisposition;
-                            Disposition.Inline = false;
-                            mail.Attachments.Add(Attachment);
-                        }
-
-
-                        EmailClient.Send(mail);
-                        FPinOutSuccess[i] = true;
+                        FileStream MessageFile = new FileStream(FPinMassagePath[0], FileMode.Open);
+                        StreamReader reader = new StreamReader(MessageFile);
+                        Message = reader.ReadToEnd();
+                        //Debug.WriteLine(Message);
+                        reader.Close();
+                        MessageFile.Close();
                     }
-                    catch (Exception ex)
+                    else
                     {
-                        FLogger.Log(LogType.Error, ex.Message);
-                        FPinOutSuccess[i] = false;
+                        Message = FPinInMessage[0];
                     }
+
+
+                    MailMessage mail = new MailMessage(FPinInFrom[0], FPinInTo[0], FPinInSubject[0], Message);
+                    mail.IsBodyHtml = FPinInIsHtml[0];
+
+                    switch (FEmailEncoding[0].Index)
+                    {
+                        case (0):
+                            mail.BodyEncoding = Encoding.ASCII;
+                            break;
+                        case (1):
+                            mail.BodyEncoding = Encoding.UTF8;
+                            break;
+                        case (2):
+                            mail.BodyEncoding = Encoding.UTF32;
+                            break;
+                        case (3):
+                            mail.BodyEncoding = Encoding.Unicode;
+                            break;
+                        default:
+                            mail.BodyEncoding = Encoding.UTF8;
+                            break;
+                    }
+
+                    if (File.Exists(FPinInAttachment[0]))
+                    {
+                        Attachment Attachment = new Attachment(FPinInAttachment[0]);
+                        ContentDisposition Disposition = Attachment.ContentDisposition;
+                        Disposition.Inline = false;
+                        mail.Attachments.Add(Attachment);
+                    }
+
+
+
+                    EmailClient.SendAsync(mail,0);
 
                 }
+                catch (Exception ex)
+                {
+                    FLogger.Log(LogType.Error, ex.Message);
+                    //FPinOutSuccess[i] = false;
+                }
+                finally
+                {
+                    EmailClient = null;
+                }
+
+
+
+                //}
             }
             else
             {
                 this.FPinOutSuccess.SliceCount = 0;
             }
+
+            if (!String.IsNullOrEmpty(FError))
+            {
+                FLogger.Log(LogType.Debug, FError);
+                FError = "";
+            }
+        }
+
+        void EmailClient_SendCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            // Get the unique identifier for this asynchronous operation.
+            int token = (int)e.UserState;
+
+            if (e.Cancelled)
+            {
+                FError = "Cancelt";
+            }
+            if (e.Error != null)
+            {
+                FError = e.Error.ToString();
+            }
+            else
+            {
+                FError = "Message sent.";
+            }
+
+            FPinOutSuccess[0] = true;
         }
         #endregion
 
