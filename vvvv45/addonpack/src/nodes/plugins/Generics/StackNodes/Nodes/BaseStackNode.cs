23758626a1749468b7007b563bf9b7f6f7960808
Julien Vulliet <julien@mrvux.com>
2011-07-22 10:25:15 +0000

    Now has a default + reset pin to set to a specific initial state.
    svn path=/plugins/c#/Generics/StackNodes/trunk/; revision=6068

--- vvvv45/addonpack/src/nodes/plugins/Generics/StackNodes/Nodes/BaseStackNode.cs
+++ vvvv45/addonpack/src/nodes/plugins/Generics/StackNodes/Nodes/BaseStackNode.cs
@@ -24,6 +24,12 @@ namespace VVVV.Nodes
 		[Input("Stack Size", DefaultValue = -1, IsSingle = true)]
 		IDiffSpread<int> FPinInStackSize;
 
+        [Input("Default")]
+        ISpread<T> FPinDefault;
+
+        [Input("Reset", IsBang = true, IsSingle = true)]
+        ISpread<bool> FPinInReset;
+
         [Output("Output")]
         ISpread<T> FPinOutput;
 
@@ -45,57 +51,70 @@ namespace VVVV.Nodes
         #region IPluginEvaluate Members
         public void Evaluate(int SpreadMax)
         {
+
 			//Update Stack size
 			if (FPinInStackSize.IsChanged)
 			{
 				FStackSize = FPinInStackSize[0];
 			}
 
-			//Push spread in the stack if required
-            if (FPinInPush[0] && FPinInput.SliceCount > 0)
+            if (this.FPinInReset[0])
             {
-				FStack.Push(FPinInput.ToList());
-            }
-
-			//Pop as many items as required to reduce buffer size
-			if (FStackSize >= 0)
-			{
-				while (this.FStack.Count > FStackSize) { FStack.Pop(); }
-			}
-			
-			//Pop an element off the stack and output it
-			if (FPinInPop[0] && this.FStack.Count > 0)
-            {
-				List<T> output = FStack.Pop();
-				FPinOutput.SliceCount = output.Count;
-				for (int i = 0; i < output.Count; i++)
-				{
-					FPinOutput[i] = output[i];
-				}
+                FStack.Clear();
+                if (FPinDefault.SliceCount > 0)
+                {
+                    FStack.Push(FPinDefault.ToList());
+                }
             }
             else
             {
-                FPinOutput.SliceCount = 0;
+
+                //Push spread in the stack if required
+                if (FPinInPush[0] && FPinInput.SliceCount > 0)
+                {
+                    FStack.Push(FPinInput.ToList());
+                }
+
+                //Pop as many items as required to reduce buffer size
+                if (FStackSize >= 0)
+                {
+                    while (this.FStack.Count > FStackSize) { FStack.Pop(); }
+                }
+
+                //Pop an element off the stack and output it
+                if (FPinInPop[0] && this.FStack.Count > 0)
+                {
+                    List<T> output = FStack.Pop();
+                    FPinOutput.SliceCount = output.Count;
+                    for (int i = 0; i < output.Count; i++)
+                    {
+                        FPinOutput[i] = output[i];
+                    }
+                }
+                else
+                {
+                    FPinOutput.SliceCount = 0;
+                }
+
+                //Output stack size
+                this.FPinOutStackSize.SliceCount = 1;
+                this.FPinOutStackSize[0] = this.FStack.Count;
+
+                //Show top level spread
+                if (this.FStack.Count > 0)
+                {
+                    List<T> peek = FStack.Peek();
+                    FPinOutPeek.SliceCount = peek.Count;
+                    for (int i = 0; i < peek.Count; i++)
+                    {
+                        FPinOutPeek[i] = peek[i];
+                    }
+                }
+                else
+                {
+                    this.FPinOutPeek.SliceCount = 0;
+                }
             }
-
-			//Output stack size
-			this.FPinOutStackSize.SliceCount = 1;
-			this.FPinOutStackSize[0] = this.FStack.Count;
-
-			//Show top level spread
-			if (this.FStack.Count > 0)
-			{
-				List<T> peek = FStack.Peek();
-				FPinOutPeek.SliceCount = peek.Count;
-				for (int i = 0; i < peek.Count; i++)
-				{
-					FPinOutPeek[i] = peek[i];
-				}
-			}
-			else
-			{
-				this.FPinOutPeek.SliceCount = 0;
-			}
         }
         #endregion
     }
