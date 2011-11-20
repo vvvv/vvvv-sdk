e5b7bb99b2f4996490e6b682cdc53b2852fba0d1
woeishi <woei@refect.net>
2011-11-20 13:31:38 +0100

    undo ISpread.GetRange bugfix in this branch

--- vvvv45/src/core/PluginInterfaces/V2/ISpread.cs
+++ vvvv45/src/core/PluginInterfaces/V2/ISpread.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -306,9 +306,9 @@ namespace VVVV.PluginInterfaces.V2
         {
             var result = new Spread<T>(count);
             
-            for (int i = 0; i < count; i++)
+            for (int i = index; i < index + count; i++)
             {
-                result[i] = spread[index + i];
+                result[i] = spread[i];
             }
             
             return result;
