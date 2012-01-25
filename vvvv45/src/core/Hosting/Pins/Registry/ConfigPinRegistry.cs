8a55167cc2a49d070924934c42da73f834bf406f
joreg <joreg@vvvv.org>
2012-01-25 23:22:46 +0100

    Merge commit '50c09595ab202619108c2e4a003ee691200c3bd6' into develop

--- vvvv45/src/core/Hosting/Pins/Registry/ConfigPinRegistry.cs
+++ vvvv45/src/core/Hosting/Pins/Registry/ConfigPinRegistry.cs
@@ -31,9 +31,12 @@ namespace VVVV.Hosting.Pins
             this.RegisterType(typeof(Vector2), (host, attribute, t) => new Vector2ConfigPin(host, attribute));
             this.RegisterType(typeof(Vector3), (host, attribute, t) => new Vector3ConfigPin(host, attribute));
             this.RegisterType(typeof(Vector4), (host, attribute, t) => new Vector4ConfigPin(host, attribute));
+            this.RegisterType(typeof(Quaternion), (host, attribute, t) => new QuaternionConfigPin(host, attribute));
 
             this.RegisterType(typeof(string), (host, attribute, t) => new StringConfigPin(host, attribute));
+
             this.RegisterType(typeof(RGBAColor), (host, attribute, t) => new ColorConfigPin(host, attribute));
+            this.RegisterType(typeof(Color4), (host, attribute, t) => new SlimDXColorConfigPin(host, attribute));
 
             this.RegisterType(typeof(EnumEntry), (host, attribute, t) => new DynamicEnumConfigPin(host, attribute));
 
