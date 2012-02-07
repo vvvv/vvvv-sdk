f49571d6a21b8e95138afe16a216e9e639644c99
Julien Vulliet <julien@mrvux.com>
2012-02-07 16:13:42 +0100

    -Added IStartable interface. -Added Startable attribute (lazy/non lazy) -Now plugin assemblies can register IStartable to be able to execute a task on load.

---
+++ vvvv45/src/core/PluginInterfaces/V2/StartableAttribute.cs
@@ -0,0 +1,17 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.ComponentModel.Composition;
+
+namespace VVVV.PluginInterfaces.V2
+{
+    [MetadataAttribute]
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    [ComVisible(false)]
+    public class StartableAttribute : ExportAttribute
+    {
+        public bool Lazy { get; set; }
+    }
+}
