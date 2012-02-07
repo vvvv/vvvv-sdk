5bd0da064bd47a60e36b650105c5cca29ae47497
Julien Vulliet <julien@mrvux.com>
2012-02-07 20:58:46 +0100

    IStartable method now has Shutdown method.

--- vvvv45/src/core/PluginInterfaces/V2/IStartable.cs
+++ vvvv45/src/core/PluginInterfaces/V2/IStartable.cs
@@ -6,9 +6,21 @@ using System.Runtime.InteropServices;
 
 namespace VVVV.PluginInterfaces.V2
 {
+   
+    /// <summary>
+    /// Interface to allow some code to be executed on startup.
+    /// </summary>
     [ComVisible(false)]
     public interface IStartable
     {
+        /// <summary>
+        /// Code to execute once library is loaded.
+        /// </summary>
         void Start();
+
+        /// <summary>
+        /// Code to execute when vvvv quits.
+        /// </summary>
+        void Shutdown();
     }
 }
