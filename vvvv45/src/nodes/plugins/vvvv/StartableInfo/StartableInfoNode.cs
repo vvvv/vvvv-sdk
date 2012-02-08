47063a00b03fbc258bb2de6e4d20e3a610f3a26c
Julien Vulliet <julien@mrvux.com>
2012-02-08 15:10:23 +0100

    -Dot Net factory exposes Startable registry. -Startable registry keeps more info about statuses. -HDE Host ShutDown stops all Startable Interfaces. -Startable Attribute Now exposes a friendly name. -Added Info (VVVV Startables) node, to know what's started and status.

---
+++ vvvv45/src/nodes/plugins/vvvv/StartableInfo/StartableInfoNode.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VVVV.PluginInterfaces.V2;
+using VVVV.PluginInterfaces.V1;
+using VVVV.Hosting.Factories;
+using System.ComponentModel.Composition;
+
+namespace VVVV.Nodes.vvvv.StartableInfo
+{
+    [PluginInfo(Name="Info",Category="VVVV",Version="Startables",Author="vux",Help="Returns info about any autostart modules in VVVV")]
+    public class StartableInfoNode : IPluginEvaluate
+    {
+        [Input("Update", IsSingle = true, IsBang = true)]
+        ISpread<bool> FInUpdate;
+
+        [Output("Name")]
+        ISpread<string> FOutName;
+
+        [Output("Is Started")]
+        ISpread<bool> FOutStarted;
+
+        [Output("Message")]
+        ISpread<string> FOutMessage;
+
+        [Output("Type Name",Visibility=PinVisibility.Hidden)]
+        ISpread<string> FOutTypeName;
+
+        DotNetPluginFactory FDNFactory;
+
+        bool FFirstFrame = true;
+
+        [ImportingConstructor()]
+        public StartableInfoNode(DotNetPluginFactory dnFactory)
+        {
+            FDNFactory = dnFactory;
+        }
+
+        public void Evaluate(int SpreadMax)
+        {
+            if (FInUpdate[0] || FFirstFrame)
+            {
+                List<StartableStatus> status = FDNFactory.StartableRegistry.Status;
+                int cnt = status.Count;
+                FOutMessage.SliceCount = cnt;
+                FOutName.SliceCount = cnt;
+                FOutStarted.SliceCount = cnt;
+                FOutTypeName.SliceCount = cnt;
+
+                for (int i = 0; i < cnt; i++)
+                {
+                    StartableStatus s = status[i];
+                    FOutTypeName[i] = s.Info.Type.FullName;
+                    FOutStarted[i] = s.Success;
+                    FOutMessage[i] = s.Message;
+                    FOutName[i] = s.Info.Name;
+                }
+
+                FFirstFrame = false;
+            }
+        }
+    }
+}
