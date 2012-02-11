235e9c4129e4c140a37c2b19ff1e041869c84f28
Julien Vulliet <julien@mrvux.com>
2012-02-11 16:06:01 +0100

    Mesh node still had dispose...

--- vvvv45/addonpack/src/nodes/plugins/Mesh/Assimp/Nodes/AssimpWorldNode.cs
+++ vvvv45/addonpack/src/nodes/plugins/Mesh/Assimp/Nodes/AssimpWorldNode.cs
@@ -8,7 +8,7 @@ using SlimDX;
 
 namespace VVVV.Assimp.Nodes
 {
-    [PluginInfo(Name = "Node", Category = "Assimp", Version = "DX11", Author = "vux,flateric")]
+    [PluginInfo(Name = "Node", Category = "Assimp", Version = "", Author = "vux,flateric")]
     public class AssimpWorldNode : IPluginEvaluate
     {
         [Input("Scene", IsSingle = true)]
