f1587a5311ac05b156ed9fe8a49b34c7661b0fab
Elias Holzer <elias@vvvv.org>
2012-03-02 18:42:40 +0100

    Merge branch 'develop' into streams
    Conflicts:
    	public/vvvv45/addonpack/src/nodes/plugins/Spreads/Interleave/Nodes/ValueInterleaveNode.cs
    	vvvv45/src/core/Graph.pas
    	vvvv45/src/core/plugins/VVVV_Hosting_TLB.pas

--- vvvv45/src/core/Hosting/Graph/Pin.cs
+++ vvvv45/src/core/Hosting/Graph/Pin.cs
@@ -6,149 +6,211 @@ using VVVV.Utils;
 
 namespace VVVV.Hosting.Graph
 {
-    internal class Pin : Disposable, IPin2
-    {
-        #region IPinListener implementation
-        
-        class PinListener : Disposable, IPinListener
-        {
-            private readonly Pin FPin;
-            
-            public PinListener(Pin pin)
-            {
-                FPin = pin;
-                
-                FPin.FInternalCOMInterf.AddListener(this);
-            }
-            protected override void DisposeManaged()
-            {
-                FPin.FInternalCOMInterf.RemoveListener(this);
-                
-                base.DisposeManaged();
-            }
-            
-            public void ChangedCB()
-            {
-                FPin.OnChanged(EventArgs.Empty);
-            }
+	internal class Pin: Disposable, IPin2
+	{
+		#region IPinListener implementation
+		
+		class PinListener : Disposable, IPinListener
+		{
+			private readonly Pin FPin;
+			
+			public PinListener(Pin pin)
+			{
+				FPin = pin;
+				
+				FPin.FInternalCOMInterf.AddListener(this);
+			}
+			protected override void DisposeManaged()
+			{
+				FPin.FInternalCOMInterf.RemoveListener(this);
+				
+				base.DisposeManaged();
+			}
+			
+			public void ChangedCB()
+			{
+				FPin.OnChanged(EventArgs.Empty);
+			}
 			
 			public void StatusChangedCB()
 			{
 				FPin.OnStatusChanged();
 			}
-        }
-        
-        #endregion
-        
-        private INode2 FParentNode;
-        private readonly IPin FInternalCOMInterf;
-        private readonly string FName;
-        private readonly ProxyNodeInfoFactory FNodeInfoFactory;
-        private PinListener FPinListener;
-        private int FObserverCount;
-        
-        internal Pin(INode2 node, IPin internalCOMInterf, ProxyNodeInfoFactory nodeInfoFactory)
-        {
-        	FParentNode = node;
-        	FInternalCOMInterf = internalCOMInterf;
-        	FNodeInfoFactory = nodeInfoFactory;
-            FName = FInternalCOMInterf.GetName();
-        }
-        
-        protected override void DisposeManaged()
-        {
+			
+			public void SubtypeChangedCB()
+			{
+				FPin.OnSubtypeChanged();
+			}
+		}
+		
+		#endregion
+		
+		private INode2 FParentNode;
+		private readonly IPin FInternalCOMInterf;
+		private readonly string FName;
+		private readonly ProxyNodeInfoFactory FNodeInfoFactory;
+		private PinListener FPinListener;
+		private int FObserverCount;
+		
+		internal Pin(INode2 node, IPin internalCOMInterf, ProxyNodeInfoFactory nodeInfoFactory)
+		{
+			FParentNode = node;
+			FInternalCOMInterf = internalCOMInterf;
+			FNodeInfoFactory = nodeInfoFactory;
+			FName = FInternalCOMInterf.Name;
+		}
+		
+		protected override void DisposeManaged()
+		{
 			if (FPinListener != null)
-            	FPinListener.Dispose();
-            base.DisposeManaged();
-        }
-        
-        public override string ToString()
-        {
-            return FName;
-        }
-        
-        private void IncObserverCount()
-        {
-        	FObserverCount++;
-        	if (FPinListener == null)
-        		FPinListener = new PinListener(this);
-        }
-        
-        private void DecObserverCount()
-        {
-        	FObserverCount--;
-        	if (FObserverCount == 0)
-        	{
-        		FPinListener.Dispose();
-        		FPinListener = null;
-        	}
-        }
-        
-        private event EventHandler FChanged;
-        public event EventHandler Changed
-        {
-        	add
-        	{
-        		IncObserverCount();
-        		FChanged += value;
-        	}
-        	remove
-        	{
-        		FChanged -= value;
-        		DecObserverCount();
-        	}
-        }
-        
-        protected virtual void OnChanged(EventArgs e)
-        {
-            if (FChanged != null) {
-                FChanged(this, e);
-            }
-        }
-        
-        public event RenamedHandler Renamed;
-        
-        protected virtual void OnRenamed(string newName)
-        {
-            if (Renamed != null) {
-                Renamed(this, newName);
-            }
-        }
-        
-        public INode2 ParentNode
-        {
-            get 
-            { 
-            	if (FParentNode == null)
-            	{
-            		FParentNode = Node.Create(FInternalCOMInterf.ParentNode, FNodeInfoFactory);
-            	}
-            	return FParentNode; 
-            }
-        }
-        
-        public string this[int sliceIndex] 
-        {
-            get 
-            {
-            	try
-            	{
-                	return FInternalCOMInterf.GetValue(sliceIndex);
-            	}
-            	catch
-            	{
-            		return string.Empty;
-            	}
-            }
-        }
-        
-        public string Name
-        {
-            get
-            {
-                return FName;
-            }
-        }
+				FPinListener.Dispose();
+			base.DisposeManaged();
+		}
+		
+		public override string ToString()
+		{
+			return FName;
+		}
+		
+		private void IncObserverCount()
+		{
+			FObserverCount++;
+			if (FPinListener == null)
+				FPinListener = new PinListener(this);
+		}
+		
+		private void DecObserverCount()
+		{
+			FObserverCount--;
+			if (FObserverCount == 0)
+			{
+				FPinListener.Dispose();
+				FPinListener = null;
+			}
+		}
+		
+		private event EventHandler FChanged;
+		public event EventHandler Changed
+		{
+			add
+			{
+				IncObserverCount();
+				FChanged += value;
+			}
+			remove
+			{
+				FChanged -= value;
+				DecObserverCount();
+			}
+		}
+		
+		protected virtual void OnChanged(EventArgs e)
+		{
+			if (FChanged != null) {
+				FChanged(this, e);
+			}
+		}
+		
+		public event RenamedHandler Renamed;
+		
+		protected virtual void OnRenamed(string newName)
+		{
+			if (Renamed != null) {
+				Renamed(this, newName);
+			}
+		}
+		
+		public INode2 ParentNode
+		{
+			get
+			{
+				if (FParentNode == null)
+				{
+					FParentNode = Node.Create(FInternalCOMInterf.ParentNode, FNodeInfoFactory);
+				}
+				return FParentNode;
+			}
+		}
+		
+		public string this[int sliceIndex]
+		{
+			get
+			{
+				try
+				{
+					return FInternalCOMInterf.GetSlice(sliceIndex);
+				}
+				catch
+				{
+					return string.Empty;
+				}
+			}
+			
+			set
+			{
+				FInternalCOMInterf.SetSlice(sliceIndex, value);
+			}
+		}
+		
+		public string Spread
+		{
+			get
+			{
+				try
+			{
+				return FInternalCOMInterf.GetSpread();
+			}
+			catch
+			{
+				return string.Empty;
+			}	
+			}
+			
+			set
+			{
+				FInternalCOMInterf.SetSpread(value);	
+			}
+		}
+
+		public int SliceCount
+		{
+			get
+			{
+				return FInternalCOMInterf.SliceCount;
+			}
+		}
+		
+		public PinDirection Direction
+		{
+			get
+			{
+				return FInternalCOMInterf.Direction;
+			}
+		}
+		
+		public string Type
+		{
+			get
+			{
+				return FInternalCOMInterf.Type;
+			}
+		}
+		
+		public string SubType
+		{
+			get
+			{
+				return FInternalCOMInterf.SubType;
+			}
+		}
+		
+		public string Name
+		{
+			get
+			{
+				return FName;
+			}
+		}
 		
 		public StatusCode Status
 		{
@@ -172,11 +234,32 @@ namespace VVVV.Hosting.Graph
 				DecObserverCount();
 			}
 		}
-        
-        protected virtual void OnStatusChanged()
-        {
-            if (FStatusChanged != null)
-                FStatusChanged(this, EventArgs.Empty);
-        }
-    }
+		
+		protected virtual void OnStatusChanged()
+		{
+			if (FStatusChanged != null)
+				FStatusChanged(this, EventArgs.Empty);
+		}
+		
+		private event EventHandler FSubtypeChanged;
+		public event EventHandler SubtypeChanged
+		{
+			add
+			{
+				IncObserverCount();
+				FSubtypeChanged += value;
+			}
+			remove
+			{
+				FSubtypeChanged -= value;
+				DecObserverCount();
+			}
+		}
+		
+		protected virtual void OnSubtypeChanged()
+		{
+			if (FSubtypeChanged != null)
+				FSubtypeChanged(this, EventArgs.Empty);
+		}
+	}
 }
--- vvvv45/src/nodes/plugins/vvvv/Kontrolleur/KontrolleurNode.cs
+++ vvvv45/src/nodes/plugins/vvvv/Kontrolleur/KontrolleurNode.cs
@@ -21,14 +21,14 @@ using VVVV.Utils.OSC;
 #endregion usings
 
 namespace VVVV.Nodes
-{	
+{
 	#region PluginInfo
 	[PluginInfo(Name = "Kontrolleur",
-	Category = "Network",
-	Help = "Communicates with the Kontrolleur Android app",
-	Tags = "remote")]
+	            Category = "Network",
+	            Help = "Communicates with the Kontrolleur Android app",
+	            AutoEvaluate = true)]
 	#endregion PluginInfo
-	public class StringOSC2PatchNode: IPluginEvaluate, IPartImportsSatisfiedNotification, IDisposable
+	public class KontrolleurNode: IPluginEvaluate, IPartImportsSatisfiedNotification, IDisposable
 	{
 		#region fields & pins
 		[Config("Kontrolleur IP", IsSingle=true, DefaultString="")]
@@ -43,16 +43,25 @@ namespace VVVV.Nodes
 		[Input("Prefix", IsSingle=true)]
 		IDiffSpread<string> FPrefix;
 		
-		[Output("Patch")]
-		ISpread<string> FPatchOut;
+		[Output("Touch ID")]
+		ISpread<int> FTouchID;
 		
-		[Output("Patch Message")]
-		ISpread<string> FMessageOut;
+		[Output("Touch")]
+		ISpread<Vector2D> FTouchXY;
 		
-		[Output("Kontrolleur Resolution")]
-		ISpread<Vector2D> FKontrolleurResolution;
+		[Output("Touch Pressure")]
+		ISpread<double> FTouchPressure;
 		
-		private IHDEHost FHost;
+		[Output("Acceleration")]
+		ISpread<Vector3D> FAcceleration;
+		
+		[Output("Orientation")]
+		ISpread<Vector3D> FOrientation;
+		
+		[Output("Magnetism")]
+		ISpread<Vector3D> FMagnetism;
+		
+		private Vector2D FResolution;
 		private INode2 FRoot;
 		private OSCTransmitter FOSCTransmitter;
 		private IPAddress FTargetIP;
@@ -64,22 +73,26 @@ namespace VVVV.Nodes
 		private bool FFirstFrame = true;
 		private List<string> FPrefixes = new List<string>();
 		// Track whether Dispose has been called.
-        private bool FDisposed = false;
+		private bool FDisposed = false;
 		
 		[Import]
 		ILogger FLogger;
 		
+		[Import]
+		IHDEHost FHDEHost;
+		
 		private Timer FTimer = new Timer(2000);
 		private bool FAllowUpdates = true;
 		private XmlDocument FXML = new XmlDocument();
-		private Dictionary<string, string> FMessages = new Dictionary<string, string>();
-		private Dictionary<string, string> FBangs = new Dictionary<string, string>();
+		private List<IPin2> FBangs = new List<IPin2>();
 		private Dictionary<string, RemoteValue> FTargets = new Dictionary<string, RemoteValue>();
+		private Dictionary<string, string> FSaveTargets = new Dictionary<string, string>();
+		private Dictionary<string, PatchMessage> FPatchMessages = new Dictionary<string, PatchMessage>();
 		#endregion fields & pins
 		
 		#region constructor/destructor
 		[ImportingConstructor]
-		public StringOSC2PatchNode(IHDEHost host)
+		public KontrolleurNode(IHDEHost host)
 		{
 			FRoot = host.RootNode;
 
@@ -88,40 +101,43 @@ namespace VVVV.Nodes
 			FTimer.Elapsed += new ElapsedEventHandler(FTimer_Elapsed);
 		}
 		
-		~StringOSC2PatchNode()
+		~KontrolleurNode()
 		{
 			Dispose(false);
 		}
 		
 		public void Dispose()
-	    {
-	        Dispose(true);
-	    }
+		{
+			Dispose(true);
+		}
 		
-        protected void Dispose(bool disposing)
-        {
-            // Check to see if Dispose has already been called.
-            if(!FDisposed)
-            {
-                if(disposing)
-                {
-                    // Dispose managed resources.
-                    FPrefix.Changed -= PrefixChangedCB;
+		protected void Dispose(bool disposing)
+		{
+			// Check to see if Dispose has already been called.
+			if(!FDisposed)
+			{
+				if(disposing)
+				{
+					// Dispose managed resources.
+					FTimer.Enabled = false;
+					FTimer.Dispose();
+					
+					FPrefix.Changed -= PrefixChangedCB;
 					UnRegisterPatch(FRoot);
-                }
-                // Release unmanaged resources. If disposing is false,
-                // only the following code is executed.
-                
-            }
-            FDisposed = true;
-        }
-        #endregion constructor/destructor
+				}
+				// Release unmanaged resources. If disposing is false,
+				// only the following code is executed.
+				
+			}
+			FDisposed = true;
+		}
+		#endregion constructor/destructor
 		
 		#region events
-		public void OnImportsSatisfied() 
-	    {
-	      FPrefix.Changed += PrefixChangedCB;
-	    } 
+		public void OnImportsSatisfied()
+		{
+			FPrefix.Changed += PrefixChangedCB;
+		}
 		
 		private void PrefixChangedCB(IDiffSpread spread)
 		{
@@ -165,19 +181,19 @@ namespace VVVV.Nodes
 				
 				if (FTargetIP != null)
 					try
-					{
-						if (FOSCTransmitter != null)
-							FOSCTransmitter.Close();
-						FOSCTransmitter = new OSCTransmitter(FTargetIP.ToString(), FTargetPort);
-						FOSCTransmitter.Connect();
+				{
+					if (FOSCTransmitter != null)
+						FOSCTransmitter.Close();
+					FOSCTransmitter = new OSCTransmitter(FTargetIP.ToString(), FTargetPort);
+					FOSCTransmitter.Connect();
 
-						FLogger.Log(LogType.Debug, "connected to Kontrolleur on: " + FTargetIP.ToString() + ":" + FTargetPort.ToString());
-					}
-					catch (Exception e)
-					{
-						FLogger.Log(LogType.Warning, "Kontrolleur: failed to open port " + FTargetPort.ToString());
-						FLogger.Log(LogType.Warning, "Kontrolleur: " + e.Message);
-					}
+					FLogger.Log(LogType.Debug, "connected to Kontrolleur on: " + FTargetIP.ToString() + ":" + FTargetPort.ToString());
+				}
+				catch (Exception e)
+				{
+					FLogger.Log(LogType.Warning, "Kontrolleur: failed to open port " + FTargetPort.ToString());
+					FLogger.Log(LogType.Warning, "Kontrolleur: " + e.Message);
+				}
 			}
 		}
 		#endregion network
@@ -192,7 +208,7 @@ namespace VVVV.Nodes
 					node.Added += new CollectionDelegate<INode2>(NodeAddedCB);
 					node.Removed += NodeRemovedCB;
 					RegisterPatch(node);
-				}					
+				}
 				else if (node.Name.Contains("IOBox"))
 					RegisterIOBox(node);
 			}
@@ -207,12 +223,12 @@ namespace VVVV.Nodes
 			//now see if this box already wants to be exposed
 			CheckExposeIOBox(io);
 			
-			//register for labelchanges 
+			//register for labelchanges
 			io.LabelPin.Changed += LabelChangedCB;
 		}
 		
 		private void UnRegisterPatch(INode2 patch)
-		{   
+		{
 			foreach (INode2 node in patch)
 			{
 				if (node.HasPatch)
@@ -220,7 +236,7 @@ namespace VVVV.Nodes
 					node.Added -= NodeAddedCB;
 					node.Removed -= NodeRemovedCB;
 					UnRegisterPatch(node);
-				}					
+				}
 				else if (node.Name.Contains("IOBox"))
 					UnRegisterIOBox(node);
 			}
@@ -234,7 +250,7 @@ namespace VVVV.Nodes
 			
 			UnExposeIOBox(io);
 			
-			//unregister from labelchanges 
+			//unregister from labelchanges
 			io.LabelPin.Changed -= LabelChangedCB;
 		}
 		
@@ -245,7 +261,7 @@ namespace VVVV.Nodes
 			if (node.HasPatch)
 				RegisterPatch(node);
 			else if (node.Name.Contains("IOBox"))
-				RegisterIOBox(node);			
+				RegisterIOBox(node);
 		}
 		
 		private void NodeRemovedCB(IViewableCollection collection, object item)
@@ -255,7 +271,7 @@ namespace VVVV.Nodes
 			if (node.HasPatch)
 				UnRegisterPatch(node);
 			else if (node.Name.Contains("IOBox"))
-				UnRegisterIOBox(node);	
+				UnRegisterIOBox(node);
 		}
 		
 		private void CheckExposeIOBox(INode2 node)
@@ -269,12 +285,14 @@ namespace VVVV.Nodes
 				var name = node.LabelPin[0];
 				
 				if ((FPrefixes.Count == 0)
-				|| ((FPrefixes.Count > 0) && name.StartsWith(FPrefix[0])))
+				    || ((FPrefixes.Count > 0) && name.StartsWith(FPrefix[0])))
 				{
 					var target = new RemoteValue(node, FPrefixes);
 					
-					if (!FTargets.ContainsKey(target.Address))
-						FTargets.Add(target.Address, target);
+					if (!FTargets.ContainsKey(target.RuntimeNodePath))
+						FTargets.Add(target.RuntimeNodePath, target);
+					else
+						target.Kill();
 				}
 				else
 					unexpose = true;
@@ -286,7 +304,7 @@ namespace VVVV.Nodes
 		
 		private void UnExposeIOBox(INode2 node)
 		{
-			var address = "/" + node.Parent.NodeInfo.Filename + "/" + node.ID;
+			var address = node.GetNodePath(false);
 			
 			if (FTargets.ContainsKey(address))
 				FTargets[address].State = RemoteValueState.Remove;
@@ -304,12 +322,15 @@ namespace VVVV.Nodes
 		{
 			if (message.Address == "/k/init")
 			{
+				foreach (var target in FTargets.ToArray())
+					target.Value.Kill();
 				FTargets.Clear();
+				FSaveTargets.Clear();
 				
 				FAutoIP = IPAddress.Parse((string)message.Values[0]);
 				FAutoPort = (int) message.Values[1];
-				FKontrolleurResolution[0] = new Vector2D((int) message.Values[2], (int) message.Values[3]);
-				
+				FResolution = new Vector2D((int) message.Values[2], (int) message.Values[3]);
+
 				InitNetwork();
 				
 				UnRegisterPatch(FRoot);
@@ -317,50 +338,113 @@ namespace VVVV.Nodes
 				
 				return;
 			}
-			else if (!message.Address.StartsWith("/k/"))
-				return;
-			
-			var address = message.Address.Split('/');
-			if (!FMessages.ContainsKey(address[2]))
-				FMessages.Add(address[2], "<PATCH></PATCH>");
-			
-			var m = FMessages[address[2]];
-			FXML.LoadXml(m);
-			var patch = FXML.SelectSingleNode(@"//PATCH");
-			
-			var node = FXML.CreateElement("NODE");
-			patch.AppendChild(node);
-			var id = FXML.CreateAttribute("id");
-			id.Value = address[3];
-			node.Attributes.Append(id);
-			
-			//var values = string.Join(",", message.Values.ToArray(typeof(string)));
-			var values = "";
-			foreach(var v in message.Values)
-				values += v.ToString().Replace(',', '.') + ",";
-			values = values.TrimEnd(',');
-			
-			if (values == "bang")
+			else if (message.Address == "/k/save")
 			{
-				node.InnerXml = "<PIN pinname=\"Y Input Value\" values=\"1\" />";
-				FBangs.Add(address[2], patch.OuterXml);
-			}	
-			else
-				node.InnerXml = "<PIN pinname=\"Y Input Value\" values=\"" + values + "\" />";
-			
-			FMessages[address[2]] = patch.OuterXml;
+				//save all savetargets to patch by sending a patchmessage
+				//savetargets can be scattered over many patches so potentially this needs to send multiple patchmessages
+				
+				FPatchMessages.Clear();
+				foreach (var target in FSaveTargets)
+				{
+					var patchClass = target.Key.Split('/');
+					PatchMessage pm;
+					if (FPatchMessages.ContainsKey(patchClass[0]))
+						pm = FPatchMessages[patchClass[0]];
+					else
+					{
+						pm = new PatchMessage(patchClass[0]);
+						FPatchMessages.Add(patchClass[0], pm);
+					}
+					
+					var node = pm.AddNode(int.Parse(patchClass[1]));
+					var pin = node.AddPin("Y Input Value");
+					pin.AddAttribute("values", target.Value);
+				}
+				
+				foreach (var pm in FPatchMessages)
+					FHDEHost.SendPatchMessage(pm.Key, pm.Value.ToString(), true);
+			}
+			else if (message.Address == "/acceleration")
+			{
+				FAcceleration[0] = new Vector3D((float)message.Values[0], (float)message.Values[1], (float)message.Values[2]);
+			}
+			else if (message.Address == "/magnetism")
+			{
+				FMagnetism[0] = new Vector3D((float)message.Values[0], (float)message.Values[1], (float)message.Values[2]);
+			}
+			else if (message.Address == "/orientation")
+			{
+				FOrientation[0] = new Vector3D((float)message.Values[0], (float)message.Values[1], (float)message.Values[2]);
+			}
+			else if (message.Address == "/touches")
+			{
+				int touchCount = message.Values.Count / 4;
+				int t = 0;
+				FTouchID.SliceCount = touchCount;
+				FTouchXY.SliceCount = touchCount;
+				FTouchPressure.SliceCount = touchCount;
+				
+				for (int i = 0; i < touchCount; i++)
+				{
+					FTouchID[i] = (int) message.Values[t];
+					FTouchXY[i] = new Vector2D((float)message.Values[t + 1] / FResolution.x * 2 - 1, (float)message.Values[t + 2] / FResolution.y * -2 + 1);
+					FTouchPressure[i] = (float) message.Values[t + 3];
+					t += 4;
+				}
+			}
+			else if (message.Address.StartsWith("/k/"))
+			{
+				FAllowUpdates = false;
+				
+				var address = "/" + message.Address.Trim('/','k');
+				if (FTargets.ContainsKey(address))
+				{
+					var pin = FTargets[address].Pin;
+					
+					//var values = string.Join(",", message.Values.ToArray(typeof(string)));
+					var values = "";
+					foreach(var v in message.Values)
+						values += v.ToString().Replace(',', '.') + ",";
+					values = values.TrimEnd(',');
+					
+					if (values == "bang")
+					{
+						values = "1";
+						//save pin for sending 0 next frame
+						FBangs.Add(pin);
+					}
+					
+					//save last value sent per path (for optional later saving to patch)
+					if (FSaveTargets.ContainsKey(FTargets[address].SourceNodePath))
+						FSaveTargets[FTargets[address].SourceNodePath] = values;
+					else
+						if (!(FTargets[address].Type == "Bang"))
+							FSaveTargets.Add(FTargets[address].SourceNodePath, values);
+					
+					pin.Spread = values;
+				}
+			}
 		}
 		#endregion OSC
 		
 		#region MainLoop
 		public void Evaluate(int SpreadMax)
 		{
+			FTouchID.SliceCount = 0;
+			FTouchXY.SliceCount = 0;
+			FTouchPressure.SliceCount = 0;
+			
 			//re/init udp
 			if (FKontrolleurIP.IsChanged || FKontrolleurPort.IsChanged)
 			{
-				FManualIP = IPAddress.Parse(FKontrolleurIP[0]);
-				FManualPort = FKontrolleurPort[0];
-				InitNetwork();
+				try
+				{
+					FManualIP = IPAddress.Parse(FKontrolleurIP[0]);
+					FManualPort = FKontrolleurPort[0];
+					InitNetwork();
+				}
+				catch
+				{}
 			}
 			
 			if (FFirstFrame)
@@ -374,41 +458,38 @@ namespace VVVV.Nodes
 			//return multiple slices, one per patch addressed
 			//ie. multiple nodes addressed in one patch go to the same slice
 			var message = FOSCIn[0];
-			FMessages.Clear();
-			//special treatment for bangs 
+			//special treatment for bangs
 			//which cause 2 patch messages in consecutive frames to be sent
-			foreach (var bang in FBangs)
+			foreach (var pin in FBangs)
 			{
 				//set all the bangs values to 0
-				var b = bang.Value.Replace("values=\"1\"", "values=\"0\"");
-				FMessages.Add(bang.Key, b);
-			}	
+				pin.Spread = "0";
+			}
 			FBangs.Clear();
-					
+			
+			//parse incoming OSC
 			if (!string.IsNullOrEmpty(message))
 			{
-				FAllowUpdates = false;
-				
 				var bundlePos = message.IndexOf("#bundle");
 				if (bundlePos == -1)
-				return;
+					return;
 				
 				while ((bundlePos = message.IndexOf("#bundle")) >= 0)
 				{
 					var nextpos = message.IndexOf("#bundle", bundlePos + 1);
-					var bundle = "";
+					var bundleMessage = "";
 					if (nextpos == -1)
 					{
-						bundle = message;
+						bundleMessage = message;
 						message = "";
 					}
 					else
 					{
-						bundle = message.Substring(bundlePos, nextpos - bundlePos);
+						bundleMessage = message.Substring(bundlePos, nextpos - bundlePos);
 						message = message.Substring(nextpos);
 					}
 					
-					var packet = OSCPacket.Unpack(Encoding.Default.GetBytes(bundle));
+					var packet = OSCPacket.Unpack(Encoding.Default.GetBytes(bundleMessage));
 					if (packet.IsBundle())
 					{
 						ArrayList messages = packet.Values;
@@ -423,73 +504,71 @@ namespace VVVV.Nodes
 				
 				FTimer.Start();
 			}
-			else 
+			
+			//send targets to kontrolleur
+			var bundle = new OSCBundle();
+			foreach (var target in FTargets.Values)
 			{
-				//send targets to kontrolleur
-				OSCBundle bundle = new OSCBundle();
-				foreach (var target in FTargets.Values)
+				if (target.State == RemoteValueState.Idle)
+					continue;
+				
+				OSCMessage osc;
+				if (target.State == RemoteValueState.Remove)
 				{
-					if (target.State == RemoteValueState.Idle)
-						continue;
-					
-					OSCMessage osc;
-					if (target.State == RemoteValueState.Remove)
-					{
-						osc = new OSCMessage("/k/remove");
-						osc.Append(target.Address);
-						bundle.Append(osc);
-						continue;
-					}
-					else if (target.State == RemoteValueState.Add)
-					{
-						osc = new OSCMessage("/k/add");
-						osc.Append(target.Address);
-						osc.Append(target.Name);
-						osc.Append(target.Type);
-						osc.Append(target.Default);
-						osc.Append(target.Minimum);
-						osc.Append(target.Maximum);
-						osc.Append(target.Stepsize);
-						osc.Append(target.Value);
-						bundle.Append(osc);
-					}	
-					else if (FAllowUpdates && target.State == RemoteValueState.Update)
-					{
-						osc = new OSCMessage("/k/update");
-						osc.Append(target.Address);
-						osc.Append(target.Name);
-						osc.Append(target.Type);
-						osc.Append(target.Default);
-						osc.Append(target.Minimum);
-						osc.Append(target.Maximum);
-						osc.Append(target.Stepsize);
-						osc.Append(target.Value);
-						bundle.Append(osc);
-					}
+					osc = new OSCMessage("/k/remove");
+					osc.Append(target.RuntimeNodePath);
+					bundle.Append(osc);
+					continue;
 				}
-				
-				if (FOSCTransmitter != null)
-					try
-					{
-						FOSCTransmitter.Send(bundle);
-					}
-					catch (Exception ex)
-					{
-						FLogger.Log(LogType.Warning, "Kontrolleur: " + ex.Message);
-					}
-				
-				//remove unused targets
-				foreach (var target in FTargets.ToArray())
-					if (target.Value.State == RemoteValueState.Remove)
-						FTargets.Remove(target.Key);
-					else
-						target.Value.InvalidateState();
+				else if (target.State == RemoteValueState.Add)
+				{
+					osc = new OSCMessage("/k/add");
+					osc.Append(target.RuntimeNodePath);
+					osc.Append(target.Name);
+					osc.Append(target.Type);
+					osc.Append(target.Default);
+					osc.Append(target.Minimum);
+					osc.Append(target.Maximum);
+					osc.Append(target.Stepsize);
+					osc.Append(target.Value);
+					bundle.Append(osc);
+				}
+				else if (FAllowUpdates && target.State == RemoteValueState.Update)
+				{
+					osc = new OSCMessage("/k/update");
+					osc.Append(target.RuntimeNodePath);
+					osc.Append(target.Name);
+					osc.Append(target.Type);
+					osc.Append(target.Default);
+					osc.Append(target.Minimum);
+					osc.Append(target.Maximum);
+					osc.Append(target.Stepsize);
+					osc.Append(target.Value);
+					bundle.Append(osc);
+				}
 			}
 			
-			//return patch messages
-			FPatchOut.AssignFrom(FMessages.Keys);
-			FMessageOut.AssignFrom(FMessages.Values);
+			if (FOSCTransmitter != null)
+				try
+			{
+				FOSCTransmitter.Send(bundle);
+			}
+			catch (Exception ex)
+			{
+				FLogger.Log(LogType.Warning, "Kontrolleur: " + ex.Message);
+			}
+			
+			//remove unused targets
+			foreach (var target in FTargets.ToArray())
+				if (target.Value.State == RemoteValueState.Remove)
+			{
+				target.Value.Kill();
+				FTargets.Remove(target.Key);
+			}
+			
+			else
+				target.Value.InvalidateState();
 		}
-		#endregion MainLoop
 	}
+	#endregion MainLoop
 }
