047ea25d746a39cebdf0c358ad8b53ff4c8c4891
joreg <joreg@vvvv.org>
2012-03-06 20:22:13 +0100

    added ContainsExposedNodes() to INode2

--- vvvv45/src/core/PluginInterfaces/V2/Graph/INode2.cs
+++ vvvv45/src/core/PluginInterfaces/V2/Graph/INode2.cs
@@ -118,7 +118,6 @@ namespace VVVV.PluginInterfaces.V2.Graph
 			return (node.InnerStatus & (StatusCode.IsMissing | StatusCode.HasInvalidData | StatusCode.HasRuntimeError)) > 0;
         }
         
-        
         public static bool IsConnected(this INode2 node)
 		{
 			return (node.InnerStatus & StatusCode.IsConnected) == StatusCode.IsConnected;
@@ -137,6 +136,11 @@ namespace VVVV.PluginInterfaces.V2.Graph
         public static bool IsExposed(this INode2 node)
 		{
 			return (node.Status & StatusCode.IsExposed) == StatusCode.IsExposed;
+		}
+        
+        public static bool ContainsExposedNodes(this INode2 node)
+		{
+			return (node.InnerStatus & StatusCode.IsExposed) == StatusCode.IsExposed;
 		}
         
         public static bool HasProblem(this INode2 node)
