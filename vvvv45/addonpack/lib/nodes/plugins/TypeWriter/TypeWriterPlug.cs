b33387abc9bd64c01021fe7a29dcf9fcd83d51c3
joreg <joreg@vvvv.org>
2012-03-12 17:10:36 +0100

    further cleanup

--- vvvv45/addonpack/lib/nodes/plugins/TypeWriter/TypeWriterPlug.cs
+++ vvvv45/addonpack/lib/nodes/plugins/TypeWriter/TypeWriterPlug.cs
@@ -24,9 +24,9 @@ namespace TypeWriter
 	            Category = "String",
 				Credits = "bo27, Yuri Dolgov",
 				Author = "vvvv group",
-	            Help = "",
-	            Tags = "")]
-	public class TypeWriterPlugin : IPluginEvaluate
+	            Help = "Takes all keyboardinput including keyboard commands and returns a resulting string",
+	            Tags = "keyboard")]
+	public class TypeWriterPlugin: IPluginEvaluate
 	{
 		[DllImport("user32.dll")]
 		static extern int ToUnicodeEx(uint wVirtKey, uint wScanCode, byte[] lpKeyState, [Out, MarshalAs(UnmanagedType.LPWStr)] System.Text.StringBuilder pwszBuff, int cchBuff, uint wFlags, IntPtr dwhkl);
@@ -89,9 +89,6 @@ namespace TypeWriter
 		[Input("KeyCode", DefaultValue = 0)]
 		IDiffSpread<uint> FInputKey;
 		
-		[Input("Doubles Speed", MinValue = 0, MaxValue = 1)]
-		IDiffSpread<double> FDoublesSpeed;
-		
 		[Input("Initial Text", IsSingle = true)]
 		ISpread<string> FInitialText;
 		
@@ -111,114 +108,24 @@ namespace TypeWriter
 		[Import()]
 		ILogger Flogger;
 		
-		public string FText;
-		/// <summary>
-		/// Входная строка
-		/// </summary>
-		public string KbdInput = string.Empty;
-		/// <summary>
-		/// Скорость повтора
-		/// </summary>
-		public double DSpeed = 0;
-		/// <summary>
-		/// Контрольный ключ
-		/// </summary>
-		public string PKey = string.Empty;
-		/// <summary>
-		/// запоминаем введенный символ
-		/// </summary>
-		private string LastChar;
-		/// <summary>
-		/// последняя команда
-		/// </summary>
-		private uint LastScanCode;
-		private uint DrebezgScanCode;
-		private int FCursorCharPos = 0;
+		[Import()]
+		IHDEHost FHDEHost;
 		
+		public string FText = "";
+		private int FCursorCharPos = 0;
 		//defined as string but must be only one character:
 		private string FNewlineSymbol = Environment.NewLine;
-
-		// Делегат, вызывающий конец паузы по таймеру
-		TimerCallback timerDelegate;
-		/// <summary>
-		/// Переинициализация модема
-		/// </summary>
-		private System.Threading.Timer FRepeatTimer;
-		private int timerdelay = 0;
+		private Dictionary<uint, double> FBufferedCommands = new Dictionary<uint, double>();
+		private Dictionary<string, double> FBufferedKeys = new Dictionary<string, double>();
 		
 		// Track whether Dispose has been called.
 		private bool FDisposed = false;
 		#endregion field declaration
 
-		#region constructor/destructor
-		public TypeWriterPlugin()
-		{
-//			timerDelegate = new TimerCallback(RepeatChar);
-//			FRepeatTimer = new System.Threading.Timer(timerDelegate, 1, Timeout.Infinite, Timeout.Infinite);
-			
-			FText = "";
-		}
-
-		// Implementing IDisposable's Dispose method.
-		// Do not make this method virtual.
-		// A derived class should not be able to override this method.
-		public void Dispose()
-		{
-			Dispose(true);
-			// Take yourself off the Finalization queue
-			// to prevent finalization code for this object
-			// from executing a second time.
-			GC.SuppressFinalize(this);
-		}
-
-		// Dispose(bool disposing) executes in two distinct scenarios.
-		// If disposing equals true, the method has been called directly
-		// or indirectly by a user's code. Managed and unmanaged resources
-		// can be disposed.
-		// If disposing equals false, the method has been called by the
-		// runtime from inside the finalizer and you should not reference
-		// other objects. Only unmanaged resources can be disposed.
-		protected virtual void Dispose(bool disposing)
-		{
-			// Check to see if Dispose has already been called.
-			if (!FDisposed)
-			{
-				if (disposing)
-				{
-					// Dispose managed resources.
-					FRepeatTimer.Change(Timeout.Infinite, Timeout.Infinite);
-					FRepeatTimer.Dispose();
-				}
-				// Release unmanaged resources. If disposing is false,
-				// only the following code is executed.
-
-				//Flogger.Log(LogType.Message, "TypeWriter is being deleted");
-
-				// Note that this is not thread safe.
-				// Another thread could start disposing the object
-				// after the managed resources are disposed,
-				// but before the disposed flag is set to true.
-				// If thread safety is necessary, it must be
-				// implemented by the client.
-			}
-			FDisposed = true;
-		}
-
-		// Use C# destructor syntax for finalization code.
-		// This destructor will run only if the Dispose method
-		// does not get called.
-		// It gives your base class the opportunity to finalize.
-		// Do not provide destructors in types derived from this class.
-		~TypeWriterPlugin()
-		{
-			// Do not re-create Dispose clean-up code here.
-			// Calling Dispose(false) is optimal in terms of
-			// readability and maintainability.
-			Dispose(false);
-		}
-		#endregion constructor/destructor
-
 		#region Text navigation helper
+		//todo:
+		//text splitting is done here every call,
+		//should be done globally
 		int GetLineCount()
 		{
 			var lines = FText.Split(new string[] {FNewlineSymbol}, StringSplitOptions.None);
@@ -368,38 +275,14 @@ namespace TypeWriter
 		}
 		#endregion
 
-		#region таймеры дребезга и повтора
-		/// <summary>
-		/// Повтор символа
-		/// </summary>
-		/// <param name="stateInfo"></param>
-		/*public void RepeatChar(Object stateInfo)
-		{
-			try
-			{
-				int sInfo = 0;
-				sInfo = (int)stateInfo;
-
-				if (sInfo == 1)
-				{
-					if (LastChar != null)
-					{
-						NewCommand(LastChar);
-//						Invalidate = true;
-					}
-					if (LastScanCode != 0)
-					{
-						RunCommand(LastScanCode);
-//						Invalidate = true;
-					}
-				}
-			}
-			catch { };
-		}*/
-
-		#endregion
-
 		#region Commands
+		private void AddNewChar(string str)
+		{
+			FText = FText.Insert(FCursorCharPos, str);
+				
+			CursorStepsRight();
+		}
+		
 		private void DeleteLeftChar()
 		{
 			if (FCursorCharPos > 0)
@@ -424,15 +307,18 @@ namespace TypeWriter
 			{
 				switch ((Keys)ScanCode)
 				{
+					case 0:
+						return true;
+					case Keys.Space:
+						AddNewChar(" ");
+						return true;
 					case Keys.Return:
 						AddNewChar(FNewlineSymbol);
 						return true;
 					case Keys.Back:
-						LastScanCode = ScanCode;
 						DeleteLeftChar();
 						return true;
 					case Keys.Delete:
-						LastScanCode = ScanCode;
 						DeleteRightChar();
 						return true;
 					case Keys.Tab:
@@ -477,136 +363,63 @@ namespace TypeWriter
 			catch { };
 			return false;
 		}
-
-		private void AddNewChar(string str)
-		{
-			FText = FText.Insert(FCursorCharPos, str);
-			LastChar = str;
-				
-			CursorStepsRight();
-		}
 		#endregion
 
 		//here we go, thats the method called by vvvv each frame
 		//all data handling should be in here
 		public void Evaluate(int SpreadMax)
 		{
-			#region входные параметры
-			
             if (FSetCursorPosition.IsChanged)
 				FCursorCharPos = Math.Min(FText.Length, Math.Max(0, FSetCursorPosition[0]));
-
-			/*
-            if (FDoublesSpeed.IsChanged)
-            {
-                DSpeed = FDoublesSpeed[0];
-                // Сбрасываем сторожевой таймер
-                if (RepeatTimer != null)
-                {
-                    if (DSpeed == 0)
-                    {
-                        RepeatTimer.Change(Timeout.Infinite, Timeout.Infinite);
-                        timerdelay = 0;
-                    }
-                    else
-                    {
-                        timerdelay = Convert.ToInt32(DSpeed * 1000);
-                        if (timerdelay > 0)
-                        {
-                            if (timerdelay < 200) timerdelay = 200;
-                            RepeatTimer.Change(timerdelay, timerdelay);
-                        }
-                    }
-                }
-            }
-			 */
-			#endregion
 
 			//initializing with text
 			if (FInitialize[0])
 			{
 				FText = FInitialText[0];
 				CursorToTextEnd();
-
-				LastChar = null;
-				LastScanCode = 0;
-				DrebezgScanCode = 0;
-//				FRepeatTimer.Change(Timeout.Infinite, Timeout.Infinite);
 			}
-
-			//key input
-			if (FInputKey.IsChanged)
+			
+			if (FInputKey[0] == 0 
+			|| (FInputKey.SliceCount == 1 && FInputKey[0] == (uint)Keys.ControlKey)
+			|| (FInputKey.SliceCount == 1 && FInputKey[0] == (uint)Keys.ShiftKey))
 			{
-//				Flogger.Log(LogType.Debug, FInputKey[0].ToString());
-				if (FInputKey.SliceCount > 0)
-				{
-					
-//					double a;
-//					/// проверка сканкода
-//					if (LastScanCode != 0 || LastChar != null)
-//					{
-//						bool flagScanCode = false;
-//						bool flagLastChar = false;
-//						
-//						for (int i = 0; i < FInputKey.SliceCount; i++)
-//						{
-//							a = FInputKey[i];
-//							uint ScanCode = Convert.ToUInt16(a);
-//							if (ScanCode == LastScanCode)
-//								flagScanCode = true;
-//							string newchar = VKCodeToUnicode(ScanCode);
-//							if (newchar == LastrstChar)
-//								flagLastChar = true;
-//						}
-//						
-//						if (flagScanCode == false)
-//							LastScanCode = 0;
-//						if (flagLastChar == false)
-//							LastChar = null;
-//					}
-					
-					for (int i = 0; i < FInputKey.SliceCount; i++)
-					{
-//						a = FInputKey[i];
-
-//						uint ScanCode = Convert.ToUInt16(a);
-//						/// все кнопки отпущены
-//						if (FInputKey.SliceCount == 1 && ScanCode == 0)
-//						{
-//							LastChar = null;
-//							LastScanCode = 0;
-//							DrebezgScanCode = 0;
-////							FRepeatTimer.Change(Timeout.Infinite, Timeout.Infinite);
-//						}
-						
-						string newchar = VKCodeToUnicode(FInputKey[i]);
-						Flogger.Log(LogType.Debug, newchar);
-						
-//						if (DrebezgScanCode == 0)
-//						{
-//							if (newchar != "" && IsKeyPressed((Keys)ScanCode))
-//								DrebezgScanCode = ScanCode;
-//						}
-//						else
-//						{
-//							if (newchar != "" && DrebezgScanCode == ScanCode)
-//								return;
-//						}
-//						
-//						if (timerdelay != 0)
-//						{
-////							if (IsKeyPressed((Keys)ScanCode))
-////								FRepeatTimer.Change(timerdelay, timerdelay);
-//						}
-						
-						if (RunCommand(FInputKey[i]))
-							continue;
-						
-						if (!string.IsNullOrEmpty(newchar) && newchar[0] > 31)
-							AddNewChar(newchar);
-					}
-				}
+				FBufferedKeys.Clear();
+				FBufferedCommands.Clear();
 			}
+			else
+			{
+				var now = FHDEHost.GetCurrentTime();
+				//make current keyboardstate i1!1nto a string
+				//access FInputKey[-1] since earlier slices would be ctrl or shift
+				var newChar = VKCodeToUnicode(FInputKey[-1]);
+				
+				//if this is a command
+				if (FInputKey[-1] < 48 || string.IsNullOrEmpty(newChar.Trim()))
+				{
+					//add this to buffered commands
+					if (FInputKey[-1] != 17)
+						if (!FBufferedCommands.ContainsKey(FInputKey[-1]))
+							FBufferedCommands.Add(FInputKey[-1], now);
+
+					FBufferedKeys.Clear();
+				}
+				else //this is a string
+				{
+					if (!FBufferedKeys.ContainsKey(newChar))
+						FBufferedKeys.Add(newChar, now);
+					
+					FBufferedCommands.Clear();
+				}
+					
+				//check if any of the buffered keys/commands need to executed
+				foreach (var key in FBufferedKeys)
+					if (now - key.Value == 0 || now - key.Value > 0.4)
+						AddNewChar(key.Key);
+				
+				foreach (var key in FBufferedCommands)
+					if (now - key.Value == 0 || now - key.Value > 0.4)
+						RunCommand(key.Key);
+			}	
 			
 			FOutput[0] = FText;
 			FCursorPosition[0] = FCursorCharPos;
