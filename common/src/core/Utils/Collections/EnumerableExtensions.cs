2784a6549dbc785e115855f36d782c6e136eb805
Elias Holzer <elias@vvvv.org>
2012-05-04 20:56:56 +0200

    Added extension method IEnumerable<U>.OfType<T>().

---
+++ common/src/core/Utils/Collections/EnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VVVV.Utils.Collections
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> OfType<T, U>(this IEnumerable<U> enumerable) where T : class
+        {
+            return enumerable.Where(e => e is T).Select(e => e as T);
+        }
+    }
+}
