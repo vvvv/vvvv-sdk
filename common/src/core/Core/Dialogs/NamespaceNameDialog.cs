b85c5c9536258d22bdd66f41e586f1f8f4563b73
Elias Holzer <elias@vvvv.org>
2012-05-25 17:51:58 +0200

    Allow custom input handling in name dialog.
    Renamed Addition -> +, Subtraction -> -, etc.

--- common/src/core/Core/Dialogs/NamespaceNameDialog.cs
+++ common/src/core/Core/Dialogs/NamespaceNameDialog.cs
@@ -9,29 +9,9 @@ namespace VVVV.Core.Dialogs
 {
     public partial class NamespaceNameDialog : NameDialog
     {
-        public NamespaceNameDialog(Point loc)
-            : base(loc)
+        public NamespaceNameDialog(string initText = "Name", Func<string, string, char, bool> isValidChar = null)
+            : base(initText, isValidChar)
         {
-            InitializeComponent();
-
-            FInitialText = TextBoxName.Text;
-        }
-
-        protected override bool AllowCharacter(char chr)
-        {
-            if (TextBoxName.Text == "" || TextBoxName.Text == FInitialText)
-            {
-                return Char.IsLetter(chr) ||
-                    Char.IsControl(chr);
-            }
-            else
-            {
-                return Char.IsLetterOrDigit(chr) ||
-                    chr == '_' ||
-                    chr == '.' ||
-                    Char.IsControl(chr);
-            }
-
         }
 
         //check if pasted text is ok
