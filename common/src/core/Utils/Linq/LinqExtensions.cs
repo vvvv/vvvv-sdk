3b206805d464a7f297a1d2f1789dd91f27527142
Elias Holzer <elias@vvvv.org>
2012-07-20 21:14:33 +0200

    All GLSL functions and types get imported by xml file. Need to write a proper mapping mechanism now...

--- common/src/core/Utils/Linq/LinqExtensions.cs
+++ common/src/core/Utils/Linq/LinqExtensions.cs
@@ -45,5 +45,25 @@ namespace VVVV.Utils.Linq
             // none means: tell me if there is no element
             return !enumerable.Any();
         }
+
+        // From: http://stackoverflow.com/questions/5039617/turning-an-ienumerableienumerablet-90-degrees
+        public static IEnumerable<IEnumerable<T>> Transpose<T>(this IEnumerable<IEnumerable<T>> source)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            var enumerators = source.Select(x => x.GetEnumerator()).ToArray();
+            try
+            {
+                while (enumerators.All(x => x.MoveNext()))
+                {
+                    yield return enumerators.Select(x => x.Current).ToArray();
+                }
+            }
+            finally
+            {
+                foreach (var enumerator in enumerators)
+                    enumerator.Dispose();
+            }
+        }
+
 	}
 }
