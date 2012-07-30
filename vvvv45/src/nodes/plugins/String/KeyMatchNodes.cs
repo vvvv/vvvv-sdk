6f66fe480d6eef57ad11c2bab3d19c6c11ee082c
joreg <joreg@vvvv.org>
2012-07-30 17:36:03 +0200

    initial version of new keymatch nodes

---
+++ vvvv45/src/nodes/plugins/String/KeyMatchNodes.cs
@@ -0,0 +1,184 @@
+#region usings
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+
+using VVVV.PluginInterfaces.V1;
+using VVVV.PluginInterfaces.V2;
+using VVVV.Utils.VColor;
+using VVVV.Utils.VMath;
+using VVVV.Utils.IO;
+
+using VVVV.Core.Logging;
+#endregion usings
+
+namespace VVVV.Nodes
+{
+	#region PluginInfo
+	[PluginInfo(Name = "KeyMatch", 
+	            Category = "String",
+	            Version = "State",
+	            Help = "Detects pressed keys when connected with a Keyboard Node. Use the inspector to specify the keys to check.",
+	            AutoEvaluate = true,
+				Tags = "")]
+	#endregion PluginInfo
+	public class KeyMatchNode: IPluginEvaluate
+	{
+		enum KeyMode {Press, Bang, Toggle};
+		
+		#region fields & pins
+		[Config("Key Match", IsSingle = true)]
+		IDiffSpread<string> FKeyMatch;
+		
+		[Input("Input")]
+		IDiffSpread<KeyboardState> FInput;
+		
+		[Input("Reset Toggle", IsBang = true)]
+		ISpread<bool> FReset;
+		
+		[Input("Key Mode")]
+		ISpread<KeyMode> FKeyMode;
+
+		[Import()]
+		ILogger FLogger;
+		
+		[Import()]
+		IIOFactory FIOFactory;
+ 		
+		Dictionary<string, ISpread<bool>> FOutputs = new Dictionary<string, ISpread<bool>>();
+		Dictionary<string, bool> FLastFrame = new Dictionary<string, bool>();
+		#endregion fields & pins
+
+		//called when data for any output pin is requested
+		public void Evaluate(int SpreadMax)
+		{
+			//update outputs
+			if (FKeyMatch.IsChanged)
+			{
+				var keys = FKeyMatch[0].Split(',').ToList().Select(s => s.Trim());
+		
+				//add new pins
+				foreach (var key in keys)
+				{;
+					var lowerKey = key.ToLower();
+					if (!string.IsNullOrWhiteSpace(key) && !FOutputs.ContainsKey(lowerKey))
+					{
+						var outAttr = new OutputAttribute(key);
+						outAttr.IsSingle = true;
+						var spread = FIOFactory.CreateSpread<bool>(outAttr, true);
+						FOutputs.Add(lowerKey, spread);
+						FLastFrame.Add(lowerKey, false);
+					}
+				}
+				
+				//remove obsolete pins
+				var toDelete = new List<string>();
+				foreach (var key in FOutputs.Keys)
+				{
+					if (!keys.Contains(key))
+						toDelete.Add(key);	
+				}
+				
+				foreach (var pin in toDelete)
+				{
+					FOutputs.Remove(pin);
+					FLastFrame.Remove(pin);
+					FLogger.Log(LogType.Debug, "removed: " + pin);
+				}	
+			}
+			
+			//reset outputs
+			if ((FKeyMode[0] == KeyMode.Press) || (FKeyMode[0] == KeyMode.Bang) || FReset[0])
+				for (int i = 0; i < FOutputs.Count; i++)
+				{
+					var key = FOutputs.ElementAt(i).Key;
+					FOutputs[key][0] = false;
+				}	
+			
+			//set active outputs
+			if (FInput[0] != null && FInput.IsChanged)
+			{
+				var currentKeys = FInput[0].KeyCodes.Select(k => k.ToString().ToLower());
+				
+				foreach (var key in FLastFrame.Keys)
+				{
+					switch (FKeyMode[0])
+					{
+						case KeyMode.Press:
+						{
+							FOutputs[key][0] = currentKeys.Contains(key);
+							break;
+						}
+						case KeyMode.Bang:
+						{
+							if (!FLastFrame[key] && currentKeys.Contains(key))
+								FOutputs[key][0] = true;
+							break;
+						}	
+						case KeyMode.Toggle:
+						{
+							if (!FLastFrame[key] && currentKeys.Contains(key))
+								FOutputs[key][0] = !FOutputs[key][0];
+							break;
+						}
+					}
+				}
+
+				//save last frames state
+				for (int i = 0; i < FLastFrame.Count; i++)
+				{
+					var key = FLastFrame.ElementAt(i).Key;
+					FLastFrame[key] = currentKeys.Contains(key);
+				}
+			}
+		}
+	}
+	
+	#region PluginInfo
+	[PluginInfo(Name = "RadioKeyMatch", 
+	            Category = "String",
+	            Version = "State",
+	            Help = "Similiar to KeyMatch, but does not create a output pin for each key to check, but returns the index of the pressed key on its output pin.",
+	            AutoEvaluate = true,
+				Tags = "")]
+	#endregion PluginInfo
+	public class RadioKeyMatchNode: IPluginEvaluate
+	{
+		#region fields & pins
+		[Input("Input")]
+		IDiffSpread<KeyboardState> FInput;
+
+		[Input("Key Match")]
+		IDiffSpread<string> FKeyMatch;
+		
+		[Output("Output")]
+		ISpread<int> FOutput;
+
+		[Import()]
+		ILogger FLogger;
+		#endregion fields & pins
+
+		//called when data for any output pin is requested
+		public void Evaluate(int SpreadMax)
+		{
+			FOutput.SliceCount = SpreadMax;
+			
+			if (FInput[0] != null)
+				if (FInput.IsChanged || FKeyMatch.IsChanged)
+				{
+					for (int i = 0; i < SpreadMax; i++)
+					{
+						if (FInput[i].KeyCodes.Count > 0)
+						{
+							var matcher = FKeyMatch[i].Split(',').ToList().Select(s => s.ToLower().Trim()).ToList();
+							var key = FInput[i].KeyCodes[0].ToString().ToLower();
+							var index = matcher.IndexOf(key);
+							if (index >= 0)
+								FOutput[i] = index;
+						}
+					}
+				}
+		}
+	}
+}
