0a38506ebb095cc5c55894299b929283766de1d3
Elias Holzer <elias@vvvv.org>
2012-10-18 18:28:58 +0200

    Added Encoding/Decoding nodes for testing purposes.

---
+++ common/src/core/Utils/IO/SystemIOExtensions.cs
@@ -0,0 +1,165 @@
+﻿ 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.IO
+{
+    public static class SystemIOExtensions
+    {
+        public static byte[] ReadBytes(this BinaryReader reader, int fieldSize, ByteOrder byteOrder)
+        {
+            byte[] bytes = new byte[fieldSize];
+            if (byteOrder == ByteOrder.LittleEndian)
+                return reader.ReadBytes(fieldSize);
+            else
+            {
+                for (int i = fieldSize - 1; i > -1; i--)
+                    bytes[i] = reader.ReadByte();
+                return bytes;
+            }
+        }
+
+        public static void Write(this BinaryWriter writer, byte[] value, ByteOrder byteOrder)
+        {
+            if (byteOrder == ByteOrder.LittleEndian)
+                writer.Write(value);
+            else
+            {
+                for (int i = value.Length - 1; i > -1; i--)
+                    writer.Write(value[i]);
+            }
+        }
+
+        public static Int16 ReadInt16(this BinaryReader reader, ByteOrder byteOrder)
+        {
+            if (byteOrder == ByteOrder.LittleEndian)
+                return (Int16)reader.ReadInt16();
+            else
+                return BitConverter.ToInt16(ReadBytes(reader, sizeof(Int16), ByteOrder.BigEndian), 0);
+        }
+
+        public static void Write(this BinaryWriter writer, Int16 value, ByteOrder byteOrder)
+        {
+            if (byteOrder == ByteOrder.LittleEndian)
+                writer.Write(value);
+            else
+                writer.Write(BitConverter.GetBytes(value), byteOrder);
+        }
+
+        public static UInt16 ReadUInt16(this BinaryReader reader, ByteOrder byteOrder)
+        {
+            if (byteOrder == ByteOrder.LittleEndian)
+                return (UInt16)reader.ReadUInt16();
+            else
+                return BitConverter.ToUInt16(ReadBytes(reader, sizeof(UInt16), ByteOrder.BigEndian), 0);
+        }
+
+        public static void Write(this BinaryWriter writer, UInt16 value, ByteOrder byteOrder)
+        {
+            if (byteOrder == ByteOrder.LittleEndian)
+                writer.Write(value);
+            else
+                writer.Write(BitConverter.GetBytes(value), byteOrder);
+        }
+
+        public static Int32 ReadInt32(this BinaryReader reader, ByteOrder byteOrder)
+        {
+            if (byteOrder == ByteOrder.LittleEndian)
+                return (Int32)reader.ReadInt32();
+            else
+                return BitConverter.ToInt32(ReadBytes(reader, sizeof(Int32), ByteOrder.BigEndian), 0);
+        }
+
+        public static void Write(this BinaryWriter writer, Int32 value, ByteOrder byteOrder)
+        {
+            if (byteOrder == ByteOrder.LittleEndian)
+                writer.Write(value);
+            else
+                writer.Write(BitConverter.GetBytes(value), byteOrder);
+        }
+
+        public static UInt32 ReadUInt32(this BinaryReader reader, ByteOrder byteOrder)
+        {
+            if (byteOrder == ByteOrder.LittleEndian)
+                return (UInt32)reader.ReadUInt32();
+            else
+                return BitConverter.ToUInt32(ReadBytes(reader, sizeof(UInt32), ByteOrder.BigEndian), 0);
+        }
+
+        public static void Write(this BinaryWriter writer, UInt32 value, ByteOrder byteOrder)
+        {
+            if (byteOrder == ByteOrder.LittleEndian)
+                writer.Write(value);
+            else
+                writer.Write(BitConverter.GetBytes(value), byteOrder);
+        }
+
+        public static Int64 ReadInt64(this BinaryReader reader, ByteOrder byteOrder)
+        {
+            if (byteOrder == ByteOrder.LittleEndian)
+                return (Int64)reader.ReadInt64();
+            else
+                return BitConverter.ToInt64(ReadBytes(reader, sizeof(Int64), ByteOrder.BigEndian), 0);
+        }
+
+        public static void Write(this BinaryWriter writer, Int64 value, ByteOrder byteOrder)
+        {
+            if (byteOrder == ByteOrder.LittleEndian)
+                writer.Write(value);
+            else
+                writer.Write(BitConverter.GetBytes(value), byteOrder);
+        }
+
+        public static UInt64 ReadUInt64(this BinaryReader reader, ByteOrder byteOrder)
+        {
+            if (byteOrder == ByteOrder.LittleEndian)
+                return (UInt64)reader.ReadUInt64();
+            else
+                return BitConverter.ToUInt64(ReadBytes(reader, sizeof(UInt64), ByteOrder.BigEndian), 0);
+        }
+
+        public static void Write(this BinaryWriter writer, UInt64 value, ByteOrder byteOrder)
+        {
+            if (byteOrder == ByteOrder.LittleEndian)
+                writer.Write(value);
+            else
+                writer.Write(BitConverter.GetBytes(value), byteOrder);
+        }
+
+        public static Single ReadSingle(this BinaryReader reader, ByteOrder byteOrder)
+        {
+            if (byteOrder == ByteOrder.LittleEndian)
+                return (Single)reader.ReadSingle();
+            else
+                return BitConverter.ToSingle(ReadBytes(reader, sizeof(Single), ByteOrder.BigEndian), 0);
+        }
+
+        public static void Write(this BinaryWriter writer, Single value, ByteOrder byteOrder)
+        {
+            if (byteOrder == ByteOrder.LittleEndian)
+                writer.Write(value);
+            else
+                writer.Write(BitConverter.GetBytes(value), byteOrder);
+        }
+
+        public static Double ReadDouble(this BinaryReader reader, ByteOrder byteOrder)
+        {
+            if (byteOrder == ByteOrder.LittleEndian)
+                return (Double)reader.ReadDouble();
+            else
+                return BitConverter.ToDouble(ReadBytes(reader, sizeof(Double), ByteOrder.BigEndian), 0);
+        }
+
+        public static void Write(this BinaryWriter writer, Double value, ByteOrder byteOrder)
+        {
+            if (byteOrder == ByteOrder.LittleEndian)
+                writer.Write(value);
+            else
+                writer.Write(BitConverter.GetBytes(value), byteOrder);
+        }
+
+    }
+}
+ 
---
+++ vvvv45/src/nodes/plugins/Network/Encoding.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Linq;
+
+using VVVV.PluginInterfaces.V1;
+using VVVV.PluginInterfaces.V2;
+using VVVV.Utils.VColor;
+using VVVV.Utils.VMath;
+
+using VVVV.Core.Logging;
+using System.Collections.Generic;
+using System.IO;
+
+namespace VVVV.Nodes.Network
+{
+    // Compatible with System.TypeCode
+    public enum Format
+    {
+        Empty = 0,
+        Boolean = 3,
+        SByte = 5,
+        Byte = 6,
+        Int16 = 7,
+        UInt16 = 8,
+        Int32 = 9,
+        UInt32 = 10,
+        Int64 = 11,
+        UInt64 = 12,
+        Single = 13,
+        Double = 14
+    }
+
+    public abstract class EncodingNode : IPluginEvaluate
+    {
+        [Input("Encoding", EnumName = "Encoding", Visibility = PinVisibility.OnlyInspector)]
+        protected ISpread<EnumEntry> FEncodingIn;
+
+        [Input("Byte Order", Visibility = PinVisibility.OnlyInspector)]
+        protected ISpread<ByteOrder> FByteOrder;
+
+        static bool registered;
+        static EncodingNode()
+        {
+            if (!registered)
+            {
+                registered = true;
+
+                var encodings = Encoding.GetEncodings()
+                    .Select(info => info.Name)
+                    .OrderBy(n => n)
+                    .ToArray();
+                EnumManager.UpdateEnum("Encoding", "Windows-1252", encodings);
+            }
+        }
+
+        public abstract void Evaluate(int spreadMax);
+    }
+
+    [PluginInfo(Name = "Encode", Category = "Network", Help = "Basic template with one string in/out", Tags = "")]
+    public class ValueEncodeNode : EncodingNode
+    {
+        [Input("Input")]
+        ISpread<ISpread<double>> FInput;
+
+        [Input("Format")]
+        ISpread<ISpread<Format>> FFormat;
+
+        [Output("Output")]
+        ISpread<string> FOutput;
+
+        [Output("Offset")]
+        ISpread<ISpread<int>> FOffset;
+
+        //called when data for any output pin is requested
+        public override void Evaluate(int spreadMax)
+        {
+            FOutput.SliceCount = FOffset.SliceCount = FInput.CombineWith(FFormat).CombineWith(FEncodingIn);
+
+            for (int i = 0; i < FOutput.SliceCount; i++)
+            {
+                spreadMax = FInput[i].CombineWith(FFormat[i]);
+                FOffset[i].SliceCount = spreadMax;
+
+                var encoding = Encoding.GetEncoding(FEncodingIn[i].Name);
+                var input = FInput[i];
+                var format = FFormat[i];
+                var resultStream = new MemoryStream();
+                var binaryWriter = new BinaryWriter(resultStream);
+                for (int j = 0; j < spreadMax; j++)
+                {
+                    FOffset[i][j] = (int)resultStream.Position;
+                    var f = format[j];
+                    if (f == Format.Empty) continue;
+                    var convertedValue = Convert.ChangeType(input[j], (TypeCode)f);
+                    switch (f)
+                    {
+                        case Format.Empty:
+                            break;
+                        case Format.Boolean:
+                            binaryWriter.Write((Boolean)convertedValue);
+                            break;
+                        case Format.SByte:
+                            binaryWriter.Write((SByte)convertedValue);
+                            break;
+                        case Format.Byte:
+                            binaryWriter.Write((Byte)convertedValue);
+                            break;
+                        case Format.Int16:
+                            binaryWriter.Write((Int16)convertedValue, FByteOrder[i]);
+                            break;
+                        case Format.UInt16:
+                            binaryWriter.Write((UInt16)convertedValue, FByteOrder[i]);
+                            break;
+                        case Format.Int32:
+                            binaryWriter.Write((Int32)convertedValue, FByteOrder[i]);
+                            break;
+                        case Format.UInt32:
+                            binaryWriter.Write((UInt32)convertedValue, FByteOrder[i]);
+                            break;
+                        case Format.Int64:
+                            binaryWriter.Write((Int64)convertedValue, FByteOrder[i]);
+                            break;
+                        case Format.UInt64:
+                            binaryWriter.Write((UInt64)convertedValue, FByteOrder[i]);
+                            break;
+                        case Format.Single:
+                            binaryWriter.Write((Single)convertedValue, FByteOrder[i]);
+                            break;
+                        case Format.Double:
+                            binaryWriter.Write((Double)convertedValue, FByteOrder[i]);
+                            break;
+                        default:
+                            throw new NotImplementedException();
+                    }
+                }
+                FOutput[i] = encoding.GetString(resultStream.GetBuffer(), 0, (int)resultStream.Length);
+            }
+        }
+    }
+
+    [PluginInfo(Name = "Decode", Category = "Network", Help = "Basic template with one string in/out", Tags = "")]
+    public class ValueDecodeNode : EncodingNode
+    {
+        [Input("Input")]
+        ISpread<string> FInput;
+
+        [Input("Offset")]
+        ISpread<ISpread<int>> FOffset;
+
+        [Input("Format")]
+        ISpread<ISpread<Format>> FFormat;
+
+        [Output("Output")]
+        ISpread<ISpread<double>> FOutput;
+
+        //called when data for any output pin is requested
+        public unsafe override void Evaluate(int spreadMax)
+        {
+            FOutput.SliceCount = FInput.CombineWith(FOffset).CombineWith(FFormat).CombineWith(FEncodingIn);
+
+            for (int i = 0; i < FOutput.SliceCount; i++)
+            {
+                var encoding = Encoding.GetEncoding(FEncodingIn[i].Name);
+                var inputStream = new MemoryStream(encoding.GetBytes(FInput[i]), false);
+                var offset = FOffset[i];
+                var format = FFormat[i];
+                var output = FOutput[i];
+                spreadMax = offset.CombineWith(format);
+                output.SliceCount = spreadMax;
+
+                var binaryReader = new BinaryReader(inputStream);
+                for (int j = 0; j < spreadMax; j++)
+                {
+                    var f = format[j];
+                    if (f == Format.Empty) continue;
+                    inputStream.Position = offset[j];
+                    object value = null;
+                    switch (f)
+                    {
+                        case Format.Empty:
+                            break;
+                        case Format.Boolean:
+                            value = binaryReader.ReadBoolean();
+                            break;
+                        case Format.SByte:
+                            value = binaryReader.ReadSByte();
+                            break;
+                        case Format.Byte:
+                            value = binaryReader.ReadByte();
+                            break;
+                        case Format.Int16:
+                            value = binaryReader.ReadInt16(FByteOrder[i]);
+                            break;
+                        case Format.UInt16:
+                            value = binaryReader.ReadUInt16(FByteOrder[i]);
+                            break;
+                        case Format.Int32:
+                            value = binaryReader.ReadInt32(FByteOrder[i]);
+                            break;
+                        case Format.UInt32:
+                            value = binaryReader.ReadUInt32(FByteOrder[i]);
+                            break;
+                        case Format.Int64:
+                            value = binaryReader.ReadInt64(FByteOrder[i]);
+                            break;
+                        case Format.UInt64:
+                            value = binaryReader.ReadUInt64(FByteOrder[i]);
+                            break;
+                        case Format.Single:
+                            value = binaryReader.ReadSingle(FByteOrder[i]);
+                            break;
+                        case Format.Double:
+                            value = binaryReader.ReadDouble(FByteOrder[i]);
+                            break;
+                        default:
+                            throw new NotImplementedException();
+                    }
+                    output[j] = (double)Convert.ChangeType(value, TypeCode.Double);
+                }
+            }
+        }
+    }
+}
