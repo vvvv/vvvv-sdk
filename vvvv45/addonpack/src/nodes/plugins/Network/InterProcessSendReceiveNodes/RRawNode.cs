f9caea5ac0bed20a58a629851afd70d9e71e30af
Frederik Tilkin <frederik_tilkin@hotmail.com>
2013-02-03 20:00:58 +0100

    UNFINISHED: added S/R Raw nodes

---
+++ vvvv45/addonpack/src/nodes/plugins/Network/InterProcessSendReceiveNodes/RRawNode.cs
@@ -0,0 +1,188 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: frederik
+ * Date: 1/02/2013
+ * Time: 11:13
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+#region usings
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using System.ComponentModel.Composition;
+using VVVV.PluginInterfaces.V1;
+using VVVV.PluginInterfaces.V2;
+using VVVV.Utils.VColor;
+using VVVV.Utils.VMath;
+using VVVV.Utils.Streams;
+
+using VVVV.Core.Logging;
+
+using LocalMessageBroadcast;
+
+#endregion usings
+
+
+namespace InterProcessSendReceiveNodes
+{
+	#region PluginInfo
+	[PluginInfo(Name = "R", Category = "Network.Interprocess.Raw", 
+	            AutoEvaluate = false, 
+	            Author="ft", Help = "S/R nodes that allow to communicate between different vvvv instances", Tags = "")]
+	#endregion PluginInfo
+	public class RRawNode : IPluginEvaluate, IDisposable
+	{
+		#region pins
+		[Input("ReceiveString", IsSingle = true, DefaultString = "vvvv")]
+		IDiffSpread<string> FChannelIn;
+
+//		[Input("Default Value")]
+//		IDiffSpread<string> FDefaultValueIn;
+
+//		I don't think we need filtering on partnedId: 
+//		use good names and you can easily filter for the messages you are interested in
+///////////////////////////////////////////////////////////////////////////////////////
+//		[Input("Partner Id", Visibility = PinVisibility.OnlyInspector)]
+//		IDiffSpread<uint> FPartnerIdIn;
+//
+//		[Input("Do Filter", IsSingle = true, Visibility = PinVisibility.OnlyInspector)]
+//		IDiffSpread<bool> FDoFilterIn;
+
+		
+		[Output("Output Value")]
+		IOutStream<Stream> FStreamOut;
+		
+//		[Output("Found", IsSingle = true)]
+//		ISpread<bool> FFoundOut;
+
+
+//		[Output("Test")]
+//		ISpread<Stream> FTestOut;
+		
+		#endregion pins
+
+		#region fields
+		[Import()]
+		ILogger FLogger;
+				
+		private string logMe = "";
+		
+		private LocalMessageBroadcastPartner localMessageBroadcastPartner = null;
+		
+		private bool received = false;
+		IOutStream<Stream> receivedStream;
+		private Dictionary<uint, uint> lastVersionPerPartner = new Dictionary<uint, uint>();
+		
+//		ISpread<Stream> receivedStreamA = new Spread<Stream>();
+//		ISpread<Stream> receivedStreamB = new Spread<Stream>();
+//		private bool selectSpread = false;
+		
+		#endregion fields
+
+		#region helper functions
+		public void LogNow(LogType logType, string message) {
+			FLogger.Log( logType, message);
+		}
+ 
+		public void Log( LogType logType, string message)
+		{
+			logMe += "\n" + (logType == LogType.Error ? "ERR " : (logType == LogType.Warning ? "WARN " : "")) + message;
+		}
+
+		#endregion helper functions
+
+//		public RRawNode() {
+//			receivedStreamA.SliceCount = 1;
+//			receivedStreamA[0] = 0;
+//			receivedStreamB.SliceCount = 1;
+//			receivedStreamB[0] = 1;
+//		}
+		
+		private void OnMessageHandler(uint sendingPartnerId, IntPtr msgData, uint msgLength) {
+//			Log(LogType.Debug, "[R.Raw] Received new message from " + sendingPartnerId + " of size " + msgLength);
+			
+			byte[] bytes = new byte[msgLength];
+			System.Runtime.InteropServices.Marshal.Copy( msgData, bytes, 0, (int)msgLength );
+
+			try {
+				//correct type: not really necessary anymore since different nodes will send on different channels
+				if (Utils.GetMessageType(bytes) == MessageTypeEnum.rawSpread) {
+					//only parse the whole message if it's a version we have never received before from that partner
+					uint currVersion = Utils.GetVersion(bytes);
+					uint lastVersion;
+					
+					if ( ! lastVersionPerPartner.TryGetValue(sendingPartnerId, out lastVersion) || (currVersion != lastVersion) ) {
+						Utils.ProcessMessage( bytes, receivedStream );
+						received = true;
+						lastVersionPerPartner[sendingPartnerId] = currVersion;
+					}
+				}
+			} catch (Exception e) {
+				Log(LogType.Debug, "[R Exception] while trying to processMessage.\n" + e.Message + "\n" + e.StackTrace);
+			}
+
+			//Log(LogType.Debug, "[R.Raw] New spread has " + receivedStream.SliceCount + " slices.");
+
+//			try {
+//				string debug = "";
+//				for ( int i = 0; i < spread.SliceCount; i++ ) {
+//					debug += ( i > 0 ? " | " : "" ) + spread[i];
+//				}
+//				Log(LogType.Debug, "[R.Raw] New spread contains: " + debug );
+//			} catch (Exception e) {
+//				Log(LogType.Debug, "[R Exception] while trying to build debug message" );				
+//			}
+		}
+
+		
+		//called when data for any output pin is requested
+		public void Evaluate(int SpreadMax) {
+//			FTestOut = selectSpread ? receivedStreamB : receivedStreamA;
+//			selectSpread = ! selectSpread;
+//			LogNow(LogType.Debug, "Test spread = " + (FTestOut == receivedStreamB ? "SPREAD 1" : "Spread 0" ) + " slice 0 = " + FTestOut[0] + (FTestOut.IsChanged ? "CHANGED" : "not changed" ) );
+//			FTestOut[0] = FTestOut[0];
+//			LogNow(LogType.Debug, (FTestOut.IsChanged ? "CHANGED" : "not changed" ) );
+//			
+			if (FChannelIn.IsChanged) {
+				if ( localMessageBroadcastPartner != null ) {
+					localMessageBroadcastPartner.Dispose();
+					localMessageBroadcastPartner = null;
+				}
+				localMessageBroadcastPartner = new LocalMessageBroadcastPartner("vvvv", Utils.GetChannelPrefix(FStreamOut) + FChannelIn[0]);
+				LogNow(LogType.Debug, "[R.Raw] New LocalMessageBroadcastPartner created with id = " + localMessageBroadcastPartner.PartnerId);
+				localMessageBroadcastPartner.OnMessage += OnMessageHandler;
+			}
+			
+			if ( received ) {
+				received = false;
+				
+				//LogNow(LogType.Debug, "[R.Raw] Try to update output" );
+				
+				StreamUtils.GetSpreadMax(FStreamOut);
+				FStreamOut.SliceCount = receivedStream.SliceCount;
+				for (int i = 0; i < receivedStream.SliceCount; i++ ) {
+					FValueOut[i] = receivedStream[i];
+				}
+	
+			}
+			
+			
+			if (logMe.Length > 0) {
+				LogNow(LogType.Message, logMe);
+				logMe = "";
+			}
+
+		}
+
+		public void Dispose() {
+			
+			// Take yourself off the Finalization queue
+			// to prevent finalization code for this object
+			// from executing a second time.
+			GC.SuppressFinalize(this);
+		}
+
+	}
+}
--- vvvv45/addonpack/src/nodes/plugins/Network/InterProcessSendReceiveNodes/Utils.cs
+++ vvvv45/addonpack/src/nodes/plugins/Network/InterProcessSendReceiveNodes/Utils.cs
@@ -7,12 +7,14 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
+using System.IO;
 
 using System.ComponentModel.Composition;
 using VVVV.PluginInterfaces.V1;
 using VVVV.PluginInterfaces.V2;
 using VVVV.Utils.VColor;
 using VVVV.Utils.VMath;
+using VVVV.Utils.Streams;
 
 using System.Collections.Generic;
 
@@ -21,7 +23,8 @@ namespace InterProcessSendReceiveNodes
 	public enum MessageTypeEnum {
 		stringSpread,
 		doubleSpread,
-		colorSpread
+		colorSpread,
+		rawSpread //System.IO.Stream
 	}
 	
 	/// <summary>
@@ -37,6 +40,9 @@ namespace InterProcessSendReceiveNodes
 		}
 		public static string GetChannelPrefix(ISpread<RGBAColor> spread) {
 			return "vvvv/InterProcess/ColorSpread/";
+		}
+		public static string GetChannelPrefix(IStream spread) {
+			return "vvvv/InterProcess/StreamSpread/";
 		}
 		
 		
@@ -165,6 +171,73 @@ namespace InterProcessSendReceiveNodes
 			}
 		}
 
+		
+		public static byte[] GenerateMessage(IInStream<Stream> spread, uint version) {
+			//message type
+			byte[] typeBytes = BitConverter.GetBytes((UInt32)MessageTypeEnum.rawSpread);
+
+			//message version
+			byte[] versionBytes = BitConverter.GetBytes((UInt32)version);
+			
+			//nr of slices
+			byte[] sliceCountBytes = BitConverter.GetBytes((Int32)spread.SliceCount);
+
+			
+			//first calculate the size
+			long size = typeBytes.Length + versionBytes.Length + sliceCountBytes.Length;
+			for ( int i = 0; i < spread.SliceCount; i++ ) {
+				//stream length
+				size += sizeof(long);
+				size += spread[i].Length;
+			}
+			
+			
+			byte[] bytes = new byte[size];
+
+			int index = 0;
+			typeBytes.CopyTo(bytes, index);
+			index += typeBytes.Length;
+			versionBytes.CopyTo(bytes, index);
+			index += versionBytes.Length;
+			sliceCountBytes.CopyTo(bytes, index);
+			index += sliceCountBytes.Length;
+			
+			for ( int i = 0; i < spread.SliceCount; i++ ) {
+				//stream length
+				BitConverter.GetBytes( spread[i].Length ).CopyTo(bytes, index);
+				index += sizeof(long);
+				
+				//stream bytes
+				spread[i].
+				spread[i].Read( bytes, index, (Int32)(spread[i].Length) );
+//				index += (Int32)spread[i].Length;
+			}
+			
+			return bytes;
+		}
+		
+		public static void ProcessMessage(byte[] bytes, IOutStream<Stream> spread) {
+			//skip message type & version
+			int pos = sizeof(uint) * 2;
+			
+			//Read slicecount
+			spread.SliceCount = BitConverter.ToInt32(bytes, pos);
+			pos += sizeof(Int32);
+			
+			for ( int i = 0; i < spread.SliceCount; i++ ) {
+				//Read Stream nr of bytes
+				Int32 streamLength = BitConverter.ToInt32(bytes, pos);
+				pos += sizeof(Int32);
+
+				//Read Stream bytes
+				spread[i] = new MemoryStream();
+				spread[i].Read(bytes, pos, streamLength);
+				pos += streamLength;
+			}
+		}
+
+		
+		
 		
 		public static MessageTypeEnum GetMessageType(byte[] bytes) {
 			//Read MessageType
