9cbc6e84f1f0edfc0cda4b6ef3312f01aef08d20
phleg ma <buhlert@meso.net>
2013-04-22 15:11:03 +0200

    revert whitspace changes

--- vvvv45/addonpack/src/nodes/plugins/Devices/PhidgetsIO/IOBoard/WrapperIOBoards.cs
+++ vvvv45/addonpack/src/nodes/plugins/Devices/PhidgetsIO/IOBoard/WrapperIOBoards.cs
@@ -8,185 +8,161 @@ using Phidgets.Events;
 
 namespace VVVV.Nodes
 {
-	class WrapperIOBoards: Phidgets<InterfaceKit>, IPhidgetsWrapper
-	{
+    class WrapperIOBoards: Phidgets<InterfaceKit>, IPhidgetsWrapper
+    {
 
-		bool FChanged = false;
-		bool FSensorChanged = false;
-		bool FDigitalInputChanged = false;
+        bool FChanged = false;
 
-		#region constructor
+        #region constructor
 
-		public WrapperIOBoards()
-			: base()
-		{
+        public WrapperIOBoards()
+            : base()
+        {
 
-		}
+        }
 
-		public WrapperIOBoards(int SerialNumber)
-			: base(SerialNumber)
-		{
+        public WrapperIOBoards(int SerialNumber)
+            : base(SerialNumber)
+        {
 
-		}
+        }
 
-		#endregion constructor
+        #endregion constructor
 
 
-		#region setter fuctions
+        #region setter fuctions
 
 
-		public void SetDigitalOutput(int Index, bool value)
-		{
-			FPhidget.outputs[Index] = value;
-		}
+        public void SetDigitalOutput(int Index, bool value)
+        {
+            FPhidget.outputs[Index] = value;
+        }
 
-		public void SetRadiometric(bool value)
-		{
-			FPhidget.ratiometric = value;
-		}
+        public void SetRadiometric(bool value)
+        {
+            FPhidget.ratiometric = value;
+        }
 
-		public void SetDataRate(int Index, int value)
-		{
-			FPhidget.sensors[Index].DataRate = value;
-		}
-
-
-		#endregion setter function
+        public void SetDataRate(int Index, int value)
+        {
+            FPhidget.sensors[Index].DataRate = value;
+        }
 
 
-		#region getter functions
-
-		public int GetInputCount()
-		{
-			return FPhidget.inputs.Count;
-		}
-
-		public bool GetInputState(int Index)
-		{
-			return FPhidget.inputs[Index];
-		}
-
-		public int GetOutputCount()
-		{
-			return FPhidget.outputs.Count;
-		}
-
-		public bool GetOutputState(int Index)
-		{
-			return FPhidget.outputs[Index];
-		}
-
-		public int GetSensorCount()
-		{
-			return FPhidget.sensors.Count;
-		}
-
-		public int GetSensorValue(int Index)
-		{
-			return FPhidget.sensors[Index].Value;
-		}
-
-		public int GetSensorRawValue(int Index)
-		{
-			return FPhidget.sensors[Index].RawValue;
-		}
-
-		public int GetDataRate(int Index)
-		{
-			return FPhidget.sensors[Index].DataRate;
-		}
-
-		public int GetDataRateMin(int Index)
-		{
-			return FPhidget.sensors[Index].DataRateMin;
-		}
+        #endregion setter function
 
 
-		public int GetDataRateMax(int Index)
-		{
-			return FPhidget.sensors[Index].DataRateMax;
-		}
+        #region getter functions
 
-		public bool GetRadiometric()
-		{
-			return FPhidget.ratiometric;
-		}
+        public int GetInputCount()
+        {
+            return FPhidget.inputs.Count;
+        }
+
+        public bool GetInputState(int Index)
+        {
+            return FPhidget.inputs[Index];
+        }
+
+        public int GetOutputCount()
+        {
+            return FPhidget.outputs.Count;
+        }
+
+        public bool GetOutputState(int Index)
+        {
+            return FPhidget.outputs[Index];
+        }
+
+        public int GetSensorCount()
+        {
+            return FPhidget.sensors.Count;
+        }
+
+        public int GetSensorValue(int Index)
+        {
+            return FPhidget.sensors[Index].Value;
+        }
+
+        public int GetSensorRawValue(int Index)
+        {
+            return FPhidget.sensors[Index].RawValue;
+        }
+
+        public int GetDataRate(int Index)
+        {
+            return FPhidget.sensors[Index].DataRate;
+        }
+
+        public int GetDataRateMin(int Index)
+        {
+            return FPhidget.sensors[Index].DataRateMin;
+        }
+
+
+        public int GetDataRateMax(int Index)
+        {
+            return FPhidget.sensors[Index].DataRateMax;
+        }
+
+        public bool GetRadiometric()
+        {
+            return FPhidget.ratiometric;
+        }
 
 
 
-		#endregion getter functions
+        #endregion getter functions
 
 
 
 
-		public override void AddChangedHandler()
-		{
-			FPhidget.SensorChange += new SensorChangeEventHandler(SensorChange);
-			FPhidget.InputChange += new InputChangeEventHandler(InputChange);
-			FPhidget.OutputChange += new OutputChangeEventHandler(OutputChange);
-		}
+        public override void AddChangedHandler()
+        {
+            FPhidget.SensorChange += new SensorChangeEventHandler(SensorChange);
+            FPhidget.InputChange += new InputChangeEventHandler(InputChange);
+            FPhidget.OutputChange += new OutputChangeEventHandler(OutputChange);
+        }
 
-		void OutputChange(object sender, OutputChangeEventArgs e)
-		{
-			
-		}
+        void OutputChange(object sender, OutputChangeEventArgs e)
+        {
+            
+        }
 
-		void InputChange(object sender, InputChangeEventArgs e)
-		{
-			FChanged = true;
-			FDigitalInputChanged = true;
-		}
+        void InputChange(object sender, InputChangeEventArgs e)
+        {
+            FChanged = true;
+        }
 
-		void SensorChange(object sender, SensorChangeEventArgs e)
-		{
-			FChanged = true;
-			FSensorChanged = true;
-		}
+        void SensorChange(object sender, SensorChangeEventArgs e)
+        {
+            FChanged = true;
+        }
 
-		public override void RemoveChangedHandler()
-		{
-			FPhidget.SensorChange -= new SensorChangeEventHandler(SensorChange);
-			FPhidget.InputChange -= new InputChangeEventHandler(InputChange);
-			FPhidget.OutputChange -= new OutputChangeEventHandler(OutputChange);
-		}
+        public override void RemoveChangedHandler()
+        {
+            FPhidget.SensorChange -= new SensorChangeEventHandler(SensorChange);
+            FPhidget.InputChange -= new InputChangeEventHandler(InputChange);
+            FPhidget.OutputChange -= new OutputChangeEventHandler(OutputChange);
+        }
 
 
-		public int Count
-		{
-			get { return FPhidget.outputs.Count; }
-		}
+        public int Count
+        {
+            get { return FPhidget.outputs.Count; }
+        }
 
 
-		public bool Changed
-		{
-			get
-			{
-				bool temp = FChanged;
-				FChanged = false;
-				return temp;
-			}
-		}
-		
-		public bool SensorChanged
-		{
-			get
-			{
-				bool temp = FSensorChanged;
-				FSensorChanged = false;
-				return temp;
-			}
-		}
-		
-		public bool DigitalInputChanged
-		{
-			get
-			{
-				bool temp = FDigitalInputChanged;
-				FDigitalInputChanged = false;
-				return temp;
-			}
-		}
+        public bool Changed
+        {
+            get
+            {
+                bool temp = FChanged;
+                FChanged = false;
+                return temp;
+            }
+        }
 
 
-	}
+    }
 }
