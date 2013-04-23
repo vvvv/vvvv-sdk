920e9d208e26304782c1a64706cc76387d0c41d4
Elias Holzer <elias@vvvv.org>
2013-04-23 22:13:30 +0200

    Added nuspec file to Hosting, UtilsIL and Utils3rdParty.

--- vvvv45/src/core/Hosting/Properties/AssemblyInfo.cs
+++ vvvv45/src/core/Hosting/Properties/AssemblyInfo.cs
@@ -3,6 +3,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+[assembly: AssemblyDescription("VVVV Hosting Library")]
 // This sets the default COM visibility of types in the assembly to invisible.
 // If you need to expose a type to COM, use [ComVisible(true)] on that type.
 [assembly: ComVisible(true)]
