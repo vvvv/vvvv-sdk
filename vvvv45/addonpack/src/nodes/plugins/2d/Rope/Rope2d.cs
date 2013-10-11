f41c1d0696a27e2e61e87e422ef60c009029adaa
Matthias Husinsky <mhusinsky@fhstp.ac.at>
2013-10-11 23:47:05 +0200

    First commit of generic 2D rope plugin and modules.

---
+++ vvvv45/addonpack/src/nodes/plugins/2d/Rope/Rope2d.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Matthias
+ * Date: 11.10.2013
+ * Time: 21:35
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Rope
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ vvvv45/addonpack/src/nodes/plugins/2d/Rope2d/Rope2d.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Matthias
+ * Date: 11.10.2013
+ * Time: 22:09
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Rope2d
+{
+	/// <summary>
+	/// Description of UserControl1.
+	/// </summary>
+	public partial class UserControl1 : UserControl
+	{
+		public UserControl1()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
