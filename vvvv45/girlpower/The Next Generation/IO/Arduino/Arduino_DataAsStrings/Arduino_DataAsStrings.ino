b4fde8f906d68b81cc101081739f02174b8b74f5
joreg <joreg@vvvv.org>
2013-11-08 22:10:55 +0100

    cosmetics

--- vvvv45/girlpower/The Next Generation/IO/Arduino/Arduino_DataAsStrings/Arduino_DataAsStrings.ino
+++ vvvv45/girlpower/The Next Generation/IO/Arduino/Arduino_DataAsStrings/Arduino_DataAsStrings.ino
@@ -1,11 +1,11 @@
 /*
 This is an example on how to communicate to vvvv 
-using the data encoded as strings.
+using data encoded as strings.
 For more about Arduino and vvvv check:
 http://vvvv.org/documentation/arduino
 
-To see the physical output plug the LED into the Arduino board 
-between the pin (defined by the 'Pin number' in vvvv patch, 
+To see the physical output plug an LED into the Arduino board 
+between the pin (defined by the 'Pin number' in the vvvv patch, 
 this should be one of PWM pins of the board) and 
 the GND (ground) pin. Short leg of the LED (it is '-') 
 goes to the GND.
@@ -37,12 +37,11 @@ void loop()
     SerialParser();
     
     //if something arrived 
-    if (ByteCount  > 0) 
+    if (ByteCount > 0) 
     {
-      
       //send some data back to vvvv
       //the values are encoded as ASCII characters
-      //the last print command sends '/r/n/' at the end
+      //the last print command sends '\r\n' at the end
       //this defines the end of the message
       Serial.print(millis());
       Serial.print(",");
@@ -54,13 +53,13 @@ void loop()
       if (ledON)
       {
         //write PWM signal value to the pin
-        analogWrite (pinNumber, value);
+        analogWrite(pinNumber, value);
       }
       else
       {
        //write digital value to the pin
        //setting 0V in this example;
-       digitalWrite (pinNumber,LOW);
+       digitalWrite(pinNumber, LOW);
       }
     }
     
@@ -68,56 +67,52 @@ void loop()
 
 void SerialParser() 
 {
-
   ByteCount = -1;
   
-  // if something is arrived over serial port
-  if (Serial.available()>0)
+  // if something has arrived over serial port
+  if (Serial.available() > 0)
   {
-    
     //read the first character
-    char ch=Serial.read();
+    char ch = Serial.read();
     
     //if it's 's', then it's the start of the message
-    if (ch=='s')
+    if (ch == 's')
     {
-      
       //read all bytes of the message until the newline character ('\n')
        ByteCount =  Serial.readBytesUntil('\n',buffer,bufferSize); 
        
        //if the number of arrived bytes > 0
-       if (ByteCount  > 0) 
+       if (ByteCount > 0) 
        {
             // copy the string until the first ','
-            strcpy(commandBuffer, strtok(buffer,","));
+            strcpy(commandBuffer, strtok(buffer, ","));
             
             // copy the same string until the next ','
-            strcpy(pinBuffer, strtok(NULL,","));
+            strcpy(pinBuffer, strtok(NULL, ","));
      
             // copy the same string until the next ',' 
-            strcpy(valueBuffer, strtok(NULL,","));
+            strcpy(valueBuffer, strtok(NULL, ","));
             
             //check the documentation about strtok() at:
             //http://www.gnu.org/software/libc/manual/html_node/Finding-Tokens-in-a-String.html
          
-         
             //check the arrived command and set the LED state
             //this is how to compare two char arrays (simple strings)
             //if they are equal, strcmp returns 0
-            if (strcmp(commandBuffer, "LED_ON")==0)
+            if (strcmp(commandBuffer, "LED_ON") == 0)
             {
-              ledON=true;
+              ledON = true;
             }
             else
             {
-              ledON=false;
+              ledON = false;
             }
             
             // convert the string into an 'int' value
-            pinNumber=atoi (pinBuffer);   
+            pinNumber = atoi (pinBuffer);   
    
-            // convert the sting into a 'float' value and bring it to (0,255) range;
-            value=atof (valueBuffer) * 255;  
+            // convert the sting into a 'float' value and bring it to (0..255) range;
+            value = atof(valueBuffer) * 255;  
        }
        
        // clear contents of buffer
