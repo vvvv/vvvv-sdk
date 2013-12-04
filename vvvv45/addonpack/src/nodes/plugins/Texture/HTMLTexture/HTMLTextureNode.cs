b7ca5aa7b0b368b16260f6970f2930d8bb6b9de1
Elias Holzer <elias@vvvv.org>
2013-12-04 17:09:16 +0100

    Revert "New version of HTMLTexture node using new mouse/keyboard."
    This reverts commit 88c72324322290096923778b4666642db915eb0c.

--- vvvv45/addonpack/src/nodes/plugins/Texture/HTMLTexture/HTMLTextureNode.cs
+++ vvvv45/addonpack/src/nodes/plugins/Texture/HTMLTexture/HTMLTextureNode.cs
@@ -55,9 +55,9 @@ namespace VVVV.Nodes.Texture.HTML
         [Input("Zoom Level")]
         public ISpread<double> FZoomLevelIn;
         [Input("Mouse Event")]
-        public ISpread<Mouse> FMouseIn;
+        public ISpread<MouseState> FMouseIn;
         [Input("Key Event")]
-        public ISpread<Keyboard> FKeyboardIn;
+        public ISpread<KeyboardState> FKeyboardIn;
         [Input("Scroll To")]
         public ISpread<Vector2D> FScrollToIn;
         [Input("Update DOM", IsBang = true)]
@@ -117,8 +117,8 @@ namespace VVVV.Nodes.Texture.HTML
                 // Assign inputs
                 webRenderer.Size = new Size(FWidthIn[i], FHeightIn[i]);
                 webRenderer.ZoomLevel = FZoomLevelIn[i];
-                webRenderer.NewMouse = FMouseIn[i];
-                webRenderer.NewKeyboard = FKeyboardIn[i];
+                webRenderer.Mouse = FMouseIn[i];
+                webRenderer.Keyboard = FKeyboardIn[i];
                 webRenderer.ScrollTo = FScrollToIn[i];
 
                 if (FExecuteIn[i])
