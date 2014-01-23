7eaede9c2a7b2e65730b3503802c4fb2b6e5ab7d
woeishi <woei@refect.net>
2014-01-23 17:23:03 +0100

    info & pin cosmetics

--- vvvv45/addonpack/src/nodes/plugins/Color/RGBVector/RGBVectorNode.cs
+++ vvvv45/addonpack/src/nodes/plugins/Color/RGBVector/RGBVectorNode.cs
@@ -11,7 +11,7 @@ using VVVV.Utils.VMath;
 namespace VVVV.Nodes
 {
 	#region PluginInfo
-	[PluginInfo(Name = "RGB", Category = "Color", Version = "Split Vector", Help = "splits a color in a vector with red, green, blue and alpha components", Tags = "")]
+	[PluginInfo(Name = "RGB", Category = "Color", Version = "Split Vector", Help = "splits a color in a vector with red, green, blue and alpha components", Author = "woei")]
 	#endregion PluginInfo
 	public unsafe class RGBVectorSplitNode : IPluginEvaluate
 	{
@@ -19,7 +19,7 @@ namespace VVVV.Nodes
 		[Input("Input")]
 		IColorIn FInPin;
 
-		[Output("Output", Dimension = 4)]
+		[Output("", Dimension = 4, DimensionNames = new string[]{"R","G","B","A"})]
 		IValueOut FOutPin;
 		#endregion fields & pins
 		
@@ -30,21 +30,22 @@ namespace VVVV.Nodes
 			double* inData, outData;
 			
 			FInPin.GetColorPointer(out inCount, out inData);
+			
 			FOutPin.SliceCount = inCount;
 			FOutPin.GetValuePointer(out outData);
 			
 			for (int i=0; i<inCount*4; i++)
-				outData[(i)] = inData[i];
+				outData[i] = inData[i];
 //			Memory.Copy(outData, inData, (uint)(inCount*4*sizeof(double)));
 		}
 		
 		#region PluginInfo
-		[PluginInfo(Name = "RGB", Category = "Color", Version = "Join Vector", Help = "creates a colour out of the red, green, blue and alpha components of a vector", Tags = "")]
+		[PluginInfo(Name = "RGB", Category = "Color", Version = "Join Vector", Help = "creates a colour out of the red, green, blue and alpha components of a vector", Author = "woei")]
 		#endregion PluginInfo
 		public unsafe class RGBVectorJoinNode : IPluginEvaluate
 		{
 			#region fields & pins
-			[Input("", Dimension = 4, DefaultValues = new double[]{0,1,0,1})]
+			[Input("", Dimension = 4, DimensionNames = new string[]{"R","G","B","A"}, DefaultValues = new double[]{0,1,0,1})]
 			IValueFastIn FInPin;
 	
 			[Output("Output")]
@@ -64,7 +65,7 @@ namespace VVVV.Nodes
 				FOutPin.GetColorPointer(out outData);
 				
 				for (int i=0; i<spreadMax*4; i++)
-					outData[(i)] = inData[i%inCount];
+					outData[i] = inData[i%inCount];
 			}
 		}
 	}
