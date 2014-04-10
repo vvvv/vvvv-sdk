1fd95a7ccc21849d1fb8a152affd95607630cd6e
Elias Holzer <elias@vvvv.org>
2014-04-10 23:01:26 +0200

    Added couple of null checks.

--- vvvv45/src/core/Hosting/IO/ComIStream.cs
+++ vvvv45/src/core/Hosting/IO/ComIStream.cs
@@ -16,6 +16,8 @@ namespace System.IO
 
         public ComIStream(Stream source)
         {
+            if (source == null)
+                throw new ArgumentNullException("source");
             this.source = source;
         }
 
