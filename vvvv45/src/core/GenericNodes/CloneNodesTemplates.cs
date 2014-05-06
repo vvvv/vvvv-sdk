5583f9014f679afb23eeadb6e9e962138f0ca30e
Elias Holzer <elias@vvvv.org>
2014-05-06 18:19:26 +0200

    Out-commented the PluginInfo in the generic nodes project and fixed tabs and whitespaces.

--- vvvv45/src/core/GenericNodes/CloneNodesTemplates.cs
+++ vvvv45/src/core/GenericNodes/CloneNodesTemplates.cs
@@ -4,17 +4,19 @@ using VVVV.Nodes.Generic;
 
 namespace VVVV.Nodes
 {
-	//1.) do a 'replace all' for REPLACEME_CLASS with the name of your own type
-	
-	//2.) do a 'replace all' for NODECATEGORY to set the version and the class name prefix for all node (e.g. Value, Color...)
+    //1.) do a 'replace all' for REPLACEME_CLASS with the name of your own type
 
-    //3.) you have to override the CloneSlice or the CloneSpread method for your type. overriding CloneSlice is easier, CloneSpread might allow some performance optimizations
+    //2.) do a 'replace all' for NODECATEGORY to set the version and the class name prefix for all node (e.g. Value, Color...)
 
-    [PluginInfo(Name = "FrameDelay",
-	            Category = "NODECATEGORY",
-                Help = "Delays the input value one calculation frame.",
-	            Tags = "generic"
-	           )]
+    //3.) remove the comments from the PluginInfo attributes
+
+    //4.) you have to override the CloneSlice or the CloneSpread method for your type. overriding CloneSlice is easier, CloneSpread might allow some performance optimizations
+
+    //[PluginInfo(Name = "FrameDelay",
+    //            Category = "NODECATEGORY",
+    //            Help = "Delays the input value one calculation frame.",
+    //            Tags = "generic"
+    //           )]
     public class NODECATEGORYFrameDelayNode : FrameDelayNode<REPLACEME_CLASS>
     {
         protected override REPLACEME_CLASS CloneSlice(REPLACEME_CLASS slice)
--- vvvv45/src/core/GenericNodes/DistanceNodesTemplates.cs
+++ vvvv45/src/core/GenericNodes/DistanceNodesTemplates.cs
@@ -4,27 +4,29 @@ using VVVV.Nodes.Generic;
 
 namespace VVVV.Nodes
 {
-	//1.) do a 'replace all' for REPLACEME_CLASS with the name of your own type
-	
-	//2.) do a 'replace all' for NODECATEGORY to set the version and the class name prefix for all node (e.g. Value, Color...)
-	
-	//3.) you have to override the Distance method for your type
-	
-	[PluginInfo(Name = "NearestNeighbour",
-	            Category = "NODECATEGORY",
-	            Help = "Finds nearest neighbours among given values.",
-	            Tags = "generic",
-	            Author = "vux"
-	           )]
+    //1.) do a 'replace all' for REPLACEME_CLASS with the name of your own type
+
+    //2.) do a 'replace all' for NODECATEGORY to set the version and the class name prefix for all node (e.g. Value, Color...)
+
+    //3.) remove the comments from the PluginInfo attributes
+
+    //4.) you have to override the Distance method for your type
+
+    //[PluginInfo(Name = "NearestNeighbour",
+    //            Category = "NODECATEGORY",
+    //            Help = "Finds nearest neighbours among given values.",
+    //            Tags = "generic",
+    //            Author = "vux"
+    //           )]
     public class NODECATEGORYNearestNeighbourNode : NearestNeighbour<REPLACEME_CLASS>
     {
-		protected override double Distance(REPLACEME_CLASS a, REPLACEME_CLASS b)
-		{
-			//calculate distance between a and b
-			throw new NotImplementedException("You need to implement the Distance method");
-		}
+        protected override double Distance(REPLACEME_CLASS a, REPLACEME_CLASS b)
+        {
+            //calculate distance between a and b
+            throw new NotImplementedException("You need to implement the Distance method");
+        }
     }
-	
+
 
 }
 
