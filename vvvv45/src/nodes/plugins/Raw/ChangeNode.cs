5aac2cd6d368954dfbd22d06231acf1afd5148e3
Elias Holzer <elias@vvvv.org>
2014-11-03 20:08:50 +0100

    Fixed null pointer exception when using more than 16 slices and major performance drop in Changed (Raw) node.

--- vvvv45/src/nodes/plugins/Raw/ChangeNode.cs
+++ vvvv45/src/nodes/plugins/Raw/ChangeNode.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using VVVV.Nodes.Generic;
 using VVVV.PluginInterfaces.V2;
+using VVVV.Utils.Streams;
 
 namespace VVVV.Nodes.Raw
 {
@@ -12,6 +13,8 @@ namespace VVVV.Nodes.Raw
         Help = "Outputs 1 when the input has changed in this frame and 0 if the input was equal to the one in the last frame")]
     public class RawChangeNode : ChangeNode<Stream>
     {
+        private static readonly byte[] buffer = new byte[4096];
+
         public RawChangeNode()
             : base(StreamEqualityComparer.Instance)
         {
@@ -21,13 +24,13 @@ namespace VVVV.Nodes.Raw
         {
             var copy = new MemoryStream((int)slice.Length);
             slice.Position = 0;
-            slice.CopyTo(copy);
+            slice.CopyTo(copy, buffer);
             return copy;
         }
 
-        protected override void CopySlices(Stream[] slices)
+        protected override void CopySlices(Stream[] slices, int count)
         {
-            for (int i = 0; i < slices.Length; i++)
+            for (int i = 0; i < count; i++)
                 slices[i] = CopySlice(slices[i]);
         }
     }
