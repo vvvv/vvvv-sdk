717e66fd681e1ac1285b70f6533f9d81a1b6c596
tebjan <tebjan@vvvv.org>
2014-12-03 18:33:40 +0100

    improved OneEuroFilter speed by 4 divisions per filter call, moved filter classes to utils, minor helppatch cosmetics

--- vvvv45/src/nodes/plugins/Animation/OneEuroFilter/OneEuroFilterNode.cs
+++ vvvv45/src/nodes/plugins/Animation/OneEuroFilter/OneEuroFilterNode.cs
@@ -9,6 +9,7 @@ using VVVV.Utils.VColor;
 using VVVV.Utils.VMath;
 
 using VVVV.Core.Logging;
+using VVVV.Utils.Animation;
 #endregion usings
 
 namespace VVVV.Nodes
@@ -39,7 +40,7 @@ namespace VVVV.Nodes
 		[Output("Output")]
 		public ISpread<double> FOutput;
 		
-		List<OneEuroFilter> FFilters = new List<OneEuroFilter>();
+		Spread<OneEuroFilter> FFilters = new Spread<OneEuroFilter>(0);
 
 		[Import()]
 		public ILogger FLogger;
@@ -58,13 +59,8 @@ namespace VVVV.Nodes
 		//called when data for any output pin is requested
 		public void Evaluate(int SpreadMax)
 		{
-			//add new slices
-			for (int i=FFilters.Count; i<=SpreadMax; i++)
-				FFilters.Add(new OneEuroFilter(FMinCutoff[i], FBeta[i]));
-			
-			//remove old slices
-			for (int i=FFilters.Count-1; i == SpreadMax-1; i--)
-				FFilters.RemoveAt(i);
+            //resize spread
+            FFilters.ResizeAndDismiss(SpreadMax, i => new OneEuroFilter(FMinCutoff[i], FBeta[i]));
 			
 			FOutput.SliceCount = SpreadMax;
 
@@ -81,95 +77,4 @@ namespace VVVV.Nodes
 			}
 		}
 	}
-	
-	//via http://www.lifl.fr/~casiez/1euro/OneEuroFilter.cs
-	public class OneEuroFilter
-    {
-        public OneEuroFilter(double minCutoff, double beta)
-        {
-            firstTime = true;
-            this.minCutoff = minCutoff;
-            this.beta = beta;
-
-            xFilt = new LowpassFilter();
-            dxFilt = new LowpassFilter();
-            dcutoff = 1;
-        }
-
-        protected bool firstTime;
-        protected double minCutoff;
-        protected double beta;
-        protected LowpassFilter xFilt;
-        protected LowpassFilter dxFilt;
-        protected double dcutoff;
-
-        public double MinCutoff
-        {
-            get { return minCutoff; }
-            set { minCutoff = value; }
-        }
-
-        public double Beta
-        {
-            get { return beta; }
-            set { beta = value; }
-        }
-    	
-    	public double CutoffDerivative
-        {
-            get { return dcutoff; }
-            set { dcutoff = value; }
-        }
-
-        public double Filter(double x, double rate)
-        {
-            double dx = firstTime ? 0 : (x - xFilt.Last()) * rate;
-            if (firstTime)
-                firstTime = false;
-
-            var edx = dxFilt.Filter(dx, Alpha(rate, dcutoff));
-            var cutoff = minCutoff + beta * Math.Abs(edx);
-
-            return xFilt.Filter(x, Alpha(rate, cutoff));
-        }
-
-        protected double Alpha(double rate, double cutoff)
-        {
-            var tau = 1.0 / (2 * Math.PI * cutoff);
-            var te = 1.0 / rate;
-            return 1.0 / (1.0 + tau / te);
-        }
-    }
-
-    public class LowpassFilter
-    {
-        public LowpassFilter()
-        {
-            firstTime = true;
-        }
-
-        protected bool firstTime;
-        protected double hatXPrev;
-
-        public double Last()
-        {
-            return hatXPrev;
-        }
-
-        public double Filter(double x, double alpha)
-        {
-            double hatX = 0;
-            if (firstTime)
-            {
-                firstTime = false;
-                hatX = x;
-            }
-            else
-                hatX = alpha * x + (1 - alpha) * hatXPrev;
-
-            hatXPrev = hatX;
-
-            return hatX;
-        }
-    }
 }
