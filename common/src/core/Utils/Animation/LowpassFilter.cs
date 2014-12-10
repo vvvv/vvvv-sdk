a5f502c0aa4453478998e0c1b71e63754a43d1aa
tebjan <tebjan@vvvv.org>
2014-12-10 18:34:20 +0100

    Merge branch 'develop' into develop-50
    Conflicts:
    	public/vvvv45/src/nodes/plugins/NativePlugins.csproj

---
+++ common/src/core/Utils/Animation/LowpassFilter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace VVVV.Utils.Animation
+{
+    /// <summary>
+    /// One pole IIR filter
+    /// </summary>
+	public class LowpassFilter
+    {
+        public LowpassFilter()
+        {
+            FFirstTime = true;
+        }
+
+        protected bool FFirstTime;
+        protected double FLastFilterValue;
+
+        public double Last
+        {
+            get
+            {
+                return FLastFilterValue;
+            }
+        }
+
+        public double Filter(double x, double alpha)
+        {
+            double filterValue = 0;
+            if (FFirstTime)
+            {
+                FFirstTime = false;
+                filterValue = x;
+            }
+            else
+            {
+                filterValue = alpha * x + (1 - alpha) * FLastFilterValue;
+            }
+
+            FLastFilterValue = filterValue;
+
+            return filterValue;
+        }
+    }
+}
+
+
