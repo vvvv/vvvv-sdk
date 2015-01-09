29a266a8646d840047162ee525bf524497c918b7
joreg <joreg@vvvv.org>
2015-01-09 22:09:19 +0100

    added Visibility set/get to IPin/IPin2

--- vvvv45/src/core/PluginInterfaces/V2/Graph/IPin2.cs
+++ vvvv45/src/core/PluginInterfaces/V2/Graph/IPin2.cs
@@ -85,6 +85,14 @@ namespace VVVV.PluginInterfaces.V2.Graph
         }
         
         /// <summary>
+        /// Gets/Sets the pins visibility. 
+        /// </summary>
+        PinVisibility Visibility
+        {
+            get; set;
+        }
+        
+        /// <summary>
         /// Returns a list of connected pins. For Inputs this is a maximum of one.
         /// </summary>
         IViewableCollection<IPin2> ConnectedPins
