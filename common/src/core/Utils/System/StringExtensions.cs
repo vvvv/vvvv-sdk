3741b70c0823dd21207bc73863ebdcaf4c5aaae4
tebjan <tebjan@vvvv.org>
2015-04-18 00:30:07 +0200

    added ToUpperFirstInvariant to StringExtensions.cs to uppercase the first letter of a string.

--- common/src/core/Utils/System/StringExtensions.cs
+++ common/src/core/Utils/System/StringExtensions.cs
@@ -21,5 +21,29 @@ namespace System
                     return titleCase.Substring(0, 1).ToLower() + titleCase.Substring(1);
             }
         }
+
+        /// <summary>
+        /// Converts the first letter of a string to upper case, using the casing rules of invariant culture.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string ToUpperFirstInvariant(this string s)
+        {
+            // Check for empty string.
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                return s;
+            }
+
+            if (s.Length > 1)
+            {
+                // Return char and concat substring.
+                return char.ToUpperInvariant(s[0]) + s.Substring(1);
+            }
+            else
+            {
+                return s.ToUpperInvariant();
+            }
+        }
     }
 }
