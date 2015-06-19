1283cacac782ea6d17eb744928f9d4024b6e59d8
Elias Holzer <elias@vvvv.org>
2015-06-19 21:58:21 +0200

    [VL] Reworked Enabled pins:
    * They're now available on nodes which
    ** Have no output
    ** Have exactly one output named "Output" and at least one input named "Input" of the same type or sub type of the output.
    * Node and pin symbols will throw an exception in case they get modified when frozen.
    * Simplified the source code update process for missing source code elements - now done via new SourceWasCompilerGenerated flag on symbol.
    * Pin symbols do have a new Index property now which reports the position of the pin in the parent list.

--- common/src/core/Utils/System/CollectionExtensions.cs
+++ common/src/core/Utils/System/CollectionExtensions.cs
@@ -19,5 +19,39 @@ namespace System.Collections.Generic
             foreach (var value in values)
                 hashSet.Add(value);
         }
+
+        public static void AddRange<T>(this IList<T> list, IEnumerable<T> items)
+        {
+            foreach (var item in items)
+                list.Add(item);
+        }
+
+        public static void AssignFrom<T>(this IList<T> list, IEnumerable<T> items)
+        {
+            list.Clear();
+            list.AddRange(items);
+        }
+
+        public static void Sort<T>(this IList<T> list, Comparison<T> comparison)
+        {
+            var mutableList = list as List<T>;
+            if (mutableList != null)
+            {
+                mutableList.Sort(comparison);
+                return;
+            }
+            throw new NotSupportedException();
+        }
+
+        public static void Sort<T>(this IList<T> list, IComparer<T> comparer)
+        {
+            var mutableList = list as List<T>;
+            if (mutableList != null)
+            {
+                mutableList.Sort(comparer);
+                return;
+            }
+            throw new NotSupportedException();
+        }
     }
 }
