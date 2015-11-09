2f8be9307356a2d18102293071c647f0ca383af8
woeishi <woei@refect.net>
2015-11-09 14:51:53 +0100

    [VVVV] structs and consts for gesture config and info

--- common/src/core/Utils/Win32/Const.cs
+++ common/src/core/Utils/Win32/Const.cs
@@ -30,5 +30,36 @@ namespace VVVV.Utils.Win32
         public const int TOUCHINPUTMASKF_TIMEFROMSYSTEM = 0x0001; // the dwTime field contains a system generated value
         public const int TOUCHINPUTMASKF_EXTRAINFO = 0x0002; // the dwExtraInfo field is valid
         public const int TOUCHINPUTMASKF_CONTACTAREA = 0x0004; // the cxContact and cyContact fields are valid
+
+        // GestureConfig
+        public const int GC_ALLGESTURES = 0x00000001;
+        public const int GC_ZOOM = 0x00000001;
+        public const int GC_PAN = 0x00000001;
+        public const int GC_PAN_WITH_SINGLE_FINGER_VERTICALLY = 0x00000002;
+        public const int GC_PAN_WITH_SINGLE_FINGER_HORIZONTALLY = 0x00000004;
+        public const int GC_PAN_WITH_GUTTER = 0x00000008;
+        public const int GC_PAN_WITH_INERTIA = 0x00000010;
+        public const int GC_ROTATE = 0x00000001;
+        public const int GC_TWOFINGERTAP = 0x00000001;
+        public const int GC_PRESSANDTAP = 0x00000001;
+
+        // Gesture IDs 
+        public const int GID_BEGIN = 1;
+        public const int GID_END = 2;
+        public const int GID_ZOOM = 3;
+        public const int GID_PAN = 4;
+        public const int GID_ROTATE = 5;
+        public const int GID_TWOFINGERTAP = 6;
+        public const int GID_PRESSANDTAP = 7;
+
+        // Gesture flags - GESTUREINFO.dwFlags
+        public const int GF_BEGIN = 0x00000001;
+        public const int GF_INERTIA = 0x00000002;
+        public const int GF_END = 0x00000004;
+
+        // One of the fields in GESTUREINFO structure is type of Int64 (8 bytes).
+        // The relevant gesture information is stored in lower 4 bytes. This
+        // bit mask is used to get 4 lower bytes from this argument.
+        public const Int64 ULL_ARGUMENTS_BIT_MASK = 0x00000000FFFFFFFF;
     }
 }
