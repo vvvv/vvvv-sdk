b2c1d0815ac5c824bbab50b63f3a76e133fe1eb9
joreg <joreg@vvvv.org>
2016-05-31 13:34:32 +0200

    [VVVV] moved User32 stuff to dedicated utils .cs file

--- common/src/core/Utils/Win32/ScreenCapture.cs
+++ common/src/core/Utils/Win32/ScreenCapture.cs
@@ -28,8 +28,8 @@ namespace VVVV.Utils.Win32
         public Image CaptureWindow(IntPtr handle)
         {
             // get the size
-            User32.RECT windowRect = new User32.RECT();
-            User32.GetWindowRect(handle, ref windowRect);
+            RECT windowRect;
+            User32.GetWindowRect(handle, out windowRect);
 
             const int CXFRAME = 0x20;
             const int CYFRAME = 0x21;
@@ -39,10 +39,10 @@ namespace VVVV.Utils.Win32
             var cdY = User32.GetSystemMetrics(CYFRAME);
             var cdC = User32.GetSystemMetrics(CYSMCAPTION);
 
-            var left = windowRect.left + cdX;
-            var top = windowRect.top + cdY + cdC;
-            var width = windowRect.right - windowRect.left - 2 * cdX;
-            var height = windowRect.bottom - windowRect.top - (2 * cdY + cdC);
+            var left = windowRect.Left + cdX;
+            var top = windowRect.Top + cdY + cdC;
+            var width = windowRect.Width - 2 * cdX;
+            var height = windowRect.Height - (2 * cdY + cdC);
 
             var bmp = new Bitmap(width, height);
 
@@ -54,35 +54,14 @@ namespace VVVV.Utils.Win32
                 //draw cursor
                 if (Cursor.Current != null)
                 {
-                    var x = Cursor.Position.X - windowRect.left - cdX - Cursor.Current.HotSpot.X;
-                    var y = Cursor.Position.Y - windowRect.top - cdY - cdC - Cursor.Current.HotSpot.Y;
+                    var x = Cursor.Position.X - windowRect.Left - cdX - Cursor.Current.HotSpot.X;
+                    var y = Cursor.Position.Y - windowRect.Top - cdY - cdC - Cursor.Current.HotSpot.Y;
                     var rect = new Rectangle(new Point(x, y), Cursor.Current.Size);
                     Cursor.Current.Draw(g, rect);
                 }
             }
 
             return bmp;
-        }
-
-        /// <summary>
-        /// Helper class containing User32 API functions
-        /// </summary>
-        private class User32
-        {
-            [StructLayout(LayoutKind.Sequential)]
-            public struct RECT
-            {
-                public int left;
-                public int top;
-                public int right;
-                public int bottom;
-            }
-            [DllImport("user32.dll")]
-            public static extern IntPtr GetDesktopWindow();
-            [DllImport("user32.dll")]
-            public static extern IntPtr GetWindowRect(IntPtr hWnd, ref RECT rect);
-            [DllImport("user32.dll")]
-            public static extern int GetSystemMetrics(int nIndex);
         }
     }
 }
