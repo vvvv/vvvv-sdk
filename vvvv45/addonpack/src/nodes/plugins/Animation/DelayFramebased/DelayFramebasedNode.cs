04e158d2aae934a46321fefd88babd705faf24d7
Whiplash <Whip@lash.de>
2016-10-27 10:51:02 +0200

    Delay Framebased
    minor changes

--- vvvv45/addonpack/src/nodes/plugins/Animation/DelayFramebased/DelayFramebasedNode.cs
+++ vvvv45/addonpack/src/nodes/plugins/Animation/DelayFramebased/DelayFramebasedNode.cs
@@ -16,7 +16,7 @@ using VVVV.Core.Logging;
 namespace VVVV.Nodes
 {
 	#region PluginInfo
-	[PluginInfo(Name = "Delay", Category = "Animation", Version ="Framebased", Help = "Framebased delay with adjustable frame count.", Tags = "Whiplash/joshuavh")]
+	[PluginInfo(Name = "Delay", Category = "Animation", Version ="Framebased", Help = "Framebased delay with adjustable frame count")]
 	#endregion PluginInfo
 	public class DelayFramebased : IPluginEvaluate
 	{
@@ -24,91 +24,68 @@ namespace VVVV.Nodes
 		[Input("Input", DefaultValue = 1.0)]
 		public ISpread<double> FInput;
 		
-		[Input("Skip", IsBang = false)]
-		public ISpread<bool> skip;
+		[Input("Skip")]
+		public ISpread<bool> FSkip;
 		
-		[Input("Reset", IsBang = false)]
-		public ISpread<bool> reset;
+		[Input("Reset")]
+		public ISpread<bool> FReset;
 
 		[Input("Delay", DefaultValue = 1.0, MinValue = 0)]
-		public ISpread<int> delayFrames;
-		
-		
+		public ISpread<int> FDelayFrames;
+			
 		[Output("Output")]
 		public ISpread<double> FOutput;
-		
-		
 		
 		[Import()]
 		public ILogger FLogger;
 		#endregion fields & pins
 
-		
-////////////////////////////////////////////////////////////////////////////		
-	
-		
 		List<Queue<double>> listOfQueue = new List<Queue<double>>();
 		
-	
-////////////////////////////////////////////////////////////////////////////		
-		
-		//called when data for any output pin is requested
-				
 		public void Evaluate(int SpreadMax)
 		{
-			
-			
 			FOutput.SliceCount = SpreadMax;
 			
 			
 			if(SpreadMax != listOfQueue.Count){
 				
 				int difference = SpreadMax - listOfQueue.Count;
-				
-				
-				
+					
 				if(difference < 0){
 					
 					int temp = difference * -1;
-					listOfQueue.RemoveRange(SpreadMax-1,temp);
-								
+					listOfQueue.RemoveRange(SpreadMax-1,temp);					
 				}
 				
 				if(difference > 0){
 					for(int i = 0; i<difference; i++){
 						listOfQueue.Add(new Queue<double>());
 					}
-				}
-				
-				
+				}			
 			}
 			
-			
 			for(int index = 0; index<SpreadMax;index++){
+						
+				if(FDelayFrames[index] != listOfQueue[index].Count){
 				
-					
-				if(delayFrames[index] != listOfQueue[index].Count){
-				
-				
-					int difference = delayFrames[index] - listOfQueue[index].Count;
+					int difference = FDelayFrames[index] - listOfQueue[index].Count;
 								
-					if(delayFrames[index]>=0){
-						setDelayQueue(index, SpreadMax, difference);	
+					if(FDelayFrames[index]>=0){
+						SetDelayQueue(index, SpreadMax, difference);	
 					}
 				
 					else{
 						FOutput[index] = FInput[index];
 					}
-			
 				}
 			
-				if(reset[index]){
+				if(FReset[index]){
 				listOfQueue[index].Clear();
 				}
 			
 			listOfQueue[index].Enqueue(FInput[index]);
 				
-				if(skip[index]){
+				if(FSkip[index]){
 					FOutput[index] = FInput[index];
 				}
 			
@@ -116,16 +93,9 @@ namespace VVVV.Nodes
 				FOutput[index] = listOfQueue[index].Dequeue();
 				}
 			}
-
-			
 		}
-			
 
-////////////////////////////////////////////////////////////////////////////	
-		
-		
-		public void setDelayQueue(int index, int SMax, int diff){
-			
+		public void SetDelayQueue(int index, int SMax, int diff){
 			
 			if(diff > 0){
 				for(int i = 0; i < diff; i++){
@@ -140,12 +110,8 @@ namespace VVVV.Nodes
 				for(int i = 0; i<temp ;i++){
 					FOutput[index] = listOfQueue[index].Dequeue();
 				}
-			}	
-			
-		}
-			
-	
-		
+			}			
+		}	
 	}
 }
 
