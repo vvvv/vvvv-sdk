343af39622e4d710a323eda976f86f4bb59e9df8
gregsn <gregor@vvvv.org>
2018-06-05 21:44:53 +0200

    [vvvv PluginInterface]: show first slice of a "node" spread. currently done in the DefaultConnectionHandler, which is currently only used in some node in put pins. For example i think vux isn't using this handler. So when having ISpread<Foo> inputs and outputs and connecting the nodes one can see that ToString() on Foo gets called on the first slice.

--- vvvv45/src/core/Hosting/IO/DefaultConnectionHandler.cs
+++ vvvv45/src/core/Hosting/IO/DefaultConnectionHandler.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Linq;
+using System.Text;
 using VVVV.PluginInterfaces.V1;
 using VVVV.Utils.Reflection;
+using VVVV.Utils.Streams;
 
 namespace VVVV.Hosting.IO
 {
@@ -29,12 +32,12 @@ namespace VVVV.Hosting.IO
 		
 		public string GetFriendlyNameForSink(object sink)
 		{
-			return string.Format(" [{0}]", GetSinkDataType(sink).GetCSharpName());
+			return string.Format(" [{0}]{1}", GetSinkDataType(sink).GetCSharpName(), GetValueRendering(sink));
 		}
 		
 		public string GetFriendlyNameForSource(object source)
 		{
-			return string.Format(" [{0}]", GetSourceDataType(source).GetCSharpName());
+			return string.Format(" [{0}]{1}", GetSourceDataType(source).GetCSharpName(), GetValueRendering(source));
 		}
 
         private static Type GetType(object value)
@@ -43,6 +46,34 @@ namespace VVVV.Hosting.IO
             if (wrapper != null)
                 return wrapper.Value.GetType();
             return value.GetType();
+        }
+
+        private static string GetValueRendering(object value)
+        {
+            var enumerable = value as IEnumerable;
+            ISynchronizable sync = null;
+            if (enumerable == null)
+            {
+                var wrapper = value as DynamicTypeWrapper;
+                if (wrapper != null)
+                    enumerable = wrapper.Value as IEnumerable;
+            }
+            sync = enumerable as ISynchronizable;
+
+            if (sync != null)
+            {
+                sync.Sync();
+                var enumerator = enumerable.GetEnumerator();
+                var b = new StringBuilder(" : ");
+                if (enumerator.MoveNext())
+                    b.Append(enumerator.Current.ToString());
+                else
+                    return " : empty";
+                if (enumerator.MoveNext())
+                    b.Append(", ..");
+                return b.ToString();
+            }
+            return ""; // value.ToString();
         }
 
         private static Type GetDataType(Type ioType)
